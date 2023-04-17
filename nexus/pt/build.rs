6e18e4948a5b9984cb45dd8f39d4a5c365e8e51c
Kaushik Iska <iska.kaushik@gmail.com>
2023-04-17 10:55:55 -0400

    Basic catalog impl

--- nexus/pt/build.rs
+++ nexus/pt/build.rs
@@ -1,8 +1,6 @@
 use std::io::Result;
 
 fn main() -> Result<()> {
-    println!("cargo:rerun-if-changed=build.rs");
-
     // path to workspace root
     let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
     let manifest_root = std::path::Path::new(&manifest_dir);
@@ -16,6 +14,11 @@ fn main() -> Result<()> {
         .filter(|e| e.file_type().map(|t| t.is_file()).unwrap_or(false))
         .map(|e| e.path())
         .collect::<Vec<_>>();
+
+    // iterate and print all the proto files
+    for proto in &proto_files {
+        println!("cargo:warning={}", proto.display());
+    }
 
     // compile all protos in <root>/protos by iterating over the directory
     prost_build::compile_protos(&proto_files, &[root.join("protos")])?;
