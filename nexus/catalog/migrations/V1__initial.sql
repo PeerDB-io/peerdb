beb897bb6cf8dc33d0f859675605095e18d188fc
Kaushik Iska <iska.kaushik@gmail.com>
2023-04-17 11:01:22 -0400

    Compiling Protos and basic catalog setup (#3)

---
+++ nexus/catalog/migrations/V1__initial.sql
@@ -0,0 +1,18 @@
+CREATE TABLE peers (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL UNIQUE,
+  type INTEGER NOT NULL,
+  options BYTEA NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS flows (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL UNIQUE,
+  source_peer INTEGER NOT NULL REFERENCES peers(id),
+  destination_peer INTEGER NOT NULL REFERENCES peers(id),
+  description TEXT,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  source_table_identifier TEXT,
+  destination_table_identifier TEXT
+);
