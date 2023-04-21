7bd8b80ac2c5b87d497c8282156fc39bc1fe2324
Kaushik Iska <iska.kaushik@gmail.com>
2023-04-20 23:14:30 -0400

    Support CREATE PEER statement (#8)

--- nexus/pt/src/lib.rs
+++ nexus/pt/src/lib.rs
@@ -1,3 +1,17 @@
+use peers::DbType;
+use sqlparser::ast::PeerType;
+
 pub mod peers {
     include!(concat!(env!("OUT_DIR"), "/peerdb.peers.rs"));
+}
+
+impl From<PeerType> for DbType {
+    fn from(peer_type: PeerType) -> Self {
+        match peer_type {
+            PeerType::Bigquery => DbType::Bigquery,
+            PeerType::Mongo => DbType::Mongo,
+            PeerType::Snowflake => DbType::Snowflake,
+            PeerType::Postgres => DbType::Postgres,
+        }
+    }
 }
