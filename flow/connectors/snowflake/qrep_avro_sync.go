cd289caeca26b2e85791b0e714ce199f1ece07f3
Kaushik Iska <iska.kaushik@gmail.com>
2023-06-17 08:24:27 -0700

    QRepWriteMode upsert and support for snowflake

--- flow/connectors/snowflake/qrep_avro_sync.go
+++ flow/connectors/snowflake/qrep_avro_sync.go
@@ -29,12 +29,12 @@ func NewSnowflakeAvroSyncMethod(connector *SnowflakeConnector, localDir string) 
 }
 
 func (s *SnowflakeAvroSyncMethod) SyncQRepRecords(
-	flowJobName string,
-	dstTableName string,
+	config *protos.QRepConfig,
 	partition *protos.QRepPartition,
 	dstTableSchema []*sql.ColumnType,
 	records *model.QRecordBatch) (int, error) {
 	startTime := time.Now()
+	dstTableName := config.DestinationTableIdentifier
 
 	// You will need to define your Avro schema as a string
 	avroSchema, err := model.GetAvroSchemaDefinition(dstTableName, records.Schema)
@@ -110,15 +110,73 @@ func (s *SnowflakeAvroSyncMethod) SyncQRepRecords(
 		"FILE_FORMAT = (TYPE = AVRO)",
 		"MATCH_BY_COLUMN_NAME='CASE_INSENSITIVE'",
 	}
-	//nolint:gosec
-	copyCmd := fmt.Sprintf("COPY INTO %s FROM @%s %s", dstTableName, stage, strings.Join(copyOpts, ","))
-	if _, err = s.connector.database.Exec(copyCmd); err != nil {
-		return 0, fmt.Errorf("failed to run COPY INTO command: %w", err)
+
+	// based on the write mode we will upsert or append, write mode.
+	writeType := config.WriteMode.WriteType
+	switch writeType {
+	case protos.QRepWriteType_QREP_WRITE_MODE_APPEND:
+		//nolint:gosec
+		copyCmd := fmt.Sprintf("COPY INTO %s FROM @%s %s", dstTableName, stage, strings.Join(copyOpts, ","))
+		if _, err = s.connector.database.Exec(copyCmd); err != nil {
+			return 0, fmt.Errorf("failed to run COPY INTO command: %w", err)
+		}
+		log.Infof("copied file from stage %s to table %s", stage, dstTableName)
+
+	case protos.QRepWriteType_QREP_WRITE_MODE_UPSERT:
+		tempTableName := fmt.Sprintf("%s_temp_%d", dstTableName, runID)
+
+		// Create a temporary table
+		//nolint:gosec
+		createTempTableCmd := fmt.Sprintf("CREATE TEMPORARY TABLE %s AS SELECT * FROM %s LIMIT 0",
+			tempTableName, dstTableName)
+		if _, err = s.connector.database.Exec(createTempTableCmd); err != nil {
+			return 0, fmt.Errorf("failed to create temp table: %w", err)
+		}
+		log.Infof("created temp table %s", tempTableName)
+
+		// Copy the data into the temporary table
+		//nolint:gosec
+		copyCmd := fmt.Sprintf("COPY INTO %s FROM @%s %s", tempTableName, stage, strings.Join(copyOpts, ","))
+		if _, err = s.connector.database.Exec(copyCmd); err != nil {
+			return 0, fmt.Errorf("failed to run COPY INTO command: %w", err)
+		}
+		log.Infof("copied file from stage %s to temp table %s", stage, tempTableName)
+
+		// Define upsert keys
+		upsertKey := strings.Join(config.WriteMode.UpsertKeyColumns, ", ")
+
+		updateSetClauses := []string{}
+		insertColumnsClauses := []string{}
+		insertValuesClauses := []string{}
+		for _, column := range colNames {
+			updateSetClauses = append(updateSetClauses, fmt.Sprintf("%s = src.%s", column, column))
+			insertColumnsClauses = append(insertColumnsClauses, column)
+			insertValuesClauses = append(insertValuesClauses, fmt.Sprintf("src.%s", column))
+		}
+		updateSetClause := strings.Join(updateSetClauses, ", ")
+		insertColumnsClause := strings.Join(insertColumnsClauses, ", ")
+		insertValuesClause := strings.Join(insertValuesClauses, ", ")
+
+		// Merge (upsert) the data into the destination table
+		//nolint:gosec
+		mergeCmd := fmt.Sprintf(`
+			MERGE INTO %s dst
+			USING %s src
+			ON %s
+			WHEN MATCHED THEN UPDATE SET %s
+			WHEN NOT MATCHED THEN INSERT (%s) VALUES (%s)
+		`, dstTableName, tempTableName, upsertKey, updateSetClause,
+			insertColumnsClause, insertValuesClause)
+		if _, err = s.connector.database.Exec(mergeCmd); err != nil {
+			return 0, fmt.Errorf("failed to merge data into destination table: %w", err)
+		}
+		log.Infof("merged data from temp table %s into destination table %s", tempTableName, dstTableName)
+	default:
+		return 0, fmt.Errorf("unknown write type: %v", writeType)
 	}
-	log.Infof("copied file from stage %s to table %s", stage, dstTableName)
 
 	// Insert metadata statement
-	insertMetadataStmt, err := s.connector.createMetadataInsertStatement(partition, flowJobName, startTime)
+	insertMetadataStmt, err := s.connector.createMetadataInsertStatement(partition, config.FlowJobName, startTime)
 	if err != nil {
 		return -1, fmt.Errorf("failed to create metadata insert statement: %v", err)
 	}
@@ -127,8 +185,7 @@ func (s *SnowflakeAvroSyncMethod) SyncQRepRecords(
 	if _, err = s.connector.database.Exec(insertMetadataStmt); err != nil {
 		return -1, fmt.Errorf("failed to execute metadata insert statement: %v", err)
 	}
+	log.Infof("inserted metadata for partition %s", partition)
 
-	log.Infof("pushed %d records to local file %s and loaded into Snowflake table %s",
-		len(records.Records), localFilePath, dstTableName)
 	return len(records.Records), nil
 }
