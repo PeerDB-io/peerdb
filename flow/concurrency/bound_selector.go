f590e7948b59e12fe62991e5d007b93eaa419a01
Amogh-Bharadwaj <amogh@peerdb.io>
2023-08-16 01:54:49 +0530

    Revert "Add some concurrency locking around bound selector (#322)"
    This reverts commit 8b57dfe58c73d29c78c3cbd7e991d83eef511da4.

--- flow/concurrency/bound_selector.go
+++ flow/concurrency/bound_selector.go
@@ -2,7 +2,6 @@ package concurrency
 
 import (
 	"errors"
-	"sync"
 
 	"go.temporal.io/sdk/workflow"
 )
@@ -13,7 +12,6 @@ type BoundSelector struct {
 	selector workflow.Selector
 	futures  map[workflow.Future]struct{}
 	ferrors  []error
-	mu       sync.Mutex
 }
 
 func NewBoundSelector(limit int, ctx workflow.Context) *BoundSelector {
@@ -27,33 +25,23 @@ func NewBoundSelector(limit int, ctx workflow.Context) *BoundSelector {
 }
 
 func (s *BoundSelector) AddFuture(future workflow.Future, f func(workflow.Future) error) {
-	s.mu.Lock()
 	if len(s.futures) >= s.limit {
 		s.selector.Select(s.ctx)
 	}
-	s.futures[future] = struct{}{}
-	s.mu.Unlock()
 
+	s.futures[future] = struct{}{}
 	s.selector.AddFuture(future, func(ready workflow.Future) {
-		s.mu.Lock()
 		delete(s.futures, ready)
 
 		err := f(ready)
 		if err != nil {
 			s.ferrors = append(s.ferrors, err)
 		}
-		s.mu.Unlock()
 	})
 }
 
 func (s *BoundSelector) Wait() error {
-	for {
-		s.mu.Lock()
-		if len(s.futures) == 0 {
-			s.mu.Unlock()
-			break
-		}
-		s.mu.Unlock()
+	for len(s.futures) > 0 {
 		s.selector.Select(s.ctx)
 	}
 
