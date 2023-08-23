7cf37682bebd28681668b5e5a630ef1f6fc66d27
Amogh-Bharadwaj <amogh@peerdb.io>
2023-08-23 17:25:32 +0530

    fix: merge,update stmts

--- flow/utils/random.go
+++ flow/utils/random.go
@@ -27,3 +27,13 @@ func RandomUInt64() (uint64, error) {
 	// Convert bytes to uint64
 	return binary.LittleEndian.Uint64(b), nil
 }
+
+func RandomString(n int) string {
+	const alphanum = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	var bytes = make([]byte, n)
+	rand.Read(bytes)
+	for i, b := range bytes {
+		bytes[i] = alphanum[b%byte(len(alphanum))]
+	}
+	return string(bytes)
+}
