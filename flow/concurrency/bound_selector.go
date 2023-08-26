7f9432c5cd799c3f3b3086e91be2bda20c0e5a3b
Kaushik Iska <iska.kaushik@gmail.com>
2023-08-26 14:15:18 -0400

    Rewrite Bound Selector
    Also fix range iterator order

--- flow/concurrency/bound_selector.go
+++ flow/concurrency/bound_selector.go
@@ -7,46 +7,54 @@ import (
 )
 
 type BoundSelector struct {
-	ctx      workflow.Context
-	limit    int
-	selector workflow.Selector
-	futures  map[workflow.Future]struct{}
-	ferrors  []error
+	ctx        workflow.Context
+	limit      workflow.Channel
+	statusCh   workflow.Channel
+	numFutures int
 }
 
-func NewBoundSelector(limit int, ctx workflow.Context) *BoundSelector {
+func NewBoundSelector(limit int, total int, ctx workflow.Context) *BoundSelector {
 	return &BoundSelector{
-		ctx:      ctx,
-		limit:    limit,
-		selector: workflow.NewSelector(ctx),
-		futures:  make(map[workflow.Future]struct{}),
-		ferrors:  make([]error, 0),
+		ctx:        ctx,
+		limit:      workflow.NewBufferedChannel(ctx, limit),
+		statusCh:   workflow.NewBufferedChannel(ctx, total),
+		numFutures: 0,
 	}
 }
 
-func (s *BoundSelector) AddFuture(future workflow.Future, f func(workflow.Future) error) {
-	if len(s.futures) >= s.limit {
-		s.selector.Select(s.ctx)
-	}
-
-	s.futures[future] = struct{}{}
-	s.selector.AddFuture(future, func(ready workflow.Future) {
-		delete(s.futures, ready)
-
-		err := f(ready)
-		if err != nil {
-			s.ferrors = append(s.ferrors, err)
-		}
+func (s *BoundSelector) SpawnChild(chCtx workflow.Context, w interface{}, args ...interface{}) {
+	s.numFutures++
+	workflow.Go(s.ctx, func(ctx workflow.Context) {
+		s.limit.Send(ctx, struct{}{})
+		future := workflow.ExecuteChildWorkflow(chCtx, w, args...)
+		err := future.Get(ctx, nil)
+		s.statusCh.Send(ctx, err)
+		s.limit.Receive(ctx, nil)
 	})
 }
 
 func (s *BoundSelector) Wait() error {
-	for len(s.futures) > 0 {
-		s.selector.Select(s.ctx)
+	defer s.statusCh.Close()
+	defer s.limit.Close()
+
+	ferrors := make([]error, 0)
+	doneCount := 0
+
+	for doneCount < s.numFutures {
+		selector := workflow.NewSelector(s.ctx)
+		selector.AddReceive(s.statusCh, func(c workflow.ReceiveChannel, more bool) {
+			var err error
+			c.Receive(s.ctx, &err)
+			if err != nil {
+				ferrors = append(ferrors, err)
+			}
+			doneCount++
+		})
+		selector.Select(s.ctx)
 	}
 
-	if len(s.ferrors) > 0 {
-		return errors.Join(s.ferrors...)
+	if len(ferrors) > 0 {
+		return errors.Join(ferrors...)
 	}
 
 	return nil
