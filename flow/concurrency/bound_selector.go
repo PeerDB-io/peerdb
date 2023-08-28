e55eee4266c4c2a90b6c08e5f656adeb99a08cfc
Kaushik Iska <iska.kaushik@gmail.com>
2023-08-28 13:01:59 -0400

    simplify bound selector always wait for the first future

--- flow/concurrency/bound_selector.go
+++ flow/concurrency/bound_selector.go
@@ -7,54 +7,49 @@ import (
 )
 
 type BoundSelector struct {
-	ctx        workflow.Context
-	limit      workflow.Channel
-	statusCh   workflow.Channel
-	numFutures int
+	ctx     workflow.Context
+	limit   int
+	futures []workflow.Future
+	ferrors []error
 }
 
 func NewBoundSelector(limit int, total int, ctx workflow.Context) *BoundSelector {
 	return &BoundSelector{
-		ctx:        ctx,
-		limit:      workflow.NewBufferedChannel(ctx, limit),
-		statusCh:   workflow.NewBufferedChannel(ctx, total),
-		numFutures: 0,
+		ctx:   ctx,
+		limit: limit,
 	}
 }
 
 func (s *BoundSelector) SpawnChild(chCtx workflow.Context, w interface{}, args ...interface{}) {
-	s.numFutures++
-	workflow.Go(s.ctx, func(ctx workflow.Context) {
-		s.limit.Send(ctx, struct{}{})
-		future := workflow.ExecuteChildWorkflow(chCtx, w, args...)
-		err := future.Get(ctx, nil)
-		s.statusCh.Send(ctx, err)
-		s.limit.Receive(ctx, nil)
-	})
+	if len(s.futures) >= s.limit {
+		s.waitOne()
+	}
+
+	future := workflow.ExecuteChildWorkflow(chCtx, w, args...)
+	s.futures = append(s.futures, future)
+}
+
+func (s *BoundSelector) waitOne() {
+	if len(s.futures) == 0 {
+		return
+	}
+
+	f := s.futures[0]
+	s.futures = s.futures[1:]
+
+	err := f.Get(s.ctx, nil)
+	if err != nil {
+		s.ferrors = append(s.ferrors, err)
+	}
 }
 
 func (s *BoundSelector) Wait() error {
-	defer s.statusCh.Close()
-	defer s.limit.Close()
-
-	ferrors := make([]error, 0)
-	doneCount := 0
-
-	for doneCount < s.numFutures {
-		selector := workflow.NewSelector(s.ctx)
-		selector.AddReceive(s.statusCh, func(c workflow.ReceiveChannel, more bool) {
-			var err error
-			c.Receive(s.ctx, &err)
-			if err != nil {
-				ferrors = append(ferrors, err)
-			}
-			doneCount++
-		})
-		selector.Select(s.ctx)
+	for len(s.futures) > 0 {
+		s.waitOne()
 	}
 
-	if len(ferrors) > 0 {
-		return errors.Join(ferrors...)
+	if len(s.ferrors) > 0 {
+		return errors.Join(s.ferrors...)
 	}
 
 	return nil
