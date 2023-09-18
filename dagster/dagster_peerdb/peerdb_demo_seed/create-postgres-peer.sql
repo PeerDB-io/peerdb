ce87750b876ad6f97ec00b0995b4d37b096a3f36
Kaushik Iska <iska.kaushik@gmail.com>
2023-09-18 09:58:02 -0400

    remove dagster

--- dagster/dagster_peerdb/peerdb_demo_seed/create-postgres-peer.sql
+++
@@ -1,8 +0,0 @@
-CREATE PEER postgres_peer FROM POSTGRES WITH
-(
-    host = 'docker.for.mac.localhost',
-    port = '5432',
-    user = 'postgres',
-    password = 'postgres',
-    database = 'postgres'
-);
