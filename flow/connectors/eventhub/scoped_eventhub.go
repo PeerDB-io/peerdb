339dc766c6bf5df5619fd08d97b0dc9a617bf4b0
Kaushik Iska <iska.kaushik@gmail.com>
2023-09-30 13:14:18 -0400

    partition more by parts in eventhub

--- flow/connectors/eventhub/scoped_eventhub.go
+++ flow/connectors/eventhub/scoped_eventhub.go
@@ -6,29 +6,33 @@ import (
 )
 
 type ScopedEventhub struct {
-	PeerName string
-	Eventhub string
+	PeerName   string
+	Eventhub   string
+	Identifier string
 }
 
 func NewScopedEventhub(raw string) (ScopedEventhub, error) {
-	// split by dot
+	// split by dot, the model is peername.eventhub.identifier
 	parts := strings.Split(raw, ".")
 
-	if len(parts) != 2 {
-		return ScopedEventhub{}, fmt.Errorf("invalid peer and topic name %s", raw)
+	if len(parts) != 3 {
+		return ScopedEventhub{}, fmt.Errorf("invalid scoped eventhub '%s'", raw)
 	}
 
 	return ScopedEventhub{
-		PeerName: parts[0],
-		Eventhub: parts[1],
+		PeerName:   parts[0],
+		Eventhub:   parts[1],
+		Identifier: parts[2],
 	}, nil
 }
 
 func (s ScopedEventhub) Equals(other ScopedEventhub) bool {
-	return s.PeerName == other.PeerName && s.Eventhub == other.Eventhub
+	return s.PeerName == other.PeerName &&
+		s.Eventhub == other.Eventhub &&
+		s.Identifier == other.Identifier
 }
 
 // ToString returns the string representation of the ScopedEventhub
 func (s ScopedEventhub) ToString() string {
-	return fmt.Sprintf("%s.%s", s.PeerName, s.Eventhub)
+	return fmt.Sprintf("%s.%s.%s", s.PeerName, s.Eventhub, s.Identifier)
 }
