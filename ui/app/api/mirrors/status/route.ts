81698646a23dffe0276255035bdf57e9b8fdcb1d
Kaushik Iska <iska.kaushik@gmail.com>
2023-10-13 15:05:29 -0400

    fix prettier issues

--- ui/app/api/mirrors/status/route.ts
+++ ui/app/api/mirrors/status/route.ts
@@ -8,7 +8,6 @@ function getMirrorStatusUrl(mirrorId: string) {
 export async function POST(request: Request) {
   const { flowJobName } = await request.json();
   const url = getMirrorStatusUrl(flowJobName);
-  console.log(`querying ${url} for flow status.`)
   const resp = await fetch(url);
   const json = await resp.json();
   return new Response(JSON.stringify(json));
