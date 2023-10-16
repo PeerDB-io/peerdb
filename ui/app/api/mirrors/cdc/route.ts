e467c9f9228b66422e8fc7e36ee33998d7564b1a
Kaushik Iska <iska.kaushik@gmail.com>
2023-10-16 10:09:30 -0400

    Either go through gRPC gateway or use prisma (#519)

--- ui/app/api/mirrors/cdc/route.ts
+++ ui/app/api/mirrors/cdc/route.ts
@@ -1,21 +1,31 @@
+import { UCreateMirrorResponse } from '@/app/dto/MirrorsDTO';
 import {
   CreateCDCFlowRequest,
   CreateCDCFlowResponse,
 } from '@/grpc_generated/route';
-import { GetFlowServiceClientFromEnv } from '@/rpc/rpc';
+import { GetFlowHttpAddressFromEnv } from '@/rpc/http';
 
 export async function POST(request: Request) {
   const body = await request.json();
   const { config } = body;
-  const flowServiceClient = GetFlowServiceClientFromEnv();
+  const flowServiceAddr = GetFlowHttpAddressFromEnv();
   const req: CreateCDCFlowRequest = {
     connectionConfigs: config,
     createCatalogEntry: true,
   };
-  const createStatus: CreateCDCFlowResponse =
-    await flowServiceClient.createCdcFlow(req);
-  if (!createStatus.worflowId) {
-    return new Response('Failed to create CDC mirror');
-  }
-  return new Response('created');
+  const createStatus: CreateCDCFlowResponse = await fetch(
+    `${flowServiceAddr}/v1/cdc/create`,
+    {
+      method: 'POST',
+      body: JSON.stringify(req),
+    }
+  ).then((res) => {
+    return res.json();
+  });
+
+  let response: UCreateMirrorResponse = {
+    created: !!createStatus.worflowId,
+  };
+
+  return new Response(JSON.stringify(response));
 }
--- ui/app/peers/handler.ts
+++
@@ -1,8 +0,0 @@
-import { ListPeersRequest } from '@/grpc_generated/route';
-import { GetFlowServiceClientFromEnv } from '@/rpc/rpc';
-export async function fetchPeers() {
-  let flowServiceClient = GetFlowServiceClientFromEnv();
-  let req: ListPeersRequest = {};
-  let peers = await flowServiceClient.listPeers(req);
-  return peers.peers;
-}
