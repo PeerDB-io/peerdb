c036c5b205e49c9687f1d2f65fcc70e0db0ec038
Kaushik Iska <iska.kaushik@gmail.com>
2023-10-16 14:37:21 -0400

    Add the ability to push to eventhubs in an asynchronous way
    `PEERDB_BETA_EVENTHUB_PUSH_ASYNC=true` enables this

---
+++ flow/connectors/utils/env.go
@@ -0,0 +1,30 @@
+package utils
+
+import (
+	"os"
+	"strconv"
+)
+
+// GetEnv returns the value of the environment variable with the given name
+// and a boolean indicating whether the environment variable exists.
+func GetEnv(name string) (string, bool) {
+	val, exists := os.LookupEnv(name)
+	return val, exists
+}
+
+// GetEnvBool returns the value of the environment variable with the given name
+// or defaultValue if the environment variable is not set or is not a valid
+// boolean value.
+func GetEnvBool(name string, defaultValue bool) bool {
+	val, ok := GetEnv(name)
+	if !ok {
+		return defaultValue
+	}
+
+	b, err := strconv.ParseBool(val)
+	if err != nil {
+		return defaultValue
+	}
+
+	return b
+}
