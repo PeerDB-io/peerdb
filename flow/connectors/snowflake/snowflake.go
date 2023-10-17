248f95603136264f78c2487d50945f8909bf41cb
Amogh-Bharadwaj <amogh@peerdb.io>
2023-10-17 20:05:23 +0530

    supports allow_invalid

--- flow/connectors/snowflake/snowflake.go
+++ flow/connectors/snowflake/snowflake.go
@@ -961,8 +961,11 @@ func (c *SnowflakeConnector) generateAndExecuteMergeStatement(
 		case qvalue.QValueKindBytes, qvalue.QValueKindBit:
 			flattenedCastsSQLArray = append(flattenedCastsSQLArray, fmt.Sprintf("BASE64_DECODE_BINARY(%s:\"%s\") "+
 				"AS %s,", toVariantColumnName, columnName, targetColumnName))
-		case qvalue.QValueKindGeography, qvalue.QValueKindGeometry, qvalue.QValueKindPoint:
-			flattenedCastsSQLArray = append(flattenedCastsSQLArray, fmt.Sprintf("CAST(%s:\"%s\" AS STRING) AS %s,",
+		case qvalue.QValueKindGeography:
+			flattenedCastsSQLArray = append(flattenedCastsSQLArray, fmt.Sprintf("TO_GEOGRAPHY(CAST(%s:\"%s\" AS STRING),true) AS %s,",
+				toVariantColumnName, columnName, targetColumnName))
+		case qvalue.QValueKindGeometry:
+			flattenedCastsSQLArray = append(flattenedCastsSQLArray, fmt.Sprintf("TO_GEOMETRY(CAST(%s:\"%s\" AS STRING),true) AS %s,",
 				toVariantColumnName, columnName, targetColumnName))
 		// TODO: https://github.com/PeerDB-io/peerdb/issues/189 - handle time types and interval types
 		// case model.ColumnTypeTime:
