926df3e1d0c1c5f143d3530b46a944d8829b4583
Kevin Biju <kevin@peerdb.io>
2023-10-23 22:06:20 +0530

    don't attach to unnecessary services for run-peerdb.sh

--- run-peerdb.sh
+++ run-peerdb.sh
@@ -8,4 +8,4 @@ then
 fi
 
 docker compose pull
-docker compose -f docker-compose.yml up
+docker compose -f docker-compose.yml up --no-attach catalog --no-attach temporal --no-attach temporal-ui --no-attach temporal-admin-tools
