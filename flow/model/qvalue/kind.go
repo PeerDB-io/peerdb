fcd127f8d89b6cfe3e66bc8a41852fca93bb642b
Kaushik Iska <iska.kaushik@gmail.com>
2023-10-30 17:14:32 -0400

    Add basic schema changes support for QRep

--- flow/model/qvalue/kind.go
+++ flow/model/qvalue/kind.go
@@ -1,5 +1,7 @@
 package qvalue
 
+import "fmt"
+
 type QValueKind string
 
 const (
@@ -45,5 +47,44 @@ func QValueKindIsArray(kind QValueKind) bool {
 		return true
 	default:
 		return false
+	}
+}
+
+func (kind QValueKind) ToDWHColumnType(dwhType QDWHType) (string, error) {
+	if dwhType != QDWHTypeSnowflake {
+		return "", fmt.Errorf("unsupported DWH type: %v", dwhType)
+	}
+
+	switch kind {
+	case QValueKindFloat32, QValueKindFloat64:
+		return "FLOAT", nil
+	case QValueKindInt16, QValueKindInt32, QValueKindInt64:
+		return "INTEGER", nil
+	case QValueKindBoolean:
+		return "BOOLEAN", nil
+	case QValueKindString:
+		return "VARCHAR", nil
+	case QValueKindTimestamp, QValueKindTimestampTZ:
+		return "TIMESTAMP_NTZ", nil // or TIMESTAMP_TZ based on your needs
+	case QValueKindDate:
+		return "DATE", nil
+	case QValueKindTime, QValueKindTimeTZ:
+		return "TIME", nil
+	case QValueKindNumeric:
+		return "NUMBER", nil
+	case QValueKindBytes:
+		return "BINARY", nil
+	case QValueKindUUID:
+		return "VARCHAR", nil // Snowflake doesn't have a native UUID type
+	case QValueKindJSON:
+		return "VARIANT", nil
+	case QValueKindArrayFloat32,
+		QValueKindArrayFloat64,
+		QValueKindArrayInt32,
+		QValueKindArrayInt64,
+		QValueKindArrayString:
+		return "ARRAY", nil
+	default:
+		return "", fmt.Errorf("unsupported QValueKind: %v for dwhtype: %v", kind, dwhType)
 	}
 }
