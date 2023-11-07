f7823d006287354f9bd27c10da4407957da19f70
Amogh-Bharadwaj <amogh@peerdb.io>
2023-11-07 09:49:06 -0500

    filter at format function

--- ui/app/mirrors/create/handlers.ts
+++ ui/app/mirrors/create/handlers.ts
@@ -58,15 +58,17 @@ interface TableMapping {
   partitionKey: string;
 }
 const reformattedTableMapping = (tableMapping: TableMapRow[]) => {
-  const mapping = tableMapping.map((row) => {
-    if (row.selected === true) {
-      return {
-        sourceTableIdentifier: row.source,
-        destinationTableIdentifier: row.destination,
-        partitionKey: row.partitionKey,
-      };
-    }
-  });
+  const mapping = tableMapping
+    .map((row) => {
+      if (row.selected === true) {
+        return {
+          sourceTableIdentifier: row.source,
+          destinationTableIdentifier: row.destination,
+          partitionKey: row.partitionKey,
+        };
+      }
+    })
+    .filter(Boolean);
   return mapping;
 };
 
@@ -90,7 +92,7 @@ export const handleCreateCDC = async (
   const isValid = validateCDCFields(rows, setMsg, config);
   if (!isValid) return;
   const tableNameMapping = reformattedTableMapping(rows);
-  config['tableMappings'] = tableNameMapping.filter(Boolean) as TableMapping[];
+  config['tableMappings'] = tableNameMapping as TableMapping[];
   config['flowJobName'] = flowJobName;
   setLoading(true);
   const statusMessage: UCreateMirrorResponse = await fetch('/api/mirrors/cdc', {
