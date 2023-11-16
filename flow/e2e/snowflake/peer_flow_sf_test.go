8444e1f860ec391ef216f425deb8bb756e771af3
Demur Rumed <junkmute@hotmail.com>
2023-11-16 01:21:54 +0000

    test: remove print, add check on record count

--- flow/e2e/snowflake/peer_flow_sf_test.go
+++ flow/e2e/snowflake/peer_flow_sf_test.go
@@ -1418,10 +1418,9 @@ func (s *PeerFlowE2ETestSuiteSF) Test_Column_Exclusion(t *testing.T) {
 	sfRows, err := s.sfHelper.ExecuteAndProcessQuery(query)
 	require.NoError(t, err)
 
-	fmt.Printf("%v\n", sfRows.Schema.Fields)
 	for _, field := range sfRows.Schema.Fields {
 		require.NotEqual(t, field.Name, "c2")
 	}
 	require.Equal(t, 4, len(sfRows.Schema.Fields))
-	// TODO inspect content
+	require.Equal(t, 10, len(sfRows.Records))
 }
