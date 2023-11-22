3463629ad52b101fd40ddbeab856ae09a0825768
Demur Rumed <junkmute@hotmail.com>
2023-11-22 02:01:46 +0000

    works

--- ui/middleware.ts
+++ ui/middleware.ts
@@ -2,20 +2,12 @@ import type {NextRequest} from 'next/server';
 
 import {NextResponse} from 'next/server'
 
-export default function middleware(request: NextRequest) {
-  console.log(request);
+export default function middleware(req: NextRequest) {
   if (req.nextUrl.pathname !== '/' &&
       !req.nextUrl.pathname.startsWith("/_next/static/") &&
       process.env.PEERDB_PASSWORD &&
       req.cookies.get('password')?.value !== process.env.PEERDB_PASSWORD) {
-    console.log('REJECT');
     return new Response('{}', {status : 401});
   }
   return NextResponse.next()
 }
-
-/*
-export function middleware(req: NextRequest) {
-  console.log(req);
-  console.log(req.nextUrl);
-}*/
