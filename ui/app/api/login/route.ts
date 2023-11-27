605326d940ee6aeb469338f9b32918723cb31f30
Amogh-Bharadwaj <amogh@peerdb.io>
2023-11-27 15:48:05 +0530

    prettify login, logout appears everywhere

--- ui/app/api/login/route.ts
+++ ui/app/api/login/route.ts
@@ -3,7 +3,12 @@ import { cookies } from 'next/headers';
 export async function POST(request: Request) {
   const { password } = await request.json();
   if (process.env.PEERDB_PASSWORD !== password) {
-    return new Response(JSON.stringify({ error: 'wrong password' }));
+    return new Response(
+      JSON.stringify({
+        error:
+          'Your password is incorrect. Please check your password and try again.',
+      })
+    );
   }
   cookies().set('auth', password, {
     expires: Date.now() + 24 * 60 * 60 * 1000,
--- ui/components/Logout.tsx
+++ ui/components/Logout.tsx
@@ -4,13 +4,14 @@ import { Button } from '@/lib/Button';
 export default function Logout() {
   return (
     <Button
+      style={{ backgroundColor: 'white', border: '1px solid rgba(0,0,0,0.15)' }}
       onClick={() =>
         fetch('/api/logout', { method: 'POST' }).then((res) =>
           location.assign('/login')
         )
       }
     >
-      Logout
+      Log out
     </Button>
   );
 }
