24706ed553aa863a1a82261d8df0b221ef976343
Amogh Bharadwaj <amogh@peerdb.io>
2023-11-27 19:51:07 +0530

    Login improvements (#717)
    Polishes UI of login screen and makes logout button appear everywhere
    (not just home page)

--- ui/app/login/page.tsx
+++ ui/app/login/page.tsx
@@ -4,13 +4,15 @@ import { useRouter, useSearchParams } from 'next/navigation';
 import { useState } from 'react';
 
 import { Button } from '@/lib/Button';
-import { Layout, LayoutMain } from '@/lib/Layout';
+import { Icon } from '@/lib/Icon';
+import { Layout } from '@/lib/Layout';
 import { TextField } from '@/lib/TextField';
 
 export default function Login() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const [pass, setPass] = useState('');
+  const [show, setShow] = useState(false);
   const [error, setError] = useState(() =>
     searchParams.has('reject') ? 'Authentication failed, please login' : ''
   );
@@ -21,41 +23,86 @@ export default function Login() {
     })
       .then((res) => res.json())
       .then((res) => {
-        if (res.error) setError(res.error);
-        else router.push('/');
+        if (res.error) {
+          setError(res.error);
+        } else router.push('/');
       });
   };
   return (
     <Layout>
-      <LayoutMain alignSelf='center' justifySelf='center' width='xxLarge'>
-        <Image src='/images/peerdb-combinedMark.svg' alt='PeerDB' width={512} />
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: 'full',
+        }}
+      >
+        <Image
+          style={{ marginBottom: '4rem' }}
+          src='/images/peerdb-combinedMark.svg'
+          alt='PeerDB'
+          width={400}
+          height={300}
+        />
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            width: '20%',
+            height: '4%',
+          }}
+        >
+          <TextField
+            variant='simple'
+            placeholder='Password'
+            type={show ? 'text' : 'password'}
+            style={{
+              width: '100%',
+              height: '100%',
+              borderRadius: '0.5rem',
+              marginRight: '1rem',
+            }}
+            value={pass}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setPass(e.target.value)
+            }
+            onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
+              if (e.key === 'Enter') {
+                login();
+              }
+            }}
+          />
+          <Button onClick={() => setShow((curr) => !curr)}>
+            <Icon name={show ? 'visibility_off' : 'visibility'} />
+          </Button>
+        </div>
+        <Button
+          style={{
+            marginTop: '2rem',
+            width: '6em',
+            height: '2em',
+            boxShadow: '0px 4px 4px rgba(0,0,0,0.15)',
+          }}
+          variant='normalSolid'
+          onClick={login}
+        >
+          Log in
+        </Button>
         {error && (
           <div
             style={{
-              borderRadius: '8px',
-              fontWeight: 'bold',
-              color: '#600',
-              backgroundColor: '#c66',
+              borderRadius: '0.2rem',
+              padding: '0.5rem',
+              color: '#d46363',
+              marginTop: '1rem',
             }}
           >
             {error}
           </div>
         )}
-        <TextField
-          variant='simple'
-          placeholder='Password'
-          value={pass}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setPass(e.target.value)
-          }
-          onKeyPress={(e: React.KeyboardEvent<HTMLInputElement>) => {
-            if (e.key === 'Enter') {
-              login();
-            }
-          }}
-        />
-        <Button onClick={login}>Login</Button>
-      </LayoutMain>
+      </div>
     </Layout>
   );
 }
