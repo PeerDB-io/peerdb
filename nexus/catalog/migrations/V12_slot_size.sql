9be66bb53424b5d8bd291b602b7cf2941d7ec45a
Amogh-Bharadwaj <amogh@peerdb.io>
2023-12-07 19:19:00 +0530

    use new migration file instead

---
+++ nexus/catalog/migrations/V12_slot_size.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS peerdb_stats.peer_slot_size (
+    slot_name TEXT NOT NULL,
+    peer_name TEXT NOT NULL,
+    redo_lsn TEXT,
+    restart_lsn TEXT,
+    confirmed_flush_lsn TEXT,
+    slot_size BIGINT,
+    updated_at 
+    PRIMARY KEY (slot_name, peer_name)
+);
