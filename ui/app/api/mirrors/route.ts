fd37ff7ca91c62e4e4939d0e280d140156ae20ed
Pankaj B <pankaj@peerdb.io>
2023-12-18 16:40:16 +0530

    add mirrors api

---
+++ ui/app/api/mirrors/route.ts
@@ -0,0 +1,21 @@
+import { getTruePeer } from '@/app/api/peers/route';
+import prisma from '@/app/utils/prisma';
+export async function GET(request: Request) {
+  const mirrors = await prisma.flows.findMany({
+    distinct: 'name',
+    include: {
+      sourcePeer: true,
+      destinationPeer: true,
+    },
+  });
+
+  const flows = mirrors?.map((mirror) => {
+    let newMirror: any = {
+      ...mirror,
+      sourcePeer: getTruePeer(mirror.sourcePeer),
+      destinationPeer: getTruePeer(mirror.destinationPeer),
+    };
+    return newMirror;
+  });
+  return new Response(JSON.stringify(flows));
+}
