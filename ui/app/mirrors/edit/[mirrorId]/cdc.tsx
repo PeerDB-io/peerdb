5e9d7af0b88ab6731fb42ed6f09820927550a186
Amogh-Bharadwaj <amogh@peerdb.io>
2023-12-28 02:18:13 +0530

    lint

--- ui/app/mirrors/edit/[mirrorId]/cdc.tsx
+++ ui/app/mirrors/edit/[mirrorId]/cdc.tsx
@@ -289,9 +289,7 @@ export function CDCMirror({
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      setSelectedTab(
-        parseInt(localStorage?.getItem(LocalStorageTabKey)) | 0
-      );
+      setSelectedTab(parseInt(localStorage?.getItem(LocalStorageTabKey)) | 0);
     }
   }, []);
 
