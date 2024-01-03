PeerDB-io/peerdb

commit ec9486ea8d852bdbe2a41991e9d69db4d4930279

go lints: enable forbidigo (#923)
forbidigo flags any identifier matching `^(fmt\.Print.*|print|println)$`

Writing directly to stdout breaks logging

flow/e2eshared/e2eshared.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"testing"
 
+	"github.com/PeerDB-io/peer-flow/model"
+
 	"github.com/ysmood/got"
 )
 
@@ -41,4 +43,24 @@ func ReadFileToBytes(path string) ([]byte, error) {
 	}
 
 	return ret, nil
+}
+
+// checks if two QRecords are identical
+func CheckQRecordEquality(t *testing.T, q model.QRecord, other model.QRecord) bool {
+	t.Helper()
+
+	if q.NumEntries != other.NumEntries {
+		t.Logf("unequal entry count: %d != %d\n", q.NumEntries, other.NumEntries)
+		return false
+	}
+
+	for i, entry := range q.Entries {
+		otherEntry := other.Entries[i]
+		if !entry.Equals(otherEntry) {
+			t.Logf("entry %d: %v != %v\n", i, entry, otherEntry)
+			return false
+		}
+	}
+
+	return true
 }

flow/e2e/bigquery/qrep_flow_bq_test.go
@@ -23,7 +23,7 @@ func (s PeerFlowE2ETestSuiteBQ) setupBQDestinationTable(dstTable string) {
 	// fail if table creation fails
 	require.NoError(s.t, err)
 
-	fmt.Printf("created table on bigquery: %s.%s. %v\n", s.bqHelper.Config.DatasetId, dstTable, err)
+	s.t.Logf("created table on bigquery: %s.%s. %v", s.bqHelper.Config.DatasetId, dstTable, err)
 }
 
 func (s PeerFlowE2ETestSuiteBQ) compareTableContentsBQ(tableName string, colsString string) {
@@ -39,11 +39,11 @@ func (s PeerFlowE2ETestSuiteBQ) compareTableContentsBQ(tableName string, colsStr
 	// read rows from destination table
 	qualifiedTableName := fmt.Sprintf("`%s.%s`", s.bqHelper.Config.DatasetId, tableName)
 	bqSelQuery := fmt.Sprintf("SELECT %s FROM %s ORDER BY id", colsString, qualifiedTableName)
-	fmt.Printf("running query on bigquery: %s\n", bqSelQuery)
+	s.t.Logf("running query on bigquery: %s", bqSelQuery)
 	bqRows, err := s.bqHelper.ExecuteAndProcessQuery(bqSelQuery)
 	require.NoError(s.t, err)
 
-	s.True(pgRows.Equals(bqRows))
+	e2e.RequireEqualRecordBatchs(s.t, pgRows, bqRows)
 }
 
 func (s PeerFlowE2ETestSuiteBQ) Test_Complete_QRep_Flow_Avro() {

flow/e2e/snowflake/qrep_flow_sf_test.go
@@ -43,13 +43,12 @@ func (s PeerFlowE2ETestSuiteSF) compareTableContentsWithDiffSelectorsSF(tableNam
 		qualifiedTableName = fmt.Sprintf(`%s.%s.%s`, s.sfHelper.testDatabaseName, s.sfHelper.testSchemaName, tableName)
 	}
 
-	sfSelQuery := fmt.Sprintf(`SELECT %s FROM %s ORDER BY id`, sfSelector, qualifiedTableName)
-	fmt.Printf("running query on snowflake: %s\n", sfSelQuery)
-
+  sfSelQuery := fmt.Sprintf(`SELECT %s FROM %s ORDER BY id`, sfSelector, qualifiedTableName)
+	s.t.Logf("running query on snowflake: %s\n", sfSelQuery)
 	sfRows, err := s.sfHelper.ExecuteAndProcessQuery(sfSelQuery)
 	require.NoError(s.t, err)
 
-	require.True(s.t, pgRows.Equals(sfRows), "rows from source and destination tables are not equal")
+	e2e.RequireEqualRecordBatchs(s.t, pgRows, sfRows)
 }
 
 func (s PeerFlowE2ETestSuiteSF) Test_Complete_QRep_Flow_Avro_SF() {

flow/e2e/test_utils.go
@@ -13,6 +13,7 @@ import (
 	"github.com/PeerDB-io/peer-flow/activities"
 	connsnowflake "github.com/PeerDB-io/peer-flow/connectors/snowflake"
 	utils "github.com/PeerDB-io/peer-flow/connectors/utils/catalog"
+	"github.com/PeerDB-io/peer-flow/e2eshared"
 	"github.com/PeerDB-io/peer-flow/generated/protos"
 	"github.com/PeerDB-io/peer-flow/logger"
 	"github.com/PeerDB-io/peer-flow/model"
@@ -105,7 +106,6 @@ func NormalizeFlowCountQuery(env *testsuite.TestWorkflowEnvironment,
 			}
 
 			if len(state.NormalizeFlowStatuses) >= minCount {
-				fmt.Println("query indicates setup is complete")
 				break
 			}
 		} else {
@@ -173,7 +173,6 @@ func CreateTableForQRep(pool *pgxpool.Pool, suffix string, tableName string) err
 		return err
 	}
 
-	fmt.Printf("created table on postgres: e2e_test_%s.%s\n", suffix, tableName)
 	return nil
 }
 
@@ -417,4 +416,42 @@ func (l *TStructuredLogger) Warn(msg string, keyvals ...interface{}) {
 
 func (l *TStructuredLogger) Error(msg string, keyvals ...interface{}) {
 	l.logger.With(l.keyvalsToFields(keyvals)).Error(msg)
+}
+
+// Equals checks if two QRecordBatches are identical.
+func RequireEqualRecordBatchs(t *testing.T, q *model.QRecordBatch, other *model.QRecordBatch) bool {
+	t.Helper()
+
+	if other == nil {
+		t.Log("other is nil")
+		return q == nil
+	}
+
+	// First check simple attributes
+	if q.NumRecords != other.NumRecords {
+		// print num records
+		t.Logf("q.NumRecords: %d", q.NumRecords)
+		t.Logf("other.NumRecords: %d", other.NumRecords)
+		return false
+	}
+
+	// Compare column names
+	if !q.Schema.EqualNames(other.Schema) {
+		t.Log("Column names are not equal")
+		t.Logf("Schema 1: %v", q.Schema.GetColumnNames())
+		t.Logf("Schema 2: %v", other.Schema.GetColumnNames())
+		return false
+	}
+
+	// Compare records
+	for i, record := range q.Records {
+		if !e2eshared.CheckQRecordEquality(t, record, other.Records[i]) {
+			t.Logf("Record %d is not equal", i)
+			t.Logf("Record 1: %v", record)
+			t.Logf("Record 2: %v", other.Records[i])
+			return false
+		}
+	}
+
+	return true
 }

flow/.golangci.yml
@@ -6,6 +6,7 @@ linters:
     - dogsled
     - durationcheck
     - errcheck
+    - forbidigo
     - gofumpt
     - gosec
     - gosimple

flow/connectors/bigquery/bigquery.go
@@ -471,7 +471,6 @@ func (c *BigQueryConnector) getTableNametoUnchangedCols(flowJobName string, sync
 			break
 		}
 		if err != nil {
-			fmt.Printf("Error while iterating through results: %v\n", err)
 			return nil, err
 		}
 		resultMap[row.Tablename] = row.UnchangedToastColumns

flow/connectors/postgres/qrep_partition_test.go
@@ -104,9 +104,6 @@ func TestGetQRepPartitions(t *testing.T) {
 	// from 2010 Jan 1 10:00 AM UTC to 2010 Jan 30 10:00 AM UTC
 	numRows := prepareTestData(t, pool.Pool, schemaName)
 
-	secondsInADay := uint32(24 * time.Hour / time.Second)
-	fmt.Printf("secondsInADay: %d\n", secondsInADay)
-
 	// Define the test cases
 	testCases := []*testCase{
 		newTestCaseForNumRows(

flow/connectors/snowflake/avro_file_writer_test.go
@@ -145,7 +145,7 @@ func TestWriteRecordsToAvroFileHappyPath(t *testing.T) {
 	avroSchema, err := model.GetAvroSchemaDefinition("not_applicable", schema)
 	require.NoError(t, err)
 
-	fmt.Printf("[test] avroSchema: %v\n", avroSchema)
+	t.Logf("[test] avroSchema: %v", avroSchema)
 
 	// Call function
 	writer := avro.NewPeerDBOCFWriter(context.Background(),
@@ -173,7 +173,7 @@ func TestWriteRecordsToZstdAvroFileHappyPath(t *testing.T) {
 	avroSchema, err := model.GetAvroSchemaDefinition("not_applicable", schema)
 	require.NoError(t, err)
 
-	fmt.Printf("[test] avroSchema: %v\n", avroSchema)
+	t.Logf("[test] avroSchema: %v", avroSchema)
 
 	// Call function
 	writer := avro.NewPeerDBOCFWriter(context.Background(),
@@ -201,7 +201,7 @@ func TestWriteRecordsToDeflateAvroFileHappyPath(t *testing.T) {
 	avroSchema, err := model.GetAvroSchemaDefinition("not_applicable", schema)
 	require.NoError(t, err)
 
-	fmt.Printf("[test] avroSchema: %v\n", avroSchema)
+	t.Logf("[test] avroSchema: %v", avroSchema)
 
 	// Call function
 	writer := avro.NewPeerDBOCFWriter(context.Background(),
@@ -228,7 +228,7 @@ func TestWriteRecordsToAvroFileNonNull(t *testing.T) {
 	avroSchema, err := model.GetAvroSchemaDefinition("not_applicable", schema)
 	require.NoError(t, err)
 
-	fmt.Printf("[test] avroSchema: %v\n", avroSchema)
+	t.Logf("[test] avroSchema: %v", avroSchema)
 
 	// Call function
 	writer := avro.NewPeerDBOCFWriter(context.Background(),
@@ -256,7 +256,7 @@ func TestWriteRecordsToAvroFileAllNulls(t *testing.T) {
 	avroSchema, err := model.GetAvroSchemaDefinition("not_applicable", schema)
 	require.NoError(t, err)
 
-	fmt.Printf("[test] avroSchema: %v\n", avroSchema)
+	t.Logf("[test] avroSchema: %v", avroSchema)
 
 	// Call function
 	writer := avro.NewPeerDBOCFWriter(context.Background(),

commit 20ed25255353efa81ae93020362f24969b074c27

Fix e2e.RequireEqualRecordBatches (#967)
I introduced regression in #923

Function was returning bool while callers expect testify/require
semantics

---------

Co-authored-by: Amogh-Bharadwaj <amogh@peerdb.io>
Co-authored-by: Kevin Biju <kevin@peerdb.io>

flow/connectors/postgres/cdc.go
@@ -12,6 +12,7 @@ import (
 	"github.com/PeerDB-io/peer-flow/connectors/utils"
 	"github.com/PeerDB-io/peer-flow/connectors/utils/cdc_records"
 	"github.com/PeerDB-io/peer-flow/generated/protos"
+	"github.com/PeerDB-io/peer-flow/geo"
 	"github.com/PeerDB-io/peer-flow/model"
 	"github.com/PeerDB-io/peer-flow/model/qvalue"
 	"github.com/PeerDB-io/peer-flow/shared"
@@ -362,8 +363,8 @@ func (p *PostgresCDCSource) consumeStream(
 					if retryAttemptForWALSegmentRemoved > maxRetriesForWalSegmentRemoved {
 						return fmt.Errorf("max retries for WAL segment removed exceeded: %+v", errMsg)
 					} else {
-						p.logger.Warn(fmt.Sprintf(
-							"WAL segment removed, restarting replication retrying in 30 seconds..."),
+						p.logger.Warn(
+							"WAL segment removed, restarting replication retrying in 30 seconds...",
 							slog.Any("error", errMsg), slog.Int("retryAttempt", retryAttemptForWALSegmentRemoved))
 						time.Sleep(30 * time.Second)
 						continue
@@ -761,7 +762,7 @@ func (p *PostgresCDCSource) decodeColumnData(data []byte, dataType uint32, forma
 	if ok {
 		customQKind := customTypeToQKind(typeName)
 		if customQKind == qvalue.QValueKindGeography || customQKind == qvalue.QValueKindGeometry {
-			wkt, err := GeoValidate(string(data))
+			wkt, err := geo.GeoValidate(string(data))
 			if err != nil {
 				return qvalue.QValue{
 					Kind:  customQKind,

flow/connectors/postgres/qrep_query_executor.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/PeerDB-io/peer-flow/connectors/utils"
+	"github.com/PeerDB-io/peer-flow/geo"
 	"github.com/PeerDB-io/peer-flow/model"
 	"github.com/PeerDB-io/peer-flow/model/qvalue"
 	"github.com/PeerDB-io/peer-flow/shared"
@@ -446,7 +447,7 @@ func mapRowToQRecord(row pgx.Rows, fds []pgconn.FieldDescription,
 			customQKind := customTypeToQKind(typeName)
 			if customQKind == qvalue.QValueKindGeography || customQKind == qvalue.QValueKindGeometry {
 				wkbString, ok := values[i].(string)
-				wkt, err := GeoValidate(wkbString)
+				wkt, err := geo.GeoValidate(wkbString)
 				if err != nil || !ok {
 					values[i] = nil
 				} else {

flow/connectors/postgres/qvalue_convert.go
@@ -1,7 +1,6 @@
 package connpostgres
 
 import (
-	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -14,8 +13,6 @@ import (
 	"github.com/PeerDB-io/peer-flow/model/qvalue"
 	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/lib/pq/oid"
-
-	geom "github.com/twpayne/go-geos"
 )
 
 func postgresOIDToQValueKind(recvOID uint32) qvalue.QValueKind {
@@ -406,29 +403,4 @@ func customTypeToQKind(typeName string) qvalue.QValueKind {
 		qValueKind = qvalue.QValueKindString
 	}
 	return qValueKind
-}
-
-// returns the WKT representation of the geometry object if it is valid
-func GeoValidate(hexWkb string) (string, error) {
-	// Decode the WKB hex string into binary
-	wkb, hexErr := hex.DecodeString(hexWkb)
-	if hexErr != nil {
-		slog.Warn(fmt.Sprintf("Ignoring invalid WKB: %s", hexWkb))
-		return "", hexErr
-	}
-
-	// UnmarshalWKB performs geometry validation along with WKB parsing
-	geometryObject, geoErr := geom.NewGeomFromWKB(wkb)
-	if geoErr != nil {
-		return "", geoErr
-	}
-
-	invalidReason := geometryObject.IsValidReason()
-	if invalidReason != "Valid Geometry" {
-		slog.Warn(fmt.Sprintf("Ignoring invalid geometry shape %s: %s", hexWkb, invalidReason))
-		return "", errors.New(invalidReason)
-	}
-
-	wkt := geometryObject.ToWKT()
-	return wkt, nil
 }

flow/e2eshared/e2eshared.go
@@ -51,14 +51,52 @@ func CheckQRecordEquality(t *testing.T, q model.QRecord, other model.QRecord) bo
 	t.Helper()
 
 	if q.NumEntries != other.NumEntries {
-		t.Logf("unequal entry count: %d != %d\n", q.NumEntries, other.NumEntries)
+		t.Logf("unequal entry count: %d != %d", q.NumEntries, other.NumEntries)
 		return false
 	}
 
 	for i, entry := range q.Entries {
 		otherEntry := other.Entries[i]
 		if !entry.Equals(otherEntry) {
-			t.Logf("entry %d: %v != %v\n", i, entry, otherEntry)
+			t.Logf("entry %d: %v != %v", i, entry, otherEntry)
+			return false
+		}
+	}
+
+	return true
+}
+
+// Equals checks if two QRecordBatches are identical.
+func CheckEqualRecordBatches(t *testing.T, q *model.QRecordBatch, other *model.QRecordBatch) bool {
+	t.Helper()
+
+	if q == nil || other == nil {
+		t.Logf("q nil? %v, other nil? %v", q == nil, other == nil)
+		return q == nil && other == nil
+	}
+
+	// First check simple attributes
+	if q.NumRecords != other.NumRecords {
+		// print num records
+		t.Logf("q.NumRecords: %d", q.NumRecords)
+		t.Logf("other.NumRecords: %d", other.NumRecords)
+		return false
+	}
+
+	// Compare column names
+	if !q.Schema.EqualNames(other.Schema) {
+		t.Log("Column names are not equal")
+		t.Logf("Schema 1: %v", q.Schema.GetColumnNames())
+		t.Logf("Schema 2: %v", other.Schema.GetColumnNames())
+		return false
+	}
+
+	// Compare records
+	for i, record := range q.Records {
+		if !CheckQRecordEquality(t, record, other.Records[i]) {
+			t.Logf("Record %d is not equal", i)
+			t.Logf("Record 1: %v", record)
+			t.Logf("Record 2: %v", other.Records[i])
 			return false
 		}
 	}

flow/geo/geo.go
@@ -1,0 +1,51 @@
+//nolint:all
+package geo
+
+import (
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"log/slog"
+
+	geom "github.com/twpayne/go-geos"
+)
+
+// returns the WKT representation of the geometry object if it is valid
+func GeoValidate(hexWkb string) (string, error) {
+	// Decode the WKB hex string into binary
+	wkb, hexErr := hex.DecodeString(hexWkb)
+	if hexErr != nil {
+		slog.Warn(fmt.Sprintf("Ignoring invalid WKB: %s", hexWkb))
+		return "", hexErr
+	}
+
+	// UnmarshalWKB performs geometry validation along with WKB parsing
+	geometryObject, geoErr := geom.NewGeomFromWKB(wkb)
+	if geoErr != nil {
+		return "", geoErr
+	}
+
+	invalidReason := geometryObject.IsValidReason()
+	if invalidReason != "Valid Geometry" {
+		slog.Warn(fmt.Sprintf("Ignoring invalid geometry shape %s: %s", hexWkb, invalidReason))
+		return "", errors.New(invalidReason)
+	}
+
+	wkt := geometryObject.ToWKT()
+	return wkt, nil
+}
+
+// compares WKTs
+func GeoCompare(wkt1, wkt2 string) bool {
+	geom1, geoErr := geom.NewGeomFromWKT(wkt1)
+	if geoErr != nil {
+		return false
+	}
+
+	geom2, geoErr := geom.NewGeomFromWKT(wkt2)
+	if geoErr != nil {
+		return false
+	}
+
+	return geom1.Equals(geom2)
+}

flow/model/qvalue/qvalue.go
@@ -9,6 +9,7 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/PeerDB-io/peer-flow/geo"
 	"github.com/google/uuid"
 )
 
@@ -203,8 +204,16 @@ func compareString(value1, value2 interface{}) bool {
 
 	str1, ok1 := value1.(string)
 	str2, ok2 := value2.(string)
+	if !ok1 || !ok2 {
+		return false
+	}
+	if str1 == str2 {
+		return true
+	}
 
-	return ok1 && ok2 && str1 == str2
+	// Catch matching WKB(in Postgres)-WKT(in destination) geo values
+	geoConvertedWKT, err := geo.GeoValidate(str1)
+	return err == nil && geo.GeoCompare(geoConvertedWKT, str2)
 }
 
 func compareStruct(value1, value2 interface{}) bool {
@@ -267,6 +276,10 @@ func compareNumericArrays(value1, value2 interface{}) bool {
 		return true
 	}
 
+	if value1 == nil && value2 == "" {
+		return true
+	}
+
 	// Helper function to convert a value to float64
 	convertToFloat64 := func(val interface{}) []float64 {
 		switch v := val.(type) {
@@ -318,6 +331,11 @@ func compareArrayString(value1, value2 interface{}) bool {
 
 	// also return true if value2 is string null
 	if value1 == nil && value2 == "null" {
+		return true
+	}
+
+	// nulls end up as empty 'variants' in snowflake
+	if value1 == nil && value2 == "" {
 		return true
 	}
 

flow/e2e/bigquery/qrep_flow_bq_test.go
@@ -1,10 +1,8 @@
 package e2e_bigquery
 
 import (
-	"context"
 	"fmt"
 
-	connpostgres "github.com/PeerDB-io/peer-flow/connectors/postgres"
 	"github.com/PeerDB-io/peer-flow/e2e"
 	"github.com/stretchr/testify/require"
 )
@@ -17,13 +15,7 @@ func (s PeerFlowE2ETestSuiteBQ) setupSourceTable(tableName string, rowCount int)
 }
 
 func (s PeerFlowE2ETestSuiteBQ) compareTableContentsBQ(tableName string, colsString string) {
-	// read rows from source table
-	pgQueryExecutor := connpostgres.NewQRepQueryExecutor(s.pool, context.Background(), "testflow", "testpart")
-	pgQueryExecutor.SetTestEnv(true)
-
-	pgRows, err := pgQueryExecutor.ExecuteAndProcessQuery(
-		fmt.Sprintf("SELECT %s FROM e2e_test_%s.%s ORDER BY id", colsString, s.bqSuffix, tableName),
-	)
+	pgRows, err := e2e.GetPgRows(s.pool, s.bqSuffix, tableName, colsString)
 	require.NoError(s.t, err)
 
 	// read rows from destination table
@@ -33,7 +25,7 @@ func (s PeerFlowE2ETestSuiteBQ) compareTableContentsBQ(tableName string, colsStr
 	bqRows, err := s.bqHelper.ExecuteAndProcessQuery(bqSelQuery)
 	require.NoError(s.t, err)
 
-	e2e.RequireEqualRecordBatchs(s.t, pgRows, bqRows)
+	e2e.RequireEqualRecordBatches(s.t, pgRows, bqRows)
 }
 
 func (s PeerFlowE2ETestSuiteBQ) Test_Complete_QRep_Flow_Avro() {

flow/e2e/snowflake/qrep_flow_sf_test.go
@@ -1,10 +1,8 @@
 package e2e_snowflake
 
 import (
-	"context"
 	"fmt"
 
-	connpostgres "github.com/PeerDB-io/peer-flow/connectors/postgres"
 	"github.com/PeerDB-io/peer-flow/e2e"
 	"github.com/PeerDB-io/peer-flow/generated/protos"
 	"github.com/google/uuid"
@@ -17,10 +15,6 @@ func (s PeerFlowE2ETestSuiteSF) setupSourceTable(tableName string, numRows int) 
 	require.NoError(s.t, err)
 	err = e2e.PopulateSourceTable(s.pool, s.pgSuffix, tableName, numRows)
 	require.NoError(s.t, err)
-}
-
-func (s PeerFlowE2ETestSuiteSF) compareTableContentsSF(tableName, selector string) {
-	s.compareTableContentsWithDiffSelectorsSF(tableName, selector, selector, false)
 }
 
 func (s PeerFlowE2ETestSuiteSF) checkJSONValue(tableName, colName, fieldName, value string) error {
@@ -38,19 +32,17 @@ func (s PeerFlowE2ETestSuiteSF) checkJSONValue(tableName, colName, fieldName, va
 	return nil
 }
 
+func (s PeerFlowE2ETestSuiteSF) compareTableContentsSF(tableName, selector string) {
+	s.compareTableContentsWithDiffSelectorsSF(tableName, selector, selector, false)
+}
+
 func (s PeerFlowE2ETestSuiteSF) compareTableContentsWithDiffSelectorsSF(tableName, pgSelector, sfSelector string,
 	tableCaseSensitive bool,
 ) {
-	// read rows from source table
-	pgQueryExecutor := connpostgres.NewQRepQueryExecutor(s.pool, context.Background(), "testflow", "testpart")
-	pgQueryExecutor.SetTestEnv(true)
-	pgRows, err := pgQueryExecutor.ExecuteAndProcessQuery(
-		fmt.Sprintf(`SELECT %s FROM e2e_test_%s."%s" ORDER BY id`, pgSelector, s.pgSuffix, tableName),
-	)
+	pgRows, err := e2e.GetPgRows(s.pool, s.pgSuffix, tableName, pgSelector)
 	require.NoError(s.t, err)
 
 	// read rows from destination table
-
 	var qualifiedTableName string
 	if tableCaseSensitive {
 		qualifiedTableName = fmt.Sprintf(`%s.%s."%s"`, s.sfHelper.testDatabaseName, s.sfHelper.testSchemaName, tableName)
@@ -59,11 +51,11 @@ func (s PeerFlowE2ETestSuiteSF) compareTableContentsWithDiffSelectorsSF(tableNam
 	}
 
 	sfSelQuery := fmt.Sprintf(`SELECT %s FROM %s ORDER BY id`, sfSelector, qualifiedTableName)
-	s.t.Logf("running query on snowflake: %s\n", sfSelQuery)
+	s.t.Logf("running query on snowflake: %s", sfSelQuery)
 	sfRows, err := s.sfHelper.ExecuteAndProcessQuery(sfSelQuery)
 	require.NoError(s.t, err)
 
-	e2e.RequireEqualRecordBatchs(s.t, pgRows, sfRows)
+	e2e.RequireEqualRecordBatches(s.t, pgRows, sfRows)
 }
 
 func (s PeerFlowE2ETestSuiteSF) Test_Complete_QRep_Flow_Avro_SF() {

flow/e2e/test_utils.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/PeerDB-io/peer-flow/activities"
+	connpostgres "github.com/PeerDB-io/peer-flow/connectors/postgres"
 	connsnowflake "github.com/PeerDB-io/peer-flow/connectors/snowflake"
 	utils "github.com/PeerDB-io/peer-flow/connectors/utils/catalog"
 	"github.com/PeerDB-io/peer-flow/e2eshared"
@@ -23,6 +24,7 @@ import (
 	peerflow "github.com/PeerDB-io/peer-flow/workflows"
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/stretchr/testify/require"
 	"go.temporal.io/sdk/testsuite"
 )
 
@@ -56,6 +58,15 @@ func RegisterWorkflowsAndActivities(t *testing.T, env *testsuite.TestWorkflowEnv
 		Alerter:     alerter,
 	})
 	env.RegisterActivity(&activities.SnapshotActivity{})
+}
+
+func GetPgRows(pool *pgxpool.Pool, suffix string, tableName string, cols string) (*model.QRecordBatch, error) {
+	pgQueryExecutor := connpostgres.NewQRepQueryExecutor(pool, context.Background(), "testflow", "testpart")
+	pgQueryExecutor.SetTestEnv(true)
+
+	return pgQueryExecutor.ExecuteAndProcessQuery(
+		fmt.Sprintf(`SELECT %s FROM e2e_test_%s."%s" ORDER BY id`, cols, suffix, tableName),
+	)
 }
 
 func SetupCDCFlowStatusQuery(env *testsuite.TestWorkflowEnvironment,
@@ -357,6 +368,12 @@ func GetOwnersSchema() *model.QRecordSchema {
 			{Name: "f6", Type: qvalue.QValueKindJSON, Nullable: true},
 			{Name: "f7", Type: qvalue.QValueKindJSON, Nullable: true},
 			{Name: "f8", Type: qvalue.QValueKindInt16, Nullable: true},
+			{Name: "geometryPoint", Type: qvalue.QValueKindGeometry, Nullable: true},
+			{Name: "geometry_linestring", Type: qvalue.QValueKindGeometry, Nullable: true},
+			{Name: "geometry_polygon", Type: qvalue.QValueKindGeometry, Nullable: true},
+			{Name: "geography_point", Type: qvalue.QValueKindGeography, Nullable: true},
+			{Name: "geography_linestring", Type: qvalue.QValueKindGeography, Nullable: true},
+			{Name: "geography_polygon", Type: qvalue.QValueKindGeography, Nullable: true},
 		},
 	}
 }
@@ -367,7 +384,16 @@ func GetOwnersSelectorStringsSF() [2]string {
 	sfFields := make([]string, 0, len(schema.Fields))
 	for _, field := range schema.Fields {
 		pgFields = append(pgFields, fmt.Sprintf(`"%s"`, field.Name))
-		sfFields = append(sfFields, connsnowflake.SnowflakeIdentifierNormalize(field.Name))
+		if strings.Contains(field.Name, "geo") {
+			colName := connsnowflake.SnowflakeIdentifierNormalize(field.Name)
+
+			// Have to apply a WKT transformation here,
+			// else the sql driver we use receives the values as snowflake's OBJECT
+			// which is troublesome to deal with. Now it receives it as string.
+			sfFields = append(sfFields, fmt.Sprintf(`ST_ASWKT(%s) as %s`, colName, colName))
+		} else {
+			sfFields = append(sfFields, connsnowflake.SnowflakeIdentifierNormalize(field.Name))
+		}
 	}
 	return [2]string{strings.Join(pgFields, ","), strings.Join(sfFields, ",")}
 }
@@ -419,40 +445,7 @@ func (l *TStructuredLogger) Error(msg string, keyvals ...interface{}) {
 	l.logger.With(l.keyvalsToFields(keyvals)).Error(msg)
 }
 
-// Equals checks if two QRecordBatches are identical.
-func RequireEqualRecordBatchs(t *testing.T, q *model.QRecordBatch, other *model.QRecordBatch) bool {
+func RequireEqualRecordBatches(t *testing.T, q *model.QRecordBatch, other *model.QRecordBatch) {
 	t.Helper()
-
-	if other == nil {
-		t.Log("other is nil")
-		return q == nil
-	}
-
-	// First check simple attributes
-	if q.NumRecords != other.NumRecords {
-		// print num records
-		t.Logf("q.NumRecords: %d", q.NumRecords)
-		t.Logf("other.NumRecords: %d", other.NumRecords)
-		return false
-	}
-
-	// Compare column names
-	if !q.Schema.EqualNames(other.Schema) {
-		t.Log("Column names are not equal")
-		t.Logf("Schema 1: %v", q.Schema.GetColumnNames())
-		t.Logf("Schema 2: %v", other.Schema.GetColumnNames())
-		return false
-	}
-
-	// Compare records
-	for i, record := range q.Records {
-		if !e2eshared.CheckQRecordEquality(t, record, other.Records[i]) {
-			t.Logf("Record %d is not equal", i)
-			t.Logf("Record 1: %v", record)
-			t.Logf("Record 2: %v", other.Records[i])
-			return false
-		}
-	}
-
-	return true
+	require.True(t, e2eshared.CheckEqualRecordBatches(t, q, other))
 }