596b531907e4cd65831c35aa89e0e660af607888
Amogh-Bharadwaj <amogh@peerdb.io>
2024-01-11 14:35:13 +0530

    add type in alert-config backend

--- ui/app/api/alert-config/route.ts
+++ ui/app/api/alert-config/route.ts
@@ -1,8 +1,9 @@
 import { alertConfigType } from '@/app/alert-config/validation';
 import prisma from '@/app/utils/prisma';
+import { alerting_config } from '@prisma/client';
 
 export async function GET() {
-  const configs = await prisma.alerting_config.findMany();
+  const configs: alerting_config[] = await prisma.alerting_config.findMany();
   const serializedConfigs = configs.map((config) => ({
     ...config,
     id: String(config.id),
