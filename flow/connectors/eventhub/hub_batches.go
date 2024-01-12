637c9c64c7eaf13dfcd140191f690aec7b6fc640
Amogh Bharadwaj <amogh@peerdb.io>
2024-01-12 06:03:11 +0530

    Eventhub: remove parallelism, move recordheartbeat, use atomic (#1061)
    - Remove push parallelism
    - Move heartbeat routine inside processBatch
    - use atomic int for num records
    
    ---------
    
    Co-authored-by: Philip Dubé <serprex@users.noreply.github.com>

--- flow/connectors/eventhub/hub_batches.go
+++ flow/connectors/eventhub/hub_batches.go
@@ -10,7 +10,6 @@ import (
 
 	azeventhubs "github.com/Azure/azure-sdk-for-go/sdk/messaging/azeventhubs"
 	"github.com/PeerDB-io/peer-flow/shared"
-	"golang.org/x/sync/errgroup"
 )
 
 // multimap from ScopedEventhub to *azeventhubs.EventDataBatch
@@ -76,10 +75,14 @@ func (h *HubBatches) Len() int {
 }
 
 // ForEach calls the given function for each ScopedEventhub and batch pair
-func (h *HubBatches) ForEach(fn func(ScopedEventhub, *azeventhubs.EventDataBatch)) {
-	for destination, batch := range h.batch {
-		fn(destination, batch)
+func (h *HubBatches) ForEach(fn func(ScopedEventhub, *azeventhubs.EventDataBatch) error) error {
+	for name, batch := range h.batch {
+		err := fn(name, batch)
+		if err != nil {
+			return err
+		}
 	}
+	return nil
 }
 
 func (h *HubBatches) sendBatch(
@@ -108,7 +111,6 @@ func (h *HubBatches) sendBatch(
 
 func (h *HubBatches) flushAllBatches(
 	ctx context.Context,
-	maxParallelism int64,
 	flowName string,
 ) error {
 	if h.Len() == 0 {
@@ -116,34 +118,36 @@ func (h *HubBatches) flushAllBatches(
 		return nil
 	}
 
-	var numEventsPushed int32
-	g, gCtx := errgroup.WithContext(ctx)
-	g.SetLimit(int(maxParallelism))
-	h.ForEach(func(destination ScopedEventhub, eventBatch *azeventhubs.EventDataBatch) {
-		g.Go(func() error {
+	var numEventsPushed atomic.Int32
+	err := h.ForEach(
+		func(
+			destination ScopedEventhub,
+			eventBatch *azeventhubs.EventDataBatch,
+		) error {
 			numEvents := eventBatch.NumEvents()
-			err := h.sendBatch(gCtx, destination, eventBatch)
+			err := h.sendBatch(ctx, destination, eventBatch)
 			if err != nil {
 				return err
 			}
 
-			atomic.AddInt32(&numEventsPushed, numEvents)
+			numEventsPushed.Add(numEvents)
 			slog.Info("flushAllBatches",
 				slog.String(string(shared.FlowNameKey), flowName),
 				slog.Int("events sent", int(numEvents)),
 				slog.String("event hub topic ", destination.ToString()))
 			return nil
 		})
-	})
 
-	err := g.Wait()
-	slog.Info("hub batches flush",
-		slog.String(string(shared.FlowNameKey), flowName),
-		slog.Int("events sent", int(numEventsPushed)))
-
-	// clear the batches after flushing them.
 	h.Clear()
 
+	if err != nil {
+		return fmt.Errorf("failed to flushAllBatches: %v", err)
+	}
+	slog.Info("hub batches flush",
+		slog.String(string(shared.FlowNameKey), flowName),
+		slog.Int("events sent", int(numEventsPushed.Load())))
+
+	// clear the batches after flushing them.
 	return err
 }
 
