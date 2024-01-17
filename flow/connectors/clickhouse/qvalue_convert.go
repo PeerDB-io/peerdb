43070e89ca3dd674eacdb2b97b9ff968a78dcb80
Pankaj B <pankaj@peerdb.io>
2024-01-17 20:26:41 +0530

    cleanup remove unused code

--- flow/connectors/clickhouse/qvalue_convert.go
+++ flow/connectors/clickhouse/qvalue_convert.go
@@ -39,19 +39,3 @@ var clickhouseTypeToQValueKindMap = map[string]qvalue.QValueKind{
 	"Array(Int64)":   qvalue.QValueKindArrayInt64,
 	"Array(Float64)": qvalue.QValueKindArrayFloat64,
 }
-
-// func qValueKindToClickhouseType(colType qvalue.QValueKind) (string, error) {
-// 	val, err := colType.ToDWHColumnType(qvalue.QDWHTypeClickhouse)
-// 	if err != nil {
-// 		return "", err
-// 	}
-
-// 	return val, err
-// }
-
-// func clickhouseTypeToQValueKind(name string) (qvalue.QValueKind, error) {
-// 	if val, ok := clickhouseTypeToQValueKindMap[name]; ok {
-// 		return val, nil
-// 	}
-// 	return "", fmt.Errorf("unsupported database type name: %s", name)
-// }
