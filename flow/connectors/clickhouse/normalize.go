f4620ba5d104d91e00541a52223fb28918f5b44f
Kaushik Iska <iska.kaushik@gmail.com>
2024-01-22 06:25:30 -0700

    fix normalize errors

--- flow/connectors/clickhouse/normalize.go
+++ flow/connectors/clickhouse/normalize.go
@@ -2,16 +2,12 @@ package connclickhouse
 
 import (
 	"fmt"
-	"log/slog"
 	"strings"
-	"sync/atomic"
-	"time"
 
 	"github.com/PeerDB-io/peer-flow/connectors/utils"
 	"github.com/PeerDB-io/peer-flow/generated/protos"
 	"github.com/PeerDB-io/peer-flow/model"
 	"github.com/PeerDB-io/peer-flow/model/qvalue"
-	"golang.org/x/sync/errgroup"
 )
 
 const (
@@ -113,8 +109,10 @@ func generateCreateTableSQLForNormalizedTable(
 func (c *ClickhouseConnector) NormalizeRecords(req *model.NormalizeRecordsRequest) (*model.NormalizeResponse, error) {
 	batchIDs, err := c.GetLastSyncAndNormalizeBatchID(req.FlowJobName)
 	if err != nil {
+		c.logger.ErrorContext(c.ctx, "[sf] error while getting last sync and normalize batch id", err)
 		return nil, err
 	}
+
 	// normalize has caught up with sync, chill until more records are loaded.
 	if batchIDs.NormalizeBatchID >= batchIDs.SyncBatchID {
 		return &model.NormalizeResponse{
@@ -124,96 +122,23 @@ func (c *ClickhouseConnector) NormalizeRecords(req *model.NormalizeRecordsReques
 		}, nil
 	}
 
-	jobMetadataExists, err := c.jobMetadataExists(req.FlowJobName)
-	if err != nil {
-		return nil, err
-	}
-	// sync hasn't created job metadata yet, chill.
-	if !jobMetadataExists {
-		return &model.NormalizeResponse{
-			Done: false,
-		}, nil
-	}
-	destinationTableNames, err := c.getDistinctTableNamesInBatch(
-		req.FlowJobName,
-		batchIDs.SyncBatchID,
-		batchIDs.NormalizeBatchID,
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	tableNametoUnchangedToastCols, err := c.getTableNametoUnchangedCols(req.FlowJobName, batchIDs.SyncBatchID, batchIDs.NormalizeBatchID)
-	if err != nil {
-		return nil, fmt.Errorf("couldn't tablename to unchanged cols mapping: %w", err)
-	}
-
-	var totalRowsAffected int64 = 0
-	g, gCtx := errgroup.WithContext(c.ctx)
-	g.SetLimit(8) // limit parallel merges to 8
-
-	for _, destinationTableName := range destinationTableNames {
-		tableName := destinationTableName // local variable for the closure
-
-		g.Go(func() error {
-			mergeGen := &mergeStmtGenerator{
-				rawTableName:          getRawTableIdentifier(req.FlowJobName),
-				dstTableName:          tableName,
-				syncBatchID:           batchIDs.SyncBatchID,
-				normalizeBatchID:      batchIDs.NormalizeBatchID,
-				normalizedTableSchema: req.TableNameSchemaMapping[tableName],
-				unchangedToastColumns: tableNametoUnchangedToastCols[tableName],
-				peerdbCols: &protos.PeerDBColumns{
-					SoftDelete:        req.SoftDelete,
-					SoftDeleteColName: req.SoftDeleteColName,
-					SyncedAtColName:   req.SyncedAtColName,
-				},
-			}
-			mergeStatement, err := mergeGen.generateMergeStmt()
-			if err != nil {
-				return err
-			}
-
-			startTime := time.Now()
-			c.logger.Info("[merge] merging records...", slog.String("destTable", tableName))
-
-			result, err := c.database.ExecContext(gCtx, mergeStatement, tableName)
-			if err != nil {
-				return fmt.Errorf("failed to merge records into %s (statement: %s): %w",
-					tableName, mergeStatement, err)
-			}
-
-			endTime := time.Now()
-			c.logger.Info(fmt.Sprintf("[merge] merged records into %s, took: %d seconds",
-				tableName, endTime.Sub(startTime)/time.Second))
-			if err != nil {
-				c.logger.Error("[merge] error while normalizing records", slog.Any("error", err))
-				return err
-			}
-
-			rowsAffected, err := result.RowsAffected()
-			if err != nil {
-				return fmt.Errorf("failed to get rows affected by merge statement for table %s: %w", tableName, err)
-			}
-
-			atomic.AddInt64(&totalRowsAffected, rowsAffected)
-			return nil
-		})
-	}
-
-	if err := g.Wait(); err != nil {
-		return nil, fmt.Errorf("error while normalizing records: %w", err)
-	}
-
-	// updating metadata with new normalizeBatchID
-	err = c.updateNormalizeMetadata(req.FlowJobName, batchIDs.SyncBatchID)
-	if err != nil {
-		return nil, err
-	}
-
+	// This will never happen.
 	return &model.NormalizeResponse{
 		Done:         true,
 		StartBatchID: batchIDs.NormalizeBatchID + 1,
 		EndBatchID:   batchIDs.SyncBatchID,
+	}, nil
+}
+
+func (c *ClickhouseConnector) GetLastSyncAndNormalizeBatchID(flowJobName string) (model.SyncAndNormalizeBatchID, error) {
+	// return sync batch id as the normalize batch id as well as this is a no-op.
+	batchId, err := c.pgMetadata.GetLastBatchID(flowJobName)
+	if err != nil {
+		return model.SyncAndNormalizeBatchID{}, err
+	}
+
+	return model.SyncAndNormalizeBatchID{
+		SyncBatchID:      batchId,
+		NormalizeBatchID: batchId,
 	}, nil
 }
