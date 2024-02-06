9dca0bb8bdd2bd219a7393b41fb74dfdc3727451
Amogh-Bharadwaj <amogh@peerdb.io>
2024-02-06 14:26:21 +0530

    Revert "Eventhubs: Refactor Destination Table Semantics (#1200)"
    This reverts commit f9fa222ea2bda151d2ca52940c5e90416af9e0bd.

--- flow/connectors/eventhub/scoped_eventhub.go
+++ flow/connectors/eventhub/scoped_eventhub.go
@@ -9,30 +9,25 @@ import (
 // partition_column is the column in the table that is used to determine
 // the partition key for the eventhub. Partition value is one such value of that column.
 type ScopedEventhub struct {
-	EventhubNamespace  string
+	PeerName           string
 	Eventhub           string
 	PartitionKeyColumn string
 	PartitionKeyValue  string
 }
 
 func NewScopedEventhub(dstTableName string) (ScopedEventhub, error) {
-	// split by dot, the model is eventhub.eventhub_namespace.table_name.partition_key_column
+	// split by dot, the model is peername.eventhub.partition_key_column
 	parts := strings.Split(dstTableName, ".")
 
-	if len(parts) != 4 {
+	if len(parts) != 3 {
 		return ScopedEventhub{}, fmt.Errorf("invalid scoped eventhub '%s'", dstTableName)
 	}
 
-	// support eventhub namespace, eventhub name, partition key with hyphens etc.
-	// part[2] will be some table identifier.
-	// It's just so that we have distinct destination table names
-	// in create mirror's table mapping.
-	// We can ignore it.
-	eventhubPart := strings.Trim(parts[0], `"`)
-	namespacePart := strings.Trim(parts[1], `"`)
-	partitionPart := strings.Trim(parts[3], `"`)
+	// support eventhub name and partition key with hyphens etc.
+	eventhubPart := strings.Trim(parts[1], `"`)
+	partitionPart := strings.Trim(parts[2], `"`)
 	return ScopedEventhub{
-		EventhubNamespace:  namespacePart,
+		PeerName:           parts[0],
 		Eventhub:           eventhubPart,
 		PartitionKeyColumn: partitionPart,
 	}, nil
@@ -42,7 +37,14 @@ func (s *ScopedEventhub) SetPartitionValue(value string) {
 	s.PartitionKeyValue = value
 }
 
+func (s ScopedEventhub) Equals(other ScopedEventhub) bool {
+	return s.PeerName == other.PeerName &&
+		s.Eventhub == other.Eventhub &&
+		s.PartitionKeyColumn == other.PartitionKeyColumn &&
+		s.PartitionKeyValue == other.PartitionKeyValue
+}
+
 // ToString returns the string representation of the ScopedEventhub
 func (s ScopedEventhub) ToString() string {
-	return fmt.Sprintf("%s.%s.%s.%s", s.EventhubNamespace, s.Eventhub, s.PartitionKeyColumn, s.PartitionKeyValue)
+	return fmt.Sprintf("%s.%s.%s.%s", s.PeerName, s.Eventhub, s.PartitionKeyColumn, s.PartitionKeyValue)
 }
