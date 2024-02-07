d1ea7456608aae13f3de005586379ff9893298b6
Kaushik Iska <iska.kaushik@gmail.com>
2024-02-07 11:58:15 -0800

    refactor heartbeats

--- flow/connectors/bigquery/bigquery.go
+++ flow/connectors/bigquery/bigquery.go
@@ -9,6 +9,7 @@ import (
 	"reflect"
 	"regexp"
 	"strings"
+	"sync/atomic"
 	"time"
 
 	"cloud.google.com/go/bigquery"
@@ -630,6 +631,15 @@ func (c *BigQueryConnector) CreateRawTable(req *protos.CreateRawTableInput) (*pr
 func (c *BigQueryConnector) SetupNormalizedTables(
 	req *protos.SetupNormalizedTableBatchInput,
 ) (*protos.SetupNormalizedTableBatchOutput, error) {
+	numTablesSetup := atomic.Uint32{}
+	totalTables := uint32(len(req.TableNameSchemaMapping))
+
+	shutdown := utils.HeartbeatRoutine(c.ctx, func() string {
+		return fmt.Sprintf("setting up normalized tables - %d of %d done",
+			numTablesSetup.Load(), totalTables)
+	})
+	defer shutdown()
+
 	tableExistsMapping := make(map[string]bool)
 	datasetTablesSet := make(map[datasetTable]struct{})
 	for tableIdentifier, tableSchema := range req.TableNameSchemaMapping {
@@ -738,7 +748,7 @@ func (c *BigQueryConnector) SetupNormalizedTables(
 		datasetTablesSet[*datasetTable] = struct{}{}
 		// log that table was created
 		c.logger.Info(fmt.Sprintf("created table %s", tableIdentifier))
-		utils.RecordHeartbeat(c.ctx, fmt.Sprintf("created table %s", tableIdentifier))
+		numTablesSetup.Add(1)
 	}
 
 	return &protos.SetupNormalizedTableBatchOutput{
