b704744ce59fedaab24a74eaaf9f2e701cf62a56
Amogh-Bharadwaj <amogh@peerdb.io>
2024-02-16 05:21:49 +0530

    replace another occurrence

--- flow/connectors/postgres/validate.go
+++ flow/connectors/postgres/validate.go
@@ -35,7 +35,7 @@ func (c *PostgresConnector) CheckSourceTables(ctx context.Context,
 
 	tableStr := strings.Join(tableArr, ",")
 	// Check if publication exists
-	err := c.conn.QueryRow(ctx, "SELECT pubname FROM pg_publication WHERE pubname=$1", pubName).Scan(nil)
+	err := c.conn.QueryRow(ctx, "SELECT pubname FROM pg_publication WHERE pubname="+QuoteLiteral(pubName)).Scan(nil)
 	if err != nil {
 		if err == pgx.ErrNoRows {
 			return fmt.Errorf("publication does not exist: %s", pubName)
