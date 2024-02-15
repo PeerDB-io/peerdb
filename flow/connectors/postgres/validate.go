c8e89a495f5efe86af7c014d29840db2c45df74e
Amogh-Bharadwaj <amogh@peerdb.io>
2024-02-15 20:58:38 +0530

    Revert "tx.Conn.Close() for safety"
    This reverts commit 7b707b0374b75be93adfb0e1812e1a31b905e0e4.

--- flow/connectors/postgres/validate.go
+++ flow/connectors/postgres/validate.go
@@ -127,7 +127,6 @@ func (c *PostgresConnector) CheckPublicationPermission(ctx context.Context, tabl
 	if err != nil {
 		return fmt.Errorf("error starting transaction: %w", err)
 	}
-	defer tx.Conn().Close(ctx)
 	defer func() {
 		err := tx.Rollback(ctx)
 		if err != nil && err != pgx.ErrTxClosed {
