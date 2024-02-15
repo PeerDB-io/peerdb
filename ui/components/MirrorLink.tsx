4207410c0967270b2f7f69188ac32f7d01f24da2
Philip Dubé <philip@peerdb.io>
2024-02-15 04:03:17 +0000

    ui: more accurately call isSetLoading when clicking mirror
    Before clicking on div, not link, would cause throbber without loading mirror

--- ui/components/MirrorLink.tsx
+++ ui/components/MirrorLink.tsx
@@ -2,16 +2,24 @@
 import { Label } from '@/lib/Label';
 import { ProgressCircle } from '@/lib/ProgressCircle';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
 const MirrorLink = ({ flowName }: { flowName: string }) => {
+  const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
   return (
-    <div onClick={() => setIsLoading(true)}>
+    <div>
       {isLoading ? (
         <ProgressCircle variant='determinate_progress_circle' />
       ) : (
-        <Link href={`/mirrors/${flowName}`}>
+        <Link
+          href={`/mirrors/${flowName}`}
+          onClick={() => {
+            setIsLoading(true);
+            router.push(`/mirrors/${flowName}`);
+          }}
+        >
           <Label>
             <div className='cursor-pointer underline'>{flowName}</div>
           </Label>
