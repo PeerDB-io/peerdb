d73839b75a4d76ad666d38c421c7063beef8d324
Amogh Bharadwaj <amogh@peerdb.io>
2024-02-27 20:13:15 +0530

    API: Add Access logging (#1384)
    We now log request and response metadata in our middleware.
    Request: Method, URL, IP, OS, Browser, Device
    Response: Status
    
    The two are linked by a UUID

--- ui/app/api/mirrors/state_change/route.ts
+++ ui/app/api/mirrors/state_change/route.ts
@@ -4,7 +4,7 @@ import { GetFlowHttpAddressFromEnv } from '@/rpc/http';
 export async function POST(request: Request) {
   const body = await request.json();
   const flowServiceAddr = GetFlowHttpAddressFromEnv();
-  console.log('/mirrors/state_change: req:', body);
+
   try {
     const res: FlowStateChangeResponse = await fetch(
       `${flowServiceAddr}/v1/mirrors/state_change`,
