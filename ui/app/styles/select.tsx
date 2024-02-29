5efb50e9bc2a497e8caf338ca8bad0f8c93abab5
Amogh-Bharadwaj <amogh@peerdb.io>
2024-02-29 22:45:12 +0530

    minor change

--- ui/app/styles/select.tsx
+++ ui/app/styles/select.tsx
@@ -4,8 +4,8 @@ const SelectTheme = (theme: Theme) => ({
   ...theme,
   colors: {
     ...theme.colors,
-    primary25: 'rgba(48, 164, 108, 0.5)',
-    primary: 'rgba(48, 164, 108, 0.5)',
+    primary25: 'rgba(48, 164, 108, 0.3)',
+    primary: 'rgba(48, 164, 108, 0.3)',
   },
 });
 
