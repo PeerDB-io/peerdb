0560fd064bf2ee5a3ce70b926533ebc87cc71786
Kevin Biju <kevin@peerdb.io>
2024-03-08 21:44:33 +0530

    test debug pt.8 - done

--- nexus/peer-snowflake/src/auth.rs
+++ nexus/peer-snowflake/src/auth.rs
@@ -96,7 +96,6 @@ impl SnowflakeAuth {
             "SHA256:{}",
             BASE64_STANDARD.encode(Sha256::new_with_prefix(public_key.as_bytes()).finalize())
         );
-        println!("{}", res);
         Ok(res)
     }
 
