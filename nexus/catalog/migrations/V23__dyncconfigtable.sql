fcd5f59ce8f913fac630ba43809ff9dbe5d082fc
Amogh-Bharadwaj <amogh@peerdb.io>
2024-03-20 00:19:40 +0530

    add some columns for the table

--- nexus/catalog/migrations/V23__dyncconfigtable.sql
+++ nexus/catalog/migrations/V23__dyncconfigtable.sql
@@ -1,1 +1,3 @@
 ALTER TABLE alerting_settings RENAME TO dynamic_settings;
+ALTER TABLE dynamic_settings ADD COLUMN setting_description TEXT;
+ALTER TABLE dynamic_settings ADD COLUMN needs_restart BOOLEAN;
