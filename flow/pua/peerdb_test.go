3282859d0d112f58a86395da0551bc845f23b7cc
Philip Dubé <philip@peerdb.io>
2024-03-21 19:00:18 +0000

    move glua64 tests to glua64

--- flow/pua/peerdb_test.go
+++ flow/pua/peerdb_test.go
@@ -25,14 +25,6 @@ func Test_Lua(t *testing.T) {
 	id := uuid.UUID([16]byte{2, 3, 5, 7, 11, 13, 17, 19, 127, 131, 137, 139, 149, 151, 241, 251})
 	ls.Env.RawSetString("uuid", LuaUuid.New(ls, id))
 
-	n5 := int64(-5)
-	ls.Env.RawSetString("i64p5", LuaI64.New(ls, 5))
-	ls.Env.RawSetString("i64p5_2", LuaI64.New(ls, 5))
-	ls.Env.RawSetString("u64p5", LuaU64.New(ls, 5))
-	ls.Env.RawSetString("u64p5_2", LuaU64.New(ls, 5))
-	ls.Env.RawSetString("i64n5", LuaI64.New(ls, n5))
-	ls.Env.RawSetString("u64n5", LuaU64.New(ls, uint64(n5)))
-
 	assert(t, ls, `
 assert(require('bit32').band(173, 21) == 5)
 assert(dofile == nil)
@@ -54,14 +46,5 @@ assert(uuid[12] == 149)
 assert(uuid[13] == 151)
 assert(uuid[14] == 241)
 assert(uuid[15] == 251)
-
-print(i64p5, u64p5)
-assert(i64p5 == u64p5)
-assert(i64p5 ~= i64n5)
-assert(i64n5 ~= u64n5)
-assert(i64p5 == i64p5_2)
-assert(u64p5 == u64p5_2)
-assert(u64n5 > i64p5)
-assert(i64p5 > i64n5)
 `)
 }
