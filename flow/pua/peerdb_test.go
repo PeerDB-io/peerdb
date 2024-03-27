d266931b15a43ea40fc43b4fb45cce3d18fbe3db
Philip Dubé <philip@peerdb.io>
2024-03-27 12:57:25 +0000

    Lua: msgpack v1.0.0, also add some constructors/comparisons
    Constructors parsing string: peerdb.UUID, peerdb.Decimal
    
    __eq for uuid
    
    __eq/__le/__lt for time, decimal, bigint
    
    Remove __len & integer indexing from bigint
    
    Serialize decimals as strings in msgpack

--- flow/pua/peerdb_test.go
+++ flow/pua/peerdb_test.go
@@ -3,7 +3,6 @@ package pua
 import (
 	"testing"
 
-	"github.com/google/uuid"
 	"github.com/yuin/gopher-lua"
 
 	"github.com/PeerDB-io/peer-flow/model"
@@ -29,14 +28,12 @@ func Test(t *testing.T) {
 	row.AddColumn("a", qvalue.QValueInt64{Val: 5040})
 	ls.Env.RawSetString("row", LuaRow.New(ls, row))
 
-	id := uuid.UUID([16]byte{2, 3, 5, 7, 11, 13, 17, 19, 127, 131, 137, 139, 149, 151, 241, 251})
-	ls.Env.RawSetString("uuid", LuaUuid.New(ls, id))
-
 	assert(t, ls, `
 assert(require('bit32').band(173, 21) == 5)
 assert(dofile == nil)
 assert(loadfile == nil)
 
+local uuid = peerdb.UUID("02030507-0b0d-1113-7f83-898b9597f1fb")
 assert(uuid[0] == 2)
 assert(uuid[1] == 3)
 assert(uuid[2] == 5)
@@ -53,6 +50,12 @@ assert(uuid[12] == 149)
 assert(uuid[13] == 151)
 assert(uuid[14] == 241)
 assert(uuid[15] == 251)
+
+local dec102 = peerdb.Decimal("10.2")
+local dec101 = peerdb.Decimal("10.1")
+assert(tostring(dec102) == "10.2")
+assert(dec101 < dec102)
+
 
 local msgpack = require "msgpack"
 assert(msgpack.encode(uuid) == string.char(0xc4, 16, 2, 3, 5, 7, 11, 13, 17, 19, 127, 131, 137, 139, 149, 151, 241, 251))
