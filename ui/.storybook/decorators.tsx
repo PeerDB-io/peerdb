1faf6a5c23274f339f0553fce21c278fafd39a72
Philip Dubé <serprex@users.noreply.github.com>
2024-03-28 07:42:43 +0000

    Remove storybook (#1551)
    It isn't be used & it makes it annoying to grep code

--- ui/.storybook/decorators.tsx
+++
@@ -1,8 +0,0 @@
-import { Decorator } from '@storybook/react';
-import { AppThemeProvider } from '../lib/AppTheme';
-
-export const withTheme: Decorator = (Story) => (
-  <AppThemeProvider>
-    <Story />
-  </AppThemeProvider>
-);
