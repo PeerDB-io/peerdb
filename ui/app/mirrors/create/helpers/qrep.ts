c500caa074f40699e9f5159050a5cb43ec801c54
Amogh Bharadwaj <amogh@peerdb.io>
2024-04-24 16:31:23 +0530

    fix setstate
    Co-authored-by: Philip Dubé <serprex@users.noreply.github.com>

--- ui/app/mirrors/create/helpers/qrep.ts
+++ ui/app/mirrors/create/helpers/qrep.ts
@@ -32,7 +32,7 @@ export const qrepSettings: MirrorSetting[] = [
     stateHandler: (value, setter) =>
       setter((curr: QRepConfig) => ({
         ...curr,
-        setupWatermarkTableOnDestination: (value as boolean) || true,
+        setupWatermarkTableOnDestination: value as boolean,
       })),
     tips: 'Specify if you want to create the watermark table on the destination as-is, can be used for some queries.',
     type: 'switch',
