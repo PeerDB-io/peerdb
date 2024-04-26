f8b5c02ecd13c666346169c04c930962e4185760
Amogh-Bharadwaj <amogh@peerdb.io>
2024-04-26 17:55:51 +0530

    import lint

--- flow/connectors/utils/aws.go
+++ flow/connectors/utils/aws.go
@@ -10,7 +10,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/PeerDB-io/peer-flow/logger"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -18,6 +17,8 @@ import (
 	"github.com/aws/aws-sdk-go-v2/credentials/stscreds"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	smithyendpoints "github.com/aws/smithy-go/endpoints"
+
+	"github.com/PeerDB-io/peer-flow/logger"
 )
 
 type AWSSecrets struct {
