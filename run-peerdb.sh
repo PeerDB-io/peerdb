05111666a80ce448bba8ace3bc16e06c3635a797
Kaushik Iska <iska.kaushik@gmail.com>
2024-05-03 13:26:48 -0400

    Improve default docker compose (#1669)
    use an explicit external network this removes environment related
    problems that can cause problems with quickstart.
    
    Also remove multiple flow workers in the default configuration

--- run-peerdb.sh
+++ run-peerdb.sh
@@ -7,5 +7,11 @@ then
     exit 1
 fi
 
+# check if peerdb_network exists if not create it
+if ! docker network inspect peerdb_network &> /dev/null
+then
+    docker network create peerdb_network
+fi
+
 docker compose pull
 docker compose -f docker-compose.yml up --no-attach catalog --no-attach temporal --no-attach temporal-ui --no-attach temporal-admin-tools
