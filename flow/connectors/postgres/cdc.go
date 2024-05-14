9fd951108bf64a02d74015a122444ffa75f01602
Amogh-Bharadwaj <amogh@peerdb.io>
2024-05-15 01:00:05 +0530

    use p.logger

--- flow/connectors/postgres/cdc.go
+++ flow/connectors/postgres/cdc.go
@@ -168,7 +168,7 @@ func (qProcessor) Process(
 		// bytea also appears here as a hex
 		data, err := p.decodeColumnData(tuple.Data, col.DataType, pgtype.TextFormatCode)
 		if err != nil {
-			slog.Error("error decoding text column data", slog.Any("error", err),
+			p.logger.Error("error decoding text column data", slog.Any("error", err),
 				slog.Any("columnName", col.Name))
 			return fmt.Errorf("error decoding text column data: %w", err)
 		}
