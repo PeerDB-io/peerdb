a358e2d8e64caa7eaeae02e0c8d26700bca2f422
Amogh-Bharadwaj <amogh@peerdb.io>
2024-05-14 16:47:08 +0530

    fix the wiring

--- ui/app/mirrors/[mirrorId]/handlers.ts
+++ ui/app/mirrors/[mirrorId]/handlers.ts
@@ -28,7 +28,6 @@ export const changeFlowState = async (
     sourcePeer: mirrorConfig.source,
     destinationPeer: mirrorConfig.destination,
     requestedFlowState: flowState,
-    customNumberOfSyncs: 0,
   };
   await fetch(`/api/mirrors/state_change`, {
     method: 'POST',
