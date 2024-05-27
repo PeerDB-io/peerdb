65dbf822f490eb3b3e671a641b438252b4759119
Amogh-Bharadwaj <amogh@peerdb.io>
2024-05-27 18:59:54 +0530

    refactoring and status endpoint

--- flow/cmd/custom_sync.go
+++ flow/cmd/custom_sync.go
@@ -3,16 +3,62 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"log/slog"
 
 	"github.com/PeerDB-io/peer-flow/generated/protos"
+	"github.com/PeerDB-io/peer-flow/peerdbenv"
 	peerflow "github.com/PeerDB-io/peer-flow/workflows"
+
+	"google.golang.org/grpc/metadata"
+	//sha256
+	"crypto/sha256"
 )
 
 const peerdbPauseGuideDocLink = "https://docs.peerdb.io/features/pause-mirror"
 
+func AuthenticateSyncRequest(ctx context.Context) error {
+	var values []string
+	var token string
+
+	md, ok := metadata.FromIncomingContext(ctx)
+	if ok {
+		values = md.Get("authorization")
+	}
+
+	if len(values) > 0 {
+		token = values[0]
+	}
+
+	deploymentUid := peerdbenv.PeerDBDeploymentUID()
+	hash := sha256.New()
+	_, err := hash.Write([]byte(deploymentUid))
+	if err != nil {
+		slog.Error("Server error: unable to verify authorization", slog.Any("error", err))
+		return fmt.Errorf("Server error: unable to verify authorization. Please try again.")
+	}
+
+	deploymentUidHashed := fmt.Sprintf("%x", hash.Sum(nil))
+	if token != "Bearer "+deploymentUidHashed {
+		slog.Error("Unauthorized: invalid authorization token", slog.String("token", token))
+		return fmt.Errorf("Unauthorized: invalid authorization token. Please check the token and try again.")
+	}
+
+	return nil
+}
+
 func (h *FlowRequestHandler) CustomSyncFlow(
 	ctx context.Context, req *protos.CreateCustomFlowRequest,
 ) (*protos.CreateCustomFlowResponse, error) {
+	err := AuthenticateSyncRequest(ctx)
+	if err != nil {
+		return &protos.CreateCustomFlowResponse{
+			FlowJobName:   req.FlowJobName,
+			NumberOfSyncs: 0,
+			ErrorMessage:  err.Error(),
+			Ok:            false,
+		}, nil
+	}
+
 	// ---- REQUEST VALIDATION ----
 	if req.FlowJobName == "" {
 		return &protos.CreateCustomFlowResponse{
@@ -24,6 +70,8 @@ func (h *FlowRequestHandler) CustomSyncFlow(
 	}
 
 	if req.NumberOfSyncs <= 0 || req.NumberOfSyncs > peerflow.MaxSyncsPerCdcFlow {
+		slog.Error("Invalid sync number request",
+			slog.Any("requested_number_of_syncs", req.NumberOfSyncs))
 		return &protos.CreateCustomFlowResponse{
 			FlowJobName:   req.FlowJobName,
 			NumberOfSyncs: 0,
@@ -35,6 +83,7 @@ func (h *FlowRequestHandler) CustomSyncFlow(
 
 	mirrorExists, err := h.CheckIfMirrorNameExists(ctx, req.FlowJobName)
 	if err != nil {
+		slog.Error("Server error: unable to check if mirror exists", slog.Any("error", err))
 		return &protos.CreateCustomFlowResponse{
 			FlowJobName:   req.FlowJobName,
 			NumberOfSyncs: 0,
@@ -43,6 +92,7 @@ func (h *FlowRequestHandler) CustomSyncFlow(
 		}, nil
 	}
 	if !mirrorExists {
+		slog.Error("Mirror does not exist", slog.Any("mirror_name", req.FlowJobName))
 		return &protos.CreateCustomFlowResponse{
 			FlowJobName:   req.FlowJobName,
 			NumberOfSyncs: 0,
@@ -55,6 +105,9 @@ func (h *FlowRequestHandler) CustomSyncFlow(
 		FlowJobName: req.FlowJobName,
 	})
 	if mirrorStatusResponse.ErrorMessage != "" {
+		slog.Error("Server error: unable to check the status of mirror",
+			slog.Any("mirror", req.FlowJobName),
+			slog.Any("error", mirrorStatusResponse.ErrorMessage))
 		return &protos.CreateCustomFlowResponse{
 			FlowJobName:   req.FlowJobName,
 			NumberOfSyncs: 0,
@@ -65,6 +118,7 @@ func (h *FlowRequestHandler) CustomSyncFlow(
 	}
 
 	if mirrorStatusResponse.CurrentFlowState != protos.FlowStatus_STATUS_PAUSED {
+		slog.Error("Mirror is not paused", slog.Any("mirror", req.FlowJobName))
 		return &protos.CreateCustomFlowResponse{
 			FlowJobName:   req.FlowJobName,
 			NumberOfSyncs: 0,
@@ -89,6 +143,9 @@ func (h *FlowRequestHandler) CustomSyncFlow(
 		},
 	})
 	if err != nil {
+		slog.Error("Unable to kick off custom sync for mirror",
+			slog.Any("mirror", req.FlowJobName),
+			slog.Any("error", err))
 		return &protos.CreateCustomFlowResponse{
 			FlowJobName:   req.FlowJobName,
 			NumberOfSyncs: 0,
@@ -97,6 +154,10 @@ func (h *FlowRequestHandler) CustomSyncFlow(
 			Ok: false,
 		}, nil
 	}
+
+	slog.Info("Custom sync started for mirror",
+		slog.String("mirror", req.FlowJobName),
+		slog.Int("number_of_syncs", int(req.NumberOfSyncs)))
 
 	return &protos.CreateCustomFlowResponse{
 		FlowJobName:   req.FlowJobName,
