36f928ddca10825ccaf54fdd767cedc70d935562
Kaushik Iska <iska.kaushik@gmail.com>
2024-05-28 10:29:33 -0400

    feat(cdc): reload peers on flow resume (#1763)
    Implement the reloadPeers function to support the edit peer
    functionality. When a flow is paused and resumed, reload the latest peer
    details for both the source and destination peers. This ensures that any
    changes made to the peer configuration during the pause are picked up
    when the flow resumes execution.

--- flow/cmd/handler.go
+++ flow/cmd/handler.go
@@ -15,6 +15,7 @@ import (
 	"google.golang.org/protobuf/proto"
 
 	"github.com/PeerDB-io/peer-flow/alerting"
+	"github.com/PeerDB-io/peer-flow/connectors/utils"
 	"github.com/PeerDB-io/peer-flow/generated/protos"
 	"github.com/PeerDB-io/peer-flow/model"
 	"github.com/PeerDB-io/peer-flow/shared"
@@ -516,110 +517,7 @@ func (h *FlowRequestHandler) CreatePeer(
 		}, nil
 	}
 
-	config := req.Peer.Config
-	wrongConfigResponse := &protos.CreatePeerResponse{
-		Status: protos.CreatePeerStatus_FAILED,
-		Message: fmt.Sprintf("invalid config for %s peer %s",
-			req.Peer.Type, req.Peer.Name),
-	}
-	var encodedConfig []byte
-	var encodingErr error
-	peerType := req.Peer.Type
-	switch peerType {
-	case protos.DBType_POSTGRES:
-		pgConfigObject, ok := config.(*protos.Peer_PostgresConfig)
-		if !ok {
-			return wrongConfigResponse, nil
-		}
-		pgConfig := pgConfigObject.PostgresConfig
-		encodedConfig, encodingErr = proto.Marshal(pgConfig)
-	case protos.DBType_SNOWFLAKE:
-		sfConfigObject, ok := config.(*protos.Peer_SnowflakeConfig)
-		if !ok {
-			return wrongConfigResponse, nil
-		}
-		sfConfig := sfConfigObject.SnowflakeConfig
-		encodedConfig, encodingErr = proto.Marshal(sfConfig)
-	case protos.DBType_BIGQUERY:
-		bqConfigObject, ok := config.(*protos.Peer_BigqueryConfig)
-		if !ok {
-			return wrongConfigResponse, nil
-		}
-		bqConfig := bqConfigObject.BigqueryConfig
-		encodedConfig, encodingErr = proto.Marshal(bqConfig)
-	case protos.DBType_SQLSERVER:
-		sqlServerConfigObject, ok := config.(*protos.Peer_SqlserverConfig)
-		if !ok {
-			return wrongConfigResponse, nil
-		}
-		sqlServerConfig := sqlServerConfigObject.SqlserverConfig
-		encodedConfig, encodingErr = proto.Marshal(sqlServerConfig)
-	case protos.DBType_S3:
-		s3ConfigObject, ok := config.(*protos.Peer_S3Config)
-		if !ok {
-			return wrongConfigResponse, nil
-		}
-		s3Config := s3ConfigObject.S3Config
-		encodedConfig, encodingErr = proto.Marshal(s3Config)
-	case protos.DBType_CLICKHOUSE:
-		chConfigObject, ok := config.(*protos.Peer_ClickhouseConfig)
-		if !ok {
-			return wrongConfigResponse, nil
-		}
-		chConfig := chConfigObject.ClickhouseConfig
-		encodedConfig, encodingErr = proto.Marshal(chConfig)
-	case protos.DBType_KAFKA:
-		kaConfigObject, ok := config.(*protos.Peer_KafkaConfig)
-		if !ok {
-			return wrongConfigResponse, nil
-		}
-		kaConfig := kaConfigObject.KafkaConfig
-		encodedConfig, encodingErr = proto.Marshal(kaConfig)
-	case protos.DBType_PUBSUB:
-		psConfigObject, ok := config.(*protos.Peer_PubsubConfig)
-		if !ok {
-			return wrongConfigResponse, nil
-		}
-		psConfig := psConfigObject.PubsubConfig
-		encodedConfig, encodingErr = proto.Marshal(psConfig)
-	case protos.DBType_EVENTHUBS:
-		ehConfigObject, ok := config.(*protos.Peer_EventhubGroupConfig)
-		if !ok {
-			return wrongConfigResponse, nil
-		}
-		ehConfig := ehConfigObject.EventhubGroupConfig
-		encodedConfig, encodingErr = proto.Marshal(ehConfig)
-	case protos.DBType_ELASTICSEARCH:
-		esConfigObject, ok := config.(*protos.Peer_ElasticsearchConfig)
-		if !ok {
-			return wrongConfigResponse, nil
-		}
-		esConfig := esConfigObject.ElasticsearchConfig
-		encodedConfig, encodingErr = proto.Marshal(esConfig)
-	default:
-		return wrongConfigResponse, nil
-	}
-	if encodingErr != nil {
-		slog.Error(fmt.Sprintf("failed to encode peer configuration for %s peer %s : %v",
-			req.Peer.Type, req.Peer.Name, encodingErr))
-		return nil, encodingErr
-	}
-
-	_, err := h.pool.Exec(ctx, "INSERT INTO peers (name, type, options) VALUES ($1, $2, $3)",
-		req.Peer.Name, peerType, encodedConfig,
-	)
-	if err != nil {
-		return &protos.CreatePeerResponse{
-			Status: protos.CreatePeerStatus_FAILED,
-			Message: fmt.Sprintf("failed to insert into peers table for %s peer %s: %s",
-				req.Peer.Type, req.Peer.Name, err.Error()),
-		}, nil
-	}
-
-	return &protos.CreatePeerResponse{
-		Status:  protos.CreatePeerStatus_CREATED,
-		Message: "",
-	}, nil
+	return utils.CreatePeerNoValidate(ctx, h.pool, req.Peer)
 }
 
 func (h *FlowRequestHandler) DropPeer(
---
+++ flow/connectors/utils/peers.go
@@ -0,0 +1,123 @@
+package utils
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"google.golang.org/protobuf/proto"
+
+	"github.com/PeerDB-io/peer-flow/generated/protos"
+)
+
+func CreatePeerNoValidate(
+	ctx context.Context,
+	pool *pgxpool.Pool,
+	peer *protos.Peer,
+) (*protos.CreatePeerResponse, error) {
+	config := peer.Config
+	wrongConfigResponse := &protos.CreatePeerResponse{
+		Status: protos.CreatePeerStatus_FAILED,
+		Message: fmt.Sprintf("invalid config for %s peer %s",
+			peer.Type, peer.Name),
+	}
+	var encodedConfig []byte
+	var encodingErr error
+	peerType := peer.Type
+	switch peerType {
+	case protos.DBType_POSTGRES:
+		pgConfigObject, ok := config.(*protos.Peer_PostgresConfig)
+		if !ok {
+			return wrongConfigResponse, nil
+		}
+		pgConfig := pgConfigObject.PostgresConfig
+		encodedConfig, encodingErr = proto.Marshal(pgConfig)
+	case protos.DBType_SNOWFLAKE:
+		sfConfigObject, ok := config.(*protos.Peer_SnowflakeConfig)
+		if !ok {
+			return wrongConfigResponse, nil
+		}
+		sfConfig := sfConfigObject.SnowflakeConfig
+		encodedConfig, encodingErr = proto.Marshal(sfConfig)
+	case protos.DBType_BIGQUERY:
+		bqConfigObject, ok := config.(*protos.Peer_BigqueryConfig)
+		if !ok {
+			return wrongConfigResponse, nil
+		}
+		bqConfig := bqConfigObject.BigqueryConfig
+		encodedConfig, encodingErr = proto.Marshal(bqConfig)
+	case protos.DBType_SQLSERVER:
+		sqlServerConfigObject, ok := config.(*protos.Peer_SqlserverConfig)
+		if !ok {
+			return wrongConfigResponse, nil
+		}
+		sqlServerConfig := sqlServerConfigObject.SqlserverConfig
+		encodedConfig, encodingErr = proto.Marshal(sqlServerConfig)
+	case protos.DBType_S3:
+		s3ConfigObject, ok := config.(*protos.Peer_S3Config)
+		if !ok {
+			return wrongConfigResponse, nil
+		}
+		s3Config := s3ConfigObject.S3Config
+		encodedConfig, encodingErr = proto.Marshal(s3Config)
+	case protos.DBType_CLICKHOUSE:
+		chConfigObject, ok := config.(*protos.Peer_ClickhouseConfig)
+		if !ok {
+			return wrongConfigResponse, nil
+		}
+		chConfig := chConfigObject.ClickhouseConfig
+		encodedConfig, encodingErr = proto.Marshal(chConfig)
+	case protos.DBType_KAFKA:
+		kaConfigObject, ok := config.(*protos.Peer_KafkaConfig)
+		if !ok {
+			return wrongConfigResponse, nil
+		}
+		kaConfig := kaConfigObject.KafkaConfig
+		encodedConfig, encodingErr = proto.Marshal(kaConfig)
+	case protos.DBType_PUBSUB:
+		psConfigObject, ok := config.(*protos.Peer_PubsubConfig)
+		if !ok {
+			return wrongConfigResponse, nil
+		}
+		psConfig := psConfigObject.PubsubConfig
+		encodedConfig, encodingErr = proto.Marshal(psConfig)
+	case protos.DBType_EVENTHUBS:
+		ehConfigObject, ok := config.(*protos.Peer_EventhubGroupConfig)
+		if !ok {
+			return wrongConfigResponse, nil
+		}
+		ehConfig := ehConfigObject.EventhubGroupConfig
+		encodedConfig, encodingErr = proto.Marshal(ehConfig)
+	case protos.DBType_ELASTICSEARCH:
+		esConfigObject, ok := config.(*protos.Peer_ElasticsearchConfig)
+		if !ok {
+			return wrongConfigResponse, nil
+		}
+		esConfig := esConfigObject.ElasticsearchConfig
+		encodedConfig, encodingErr = proto.Marshal(esConfig)
+	default:
+		return wrongConfigResponse, nil
+	}
+	if encodingErr != nil {
+		slog.Error(fmt.Sprintf("failed to encode peer configuration for %s peer %s : %v",
+			peer.Type, peer.Name, encodingErr))
+		return nil, encodingErr
+	}
+
+	_, err := pool.Exec(ctx, "INSERT INTO peers (name, type, options) VALUES ($1, $2, $3)",
+		peer.Name, peerType, encodedConfig,
+	)
+	if err != nil {
+		return &protos.CreatePeerResponse{
+			Status: protos.CreatePeerStatus_FAILED,
+			Message: fmt.Sprintf("failed to insert into peers table for %s peer %s: %s",
+				peer.Type, peer.Name, err.Error()),
+		}, nil
+	}
+
+	return &protos.CreatePeerResponse{
+		Status:  protos.CreatePeerStatus_CREATED,
+		Message: "",
+	}, nil
+}
