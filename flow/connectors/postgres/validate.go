62a89898b668b982149cb1fde27d1e32dc022302
Amogh-Bharadwaj <amogh@peerdb.io>
2024-06-12 03:24:58 +0530

    check index

--- flow/connectors/postgres/validate.go
+++ flow/connectors/postgres/validate.go
@@ -138,28 +138,41 @@ func (c *PostgresConnector) CheckReplicationConnectivity(ctx context.Context) er
 	return conn.Close(ctx)
 }
 
-// Check if all tables have either a primary key or REPLICA IDENTITY FULL
+// Check if all tables have either a primary key or REPLICA IDENTITY
 func (c *PostgresConnector) CheckIfTablesAreMirrorable(
 	ctx context.Context,
 	tables []*utils.SchemaTable,
 ) error {
-	badTables := make([]string, 0, len(tables))
+	var badTables []string
 	for _, table := range tables {
-		var canMirror sql.NullBool
+		var pkeyOrReplIdentFull sql.NullBool
 		err := c.conn.QueryRow(ctx, `SELECT
-		(con.contype = 'p' OR t.relreplident in ('i', 'f')) AS can_mirror
+		(con.contype = 'p' OR t.relreplident in ('f')) AS can_mirror
 		FROM pg_class t
 		LEFT JOIN pg_namespace n ON t.relnamespace = n.oid
 		LEFT JOIN pg_constraint con ON con.conrelid = t.oid
 		WHERE n.nspname = $1 AND t.relname = $2;
-		`, table.Schema, table.Table).Scan(&canMirror)
+		`, table.Schema, table.Table).Scan(&pkeyOrReplIdentFull)
 		if err != nil {
 			slog.Info("failed to check if table can be mirrored", slog.Any("error", err))
 			return err
 		}
 
-		if !canMirror.Bool || !canMirror.Valid {
-			badTables = append(badTables, table.Schema+"."+table.Table)
+		if !pkeyOrReplIdentFull.Valid || !pkeyOrReplIdentFull.Bool {
+			// Check for replica identity index
+			relId, err := c.getRelIDForTable(ctx, table)
+			if err != nil {
+				return err
+			}
+
+			indexCols, err := c.getReplicaIdentityIndexColumns(ctx, relId, table)
+			if err != nil {
+				return err
+			}
+
+			if len(indexCols) == 0 {
+				badTables = append(badTables, fmt.Sprintf("%s.%s", table.Schema, table.Table))
+			}
 		}
 	}
 
@@ -167,5 +180,5 @@ func (c *PostgresConnector) CheckIfTablesAreMirrorable(
 		return nil
 	}
 
-	return fmt.Errorf("tables %s do not have a primary key nor REPLICA IDENTITY FULL", strings.Join(badTables, ","))
+	return fmt.Errorf("tables %s neither have a primary key nor valid REPLICA IDENTITY INDEX/FULL", strings.Join(badTables, ","))
 }
