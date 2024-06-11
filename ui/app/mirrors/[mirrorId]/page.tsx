46a1f149e586a39a07bf121efba2c1c3c897a34d
Kevin Biju <kevin@peerdb.io>
2024-06-11 20:49:18 +0530

    fix review feedback

--- ui/app/mirrors/[mirrorId]/page.tsx
+++ ui/app/mirrors/[mirrorId]/page.tsx
@@ -96,9 +96,8 @@ export default async function ViewMirror({
       mirrorStatus.currentFlowState.toString() !==
       FlowStatus[FlowStatus.STATUS_PAUSED];
     const canResync =
-      (mirrorStatus.currentFlowState.toString() ===
-        FlowStatus[FlowStatus.STATUS_RUNNING] ||
-        !isNotPaused) &&
+      mirrorStatus.currentFlowState.toString() !==
+        FlowStatus[FlowStatus.STATUS_SETUP] &&
       (dbType.valueOf() === DBType.BIGQUERY.valueOf() ||
         dbType.valueOf() === DBType.SNOWFLAKE.valueOf());
 
