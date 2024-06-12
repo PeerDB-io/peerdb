bf9a148cfb66617058ab6097bcf76340dbc7639e
Amogh-Bharadwaj <amogh@peerdb.io>
2024-06-12 21:11:08 +0530

    dont send password from ui for status

--- ui/app/api/mirrors/state/route.ts
+++ ui/app/api/mirrors/state/route.ts
@@ -8,7 +8,6 @@ import { GetFlowHttpAddressFromEnv } from '@/rpc/http';
 export async function POST(request: Request) {
   const body: MirrorStatusRequest = await request.json();
   const flowServiceAddr = GetFlowHttpAddressFromEnv();
-  const authToken = GetHashedPeerDBPasswordFromEnv();
   try {
     const res: MirrorStatusResponse = await fetch(
       `${flowServiceAddr}/v1/mirrors/${body.flowJobName}?` +
@@ -17,7 +16,6 @@ export async function POST(request: Request) {
         }),
       {
         cache: 'no-store',
-        headers: new Headers({ Authorization: `Bearer ${authToken}` }),
       }
     ).then((res) => {
       return res.json();
