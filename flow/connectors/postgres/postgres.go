dbc4cc3994bffc8e4d766e6bd675b7e3338742a7
Amogh-Bharadwaj <amogh@peerdb.io>
2024-06-13 05:23:05 +0530

    more logs

--- flow/connectors/postgres/postgres.go
+++ flow/connectors/postgres/postgres.go
@@ -138,6 +138,8 @@ func (c *PostgresConnector) ReplPing(ctx context.Context) error {
 	if c.replLock.TryLock() {
 		defer c.replLock.Unlock()
 		if c.replState != nil {
+			c.logger.Info("[replPing]:sending replication status update",
+				slog.Int64("last offset", c.replState.LastOffset.Load()))
 			return pglogrepl.SendStandbyStatusUpdate(
 				ctx,
 				c.replConn.PgConn(),
