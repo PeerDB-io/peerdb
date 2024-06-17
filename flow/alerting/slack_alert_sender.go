71b414f3f1b6fbaa1bedb8970961e974d306817b
Amogh Bharadwaj <amogh@peerdb.io>
2024-06-17 22:43:54 +0530

    Slack alerts : can now specify members to tag in the channel (#1832)
    Adds a Members field in UI where you can add comma separated slack
    usernames to tag in the channel. If left empty, tags the whole channel
    Also adds a guide to slack alerting in the alerts UI
    
    Functionally tested:
    <img width="818" alt="Screenshot 2024-06-14 at 2 00 40 AM"
    src="https://github.com/PeerDB-io/peerdb/assets/65964360/1acdde2b-6955-49d0-9064-dc5af7338473">

--- flow/alerting/slack_alert_sender.go
+++ flow/alerting/slack_alert_sender.go
@@ -3,6 +3,7 @@ package alerting
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/slack-go/slack"
 )
@@ -11,6 +12,7 @@ type SlackAlertSender struct {
 	AlertSender
 	client                        *slack.Client
 	channelIDs                    []string
+	members                       []string
 	slotLagMBAlertThreshold       uint32
 	openConnectionsAlertThreshold uint32
 }
@@ -26,6 +28,7 @@ func (s *SlackAlertSender) getOpenConnectionsAlertThreshold() uint32 {
 type slackAlertConfig struct {
 	AuthToken                     string   `json:"auth_token"`
 	ChannelIDs                    []string `json:"channel_ids"`
+	Members                       []string `json:"members"`
 	SlotLagMBAlertThreshold       uint32   `json:"slot_lag_mb_alert_threshold"`
 	OpenConnectionsAlertThreshold uint32   `json:"open_connections_alert_threshold"`
 }
@@ -36,14 +39,25 @@ func newSlackAlertSender(config *slackAlertConfig) *SlackAlertSender {
 		channelIDs:                    config.ChannelIDs,
 		slotLagMBAlertThreshold:       config.SlotLagMBAlertThreshold,
 		openConnectionsAlertThreshold: config.OpenConnectionsAlertThreshold,
+		members:                       config.Members,
 	}
 }
 
 func (s *SlackAlertSender) sendAlert(ctx context.Context, alertTitle string, alertMessage string) error {
 	for _, channelID := range s.channelIDs {
+		var ccMembersPart strings.Builder
+		if len(s.members) == 0 {
+			ccMembersPart.WriteString("cc: <!channel>")
+		} else {
+			ccMembersPart.WriteString("cc:")
+			for _, member := range s.members {
+				ccMembersPart.WriteString(" @")
+				ccMembersPart.WriteString(member)
+			}
+		}
 		_, _, _, err := s.client.SendMessageContext(ctx, channelID, slack.MsgOptionBlocks(
 			slack.NewHeaderBlock(slack.NewTextBlockObject("plain_text", ":rotating_light:Alert:rotating_light:: "+alertTitle, true, false)),
-			slack.NewSectionBlock(slack.NewTextBlockObject("mrkdwn", alertMessage+"\ncc: <!channel>", false, false), nil, nil),
+			slack.NewSectionBlock(slack.NewTextBlockObject("mrkdwn", alertMessage+"\n"+ccMembersPart.String(), false, false), nil, nil),
 		))
 		if err != nil {
 			return fmt.Errorf("failed to send message to Slack channel %s: %w", channelID, err)
