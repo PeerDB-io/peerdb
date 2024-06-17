92f318480718e35eaf82e98d889e9b8b90933db9
Kunal Gupta <39487888+iamKunalGupta@users.noreply.github.com>
2024-06-17 22:17:43 +0530

    chore: add a 30 seconds timeout for first message close

--- flow-jvm/src/main/java/io/peerdb/flow/jvm/iceberg/service/IcebergService.java
+++ flow-jvm/src/main/java/io/peerdb/flow/jvm/iceberg/service/IcebergService.java
@@ -121,7 +121,8 @@ public class IcebergService {
 
     public boolean processAppendRecordsStreamRequest(Multi<AppendRecordsStreamRequest> request) {
         Log.info("Received a Process Append Records Stream Request, will wait for first message");
-        var firstMessage = Uni.createFrom().multi(request).await().indefinitely();
+        // Lets error out after 1 minute
+        var firstMessage = Uni.createFrom().multi(request).await().atMost(Duration.ofSeconds(30));
         if (firstMessage.getCommandCase() != AppendRecordsStreamRequest.CommandCase.TABLE_HEADER && !firstMessage.hasTableHeader()) {
             Log.errorf("TableHeader should be present in the first message, found %s", firstMessage.getCommandCase());
             throw new IllegalArgumentException("TableHeader should be present in the first message");
