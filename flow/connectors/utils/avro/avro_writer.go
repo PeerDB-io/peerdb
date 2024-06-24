af9c0044331ecdf73e85fbf0c484790b92fd14e6
Kaushik Iska <iska.kaushik@gmail.com>
2024-06-24 17:30:46 -0400

    Use snappy for BQ avro writing (#1855)
    Co-authored-by: Amogh Bharadwaj <amogh@peerdb.io>

--- flow/connectors/utils/avro/avro_writer.go
+++ flow/connectors/utils/avro/avro_writer.go
@@ -1,12 +1,15 @@
 package utils
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"io"
 	"log/slog"
 	"os"
 	"runtime/debug"
+	"sync/atomic"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
@@ -14,7 +17,6 @@ import (
 	"github.com/djherbis/buffer"
 	"github.com/djherbis/nio/v3"
 	"github.com/klauspost/compress/flate"
-	"github.com/klauspost/compress/snappy"
 	"github.com/klauspost/compress/zstd"
 	"github.com/linkedin/goavro/v2"
 
@@ -22,6 +24,7 @@ import (
 	"github.com/PeerDB-io/peer-flow/generated/protos"
 	"github.com/PeerDB-io/peer-flow/logger"
 	"github.com/PeerDB-io/peer-flow/model"
+	"github.com/PeerDB-io/peer-flow/shared"
 )
 
 type (
@@ -95,7 +98,7 @@ func (p *peerDBOCFWriter) initWriteCloser(w io.Writer) error {
 			return fmt.Errorf("error while initializing deflate encoding writer: %w", err)
 		}
 	case CompressSnappy:
-		p.writer = snappy.NewBufferedWriter(w)
+		p.writer = &nopWriteCloser{w}
 	}
 
 	return nil
@@ -107,9 +110,15 @@ func (p *peerDBOCFWriter) createOCFWriter(w io.Writer) (*goavro.OCFWriter, error
 		return nil, fmt.Errorf("failed to create compressed writer: %w", err)
 	}
 
+	var compressionName string
+	if p.avroCompressionCodec == CompressSnappy {
+		compressionName = "snappy"
+	}
+
 	ocfWriter, err := goavro.NewOCFWriter(goavro.OCFConfig{
-		W:      p.writer,
-		Schema: p.avroSchema.Schema,
+		W:               p.writer,
+		Schema:          p.avroSchema.Schema,
+		CompressionName: compressionName,
 	})
 	if err != nil {
 		return nil, fmt.Errorf("failed to create OCF writer: %w", err)
@@ -118,7 +127,7 @@ func (p *peerDBOCFWriter) createOCFWriter(w io.Writer) (*goavro.OCFWriter, error
 	return ocfWriter, nil
 }
 
-func (p *peerDBOCFWriter) writeRecordsToOCFWriter(ctx context.Context, ocfWriter *goavro.OCFWriter) (int, error) {
+func (p *peerDBOCFWriter) writeRecordsToOCFWriter(ctx context.Context, ocfWriter *goavro.OCFWriter) (int64, error) {
 	logger := logger.LoggerFromCtx(ctx)
 	schema := p.stream.Schema()
 
@@ -129,33 +138,39 @@ func (p *peerDBOCFWriter) writeRecordsToOCFWriter(ctx context.Context, ocfWriter
 		logger,
 	)
 
-	numRows := 0
+	numRows := atomic.Int64{}
+
+	shutdown := shared.Interval(ctx, time.Minute, func() {
+		logger.Info(fmt.Sprintf("written %d records to OCF", numRows.Load()))
+	})
+	defer shutdown()
+
 	for qrecord := range p.stream.Records {
 		if err := ctx.Err(); err != nil {
-			return numRows, ctx.Err()
+			return numRows.Load(), ctx.Err()
 		} else {
 			avroMap, err := avroConverter.Convert(qrecord)
 			if err != nil {
 				logger.Error("Failed to convert QRecord to Avro compatible map", slog.Any("error", err))
-				return numRows, fmt.Errorf("failed to convert QRecord to Avro compatible map: %w", err)
+				return numRows.Load(), fmt.Errorf("failed to convert QRecord to Avro compatible map: %w", err)
 			}
 
 			err = ocfWriter.Append([]interface{}{avroMap})
 			if err != nil {
 				logger.Error("Failed to write record to OCF", slog.Any("error", err))
-				return numRows, fmt.Errorf("failed to write record to OCF: %w", err)
+				return numRows.Load(), fmt.Errorf("failed to write record to OCF: %w", err)
 			}
 
-			numRows++
+			numRows.Add(1)
 		}
 	}
 
 	if err := p.stream.Err(); err != nil {
 		logger.Error("Failed to get record from stream", slog.Any("error", err))
-		return numRows, fmt.Errorf("failed to get record from stream: %w", err)
+		return numRows.Load(), fmt.Errorf("failed to get record from stream: %w", err)
 	}
 
-	return numRows, nil
+	return numRows.Load(), nil
 }
 
 func (p *peerDBOCFWriter) WriteOCF(ctx context.Context, w io.Writer) (int, error) {
@@ -170,7 +185,7 @@ func (p *peerDBOCFWriter) WriteOCF(ctx context.Context, w io.Writer) (int, error
 	if err != nil {
 		return 0, fmt.Errorf("failed to write records to OCF writer: %w", err)
 	}
-	return numRows, nil
+	return int(numRows), nil
 }
 
 func (p *peerDBOCFWriter) WriteRecordsToS3(
@@ -230,8 +245,13 @@ func (p *peerDBOCFWriter) WriteRecordsToAvroFile(ctx context.Context, filePath s
 	if err != nil {
 		return nil, fmt.Errorf("failed to create temporary Avro file: %w", err)
 	}
+	defer file.Close()
 
-	numRecords, err := p.WriteOCF(ctx, file)
+	buffSizeBytes := 1 << 26 // 64 MB
+	bufferedWriter := bufio.NewWriterSize(file, buffSizeBytes)
+	defer bufferedWriter.Flush()
+
+	numRecords, err := p.WriteOCF(ctx, bufferedWriter)
 	if err != nil {
 		return nil, fmt.Errorf("failed to write records to temporary Avro file: %w", err)
 	}
