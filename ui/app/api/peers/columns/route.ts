77098e15a66da8d3f763df995d316e2c65e28054
Philip Dubé <philip@peerdb.io>
2024-06-25 22:36:36 +0000

    ui: move GetPeerInfo to flow-api
    This was one place peer config was being decoded in ui,
    lift to flow-api so encrypted peer configs don't matter to ui code

--- ui/app/api/peers/columns/route.ts
+++ ui/app/api/peers/columns/route.ts
@@ -1,22 +1,10 @@
-import { UColumnsResponse } from '@/app/dto/PeersDTO';
-import { TableColumnsResponse } from '@/grpc_generated/route';
 import { GetFlowHttpAddressFromEnv } from '@/rpc/http';
 
 export async function POST(request: Request) {
   const body = await request.json();
   const { peerName, schemaName, tableName } = body;
   const flowServiceAddr = GetFlowHttpAddressFromEnv();
-  try {
-    const columnsList: TableColumnsResponse = await fetch(
-      `${flowServiceAddr}/v1/peers/columns?peer_name=${peerName}&schema_name=${schemaName}&table_name=${tableName}`
-    ).then((res) => {
-      return res.json();
-    });
-    let response: UColumnsResponse = {
-      columns: columnsList.columns,
-    };
-    return new Response(JSON.stringify(response));
-  } catch (e) {
-    console.log(e);
-  }
+  return fetch(
+    `${flowServiceAddr}/v1/peers/columns?peer_name=${encodeURIComponent(peerName)}&schema_name=${encodeURIComponent(schemaName)}&table_name=${encodeURIComponent(tableName)}`
+  );
 }
