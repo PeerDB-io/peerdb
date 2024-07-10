847667ee39a40f5d452e0891784074f91ff3beb9
Philip Dubé <philip@peerdb.io>
2024-07-10 20:20:21 +0000

    npm run format

--- ui/app/api/mirrors/errors/route.ts
+++ ui/app/api/mirrors/errors/route.ts
@@ -1,11 +1,11 @@
-import {GetFlowHttpAddressFromEnv} from '@/rpc/http';
+import { GetFlowHttpAddressFromEnv } from '@/rpc/http';
 
 export async function POST(request: Request) {
   const flowServiceAddr = GetFlowHttpAddressFromEnv();
   return fetch(`${flowServiceAddr}/v1/mirrors/logs`, {
-    method : 'POST',
-    cache : 'no-cache',
-    body : request.body,
-    duplex : 'half',
+    method: 'POST',
+    cache: 'no-cache',
+    body: request.body,
+    duplex: 'half',
   } as any);
 }
