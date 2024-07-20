4b217c38d8b7405d61e645c4d64f5a1303a8ba03
Philip Dubé <serprex@users.noreply.github.com>
2024-07-20 02:36:21 +0000

    remove /api/peers (#1966)
    remove `@grpc/grpc-js`, which isn't browser compatible & wasn't being used

--- ui/app/peers/create/[peerType]/handlers.ts
+++ ui/app/peers/create/[peerType]/handlers.ts
@@ -1,10 +1,29 @@
+import { PeerConfig } from '@/app/dto/PeersDTO';
 import {
-  PeerConfig,
-  UCreatePeerResponse,
-  UValidatePeerResponse,
-} from '@/app/dto/PeersDTO';
-import { S3Config } from '@/grpc_generated/peers';
+  BigqueryConfig,
+  ClickhouseConfig,
+  DBType,
+  ElasticsearchConfig,
+  EventHubGroupConfig,
+  KafkaConfig,
+  Peer,
+  PostgresConfig,
+  PubSubConfig,
+  S3Config,
+  SnowflakeConfig,
+} from '@/grpc_generated/peers';
+import {
+  CreatePeerRequest,
+  CreatePeerResponse,
+  CreatePeerStatus,
+  createPeerStatusFromJSON,
+  ValidatePeerRequest,
+  ValidatePeerResponse,
+  ValidatePeerStatus,
+  validatePeerStatusFromJSON,
+} from '@/grpc_generated/route';
 import { Dispatch, SetStateAction } from 'react';
+
 import {
   bqSchema,
   chSchema,
@@ -18,12 +37,77 @@ import {
   sfSchema,
 } from './schema';
 
-const validateFields = (
+function constructPeer(
+  name: string,
+  type: string,
+  config: PeerConfig
+): Peer | undefined {
+  switch (type) {
+    case 'POSTGRES':
+      return {
+        name,
+        type: DBType.POSTGRES,
+        postgresConfig: config as PostgresConfig,
+      };
+    case 'SNOWFLAKE':
+      return {
+        name,
+        type: DBType.SNOWFLAKE,
+        snowflakeConfig: config as SnowflakeConfig,
+      };
+    case 'BIGQUERY':
+      return {
+        name,
+        type: DBType.BIGQUERY,
+        bigqueryConfig: config as BigqueryConfig,
+      };
+    case 'CLICKHOUSE':
+      return {
+        name,
+        type: DBType.CLICKHOUSE,
+        clickhouseConfig: config as ClickhouseConfig,
+      };
+    case 'S3':
+      return {
+        name,
+        type: DBType.S3,
+        s3Config: config as S3Config,
+      };
+    case 'KAFKA':
+      return {
+        name,
+        type: DBType.KAFKA,
+        kafkaConfig: config as KafkaConfig,
+      };
+    case 'PUBSUB':
+      return {
+        name,
+        type: DBType.PUBSUB,
+        pubsubConfig: config as PubSubConfig,
+      };
+    case 'EVENTHUBS':
+      return {
+        name,
+        type: DBType.EVENTHUBS,
+        eventhubGroupConfig: config as EventHubGroupConfig,
+      };
+    case 'ELASTICSEARCH':
+      return {
+        name,
+        type: DBType.ELASTICSEARCH,
+        elasticsearchConfig: config as ElasticsearchConfig,
+      };
+    default:
+      return;
+  }
+}
+
+function validateFields(
   type: string,
   config: PeerConfig,
   notify: (msg: string) => void,
   name?: string
-): boolean => {
+): boolean {
   const peerNameValid = peerNameSchema.safeParse(name);
   if (!peerNameValid.success) {
     const peerNameErr = peerNameValid.error.issues[0].message;
@@ -76,10 +160,7 @@ const validateFields = (
       break;
     case 'ELASTICSEARCH':
       const esConfig = esSchema.safeParse(config);
-      if (!esConfig.success) {
-        console.log(esConfig.error);
-        validationErr = esConfig.error.issues[0].message;
-      }
+      if (!esConfig.success) validationErr = esConfig.error.issues[0].message;
       break;
     default:
       validationErr = 'Unsupported peer type ' + type;
@@ -89,7 +170,7 @@ const validateFields = (
     return false;
   }
   return true;
-};
+}
 
 // API call to validate peer
 export const handleValidate = async (
@@ -103,17 +184,15 @@ export const handleValidate = async (
   if (!isValid) return;
   setLoading(true);
 
-  const valid: UValidatePeerResponse = await fetch('/api/peers/', {
+  const validateReq: ValidatePeerRequest = {
+    peer: constructPeer(name!, type, config),
+  };
+  const valid: ValidatePeerResponse = await fetch('/api/v1/peers/validate', {
     method: 'POST',
-    body: JSON.stringify({
-      name,
-      type,
-      config,
-      mode: 'validate',
-    }),
+    body: JSON.stringify(validateReq),
     cache: 'no-store',
   }).then((res) => res.json());
-  if (!valid.valid) {
+  if (validatePeerStatusFromJSON(valid.status) !== ValidatePeerStatus.VALID) {
     notify(valid.message);
     setLoading(false);
     return;
@@ -130,28 +209,29 @@ const S3Validation = (config: S3Config): string => {
 };
 
 // API call to create peer
-export const handleCreate = async (
+export async function handleCreate(
   type: string,
   config: PeerConfig,
   notify: (msg: string) => void,
   setLoading: Dispatch<SetStateAction<boolean>>,
   route: RouteCallback,
   name?: string
-) => {
+) {
   let isValid = validateFields(type, config, notify, name);
   if (!isValid) return;
   setLoading(true);
-  const createdPeer: UCreatePeerResponse = await fetch('/api/peers/', {
+  const req: CreatePeerRequest = {
+    peer: constructPeer(name!, type, config),
+    allowUpdate: true,
+  };
+  const createdPeer: CreatePeerResponse = await fetch('/api/v1/peers/create', {
     method: 'POST',
-    body: JSON.stringify({
-      name,
-      type,
-      config,
-      mode: 'create',
-    }),
+    body: JSON.stringify(req),
     cache: 'no-store',
   }).then((res) => res.json());
-  if (!createdPeer.created) {
+  if (
+    createPeerStatusFromJSON(createdPeer.status) !== CreatePeerStatus.CREATED
+  ) {
     notify(createdPeer.message);
     setLoading(false);
     return;
@@ -159,4 +239,4 @@ export const handleCreate = async (
 
   route();
   setLoading(false);
-};
+}
