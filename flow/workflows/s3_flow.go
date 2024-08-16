5b8600c6b254854fab98508fc35c5181c7142618
Philip Dubé <philip@peerdb.io>
2024-08-16 23:33:51 +0000

    wiring

--- flow/workflows/s3_flow.go
+++ flow/workflows/s3_flow.go
@@ -1,12 +1,17 @@
 package peerflow
 
 import (
+	"time"
+
 	"go.temporal.io/sdk/workflow"
 
 	"github.com/PeerDB-io/peer-flow/generated/protos"
 )
 
 func S3Workflow(ctx workflow.Context, config *protos.FlowConnectionConfigs) error {
+	ctx = workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
+		StartToCloseTimeout: time.Hour,
+	})
 	exportFuture := workflow.ExecuteActivity(ctx, snapshot.S3Export, config)
 	if err := exportFuture.Get(ctx, nil); err != nil {
 		return err
