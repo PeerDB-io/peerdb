bbf28403d5733232ac1414e0e92c6aa6aab8a22d
Kaushik Iska <iska.kaushik@gmail.com>
2024-09-01 23:18:23 -0500

    fix a couple of issues

--- flow/connectors/clickhouse/normalize.go
+++ flow/connectors/clickhouse/normalize.go
@@ -254,8 +254,7 @@ func (c *ClickhouseConnector) NormalizeRecords(ctx context.Context,
 		}
 	}
 
-	endNormalizeBatchId := normBatchID + 1
-	err = c.UpdateNormalizeBatchID(ctx, req.FlowJobName, endNormalizeBatchId)
+	err = c.UpdateNormalizeBatchID(ctx, req.FlowJobName, req.SyncBatchID)
 	if err != nil {
 		c.logger.Error("[clickhouse] error while updating normalize batch id", "error", err)
 		return nil, err
@@ -263,7 +262,7 @@ func (c *ClickhouseConnector) NormalizeRecords(ctx context.Context,
 
 	return &model.NormalizeResponse{
 		Done:         true,
-		StartBatchID: endNormalizeBatchId,
+		StartBatchID: normBatchID,
 		EndBatchID:   req.SyncBatchID,
 	}, nil
 }
