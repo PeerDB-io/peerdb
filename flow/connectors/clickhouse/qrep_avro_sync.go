9dd7b1f6c807be834daf5ba2471ff55eed65f5cf
Amogh-Bharadwaj <amogh@peerdb.io>
2024-09-13 14:18:07 +0530

    change initial load parts to 3

--- flow/connectors/clickhouse/qrep_avro_sync.go
+++ flow/connectors/clickhouse/qrep_avro_sync.go
@@ -150,7 +150,7 @@ func (s *ClickhouseAvroSyncMethod) SyncQRepRecords(
 
 	hashColName := dstTableSchema[0].Name()
 
-	numParts := 15
+	numParts := 3
 	for i := 0; i < numParts; i++ {
 		whereClause := fmt.Sprintf("cityHash64(%s) %% %d = %d", hashColName, numParts, i)
 		query := fmt.Sprintf("INSERT INTO %s(%s) SELECT %s FROM s3('%s','%s','%s'%s, 'Avro') WHERE %s",
