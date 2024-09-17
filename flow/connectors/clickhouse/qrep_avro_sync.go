7f1276b0df6bcf3282cb52194528dc0031493675
Amogh-Bharadwaj <amogh@peerdb.io>
2024-09-17 12:07:59 +0530

    change parts back to 7

--- flow/connectors/clickhouse/qrep_avro_sync.go
+++ flow/connectors/clickhouse/qrep_avro_sync.go
@@ -53,7 +53,7 @@ func (s *ClickhouseAvroSyncMethod) CopyStageToDestination(ctx context.Context, a
 	}
 
 	hashColName := "_peerdb_uid"
-	numParts := 29
+	numParts := 7
 	query := fmt.Sprintf("INSERT INTO %s SELECT * FROM s3('%s','%s','%s'%s, 'Avro')",
 		s.config.DestinationTableIdentifier, avroFileUrl,
 		creds.AWS.AccessKeyID, creds.AWS.SecretAccessKey, sessionTokenPart)
