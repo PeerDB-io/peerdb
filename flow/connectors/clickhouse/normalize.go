efb7c8f5693e30788d5fdd4e060e34cc50623e47
Amogh-Bharadwaj <amogh@peerdb.io>
2024-09-23 11:45:22 +0530

    change norm to 41

--- flow/connectors/clickhouse/normalize.go
+++ flow/connectors/clickhouse/normalize.go
@@ -232,7 +232,7 @@ func (c *ClickhouseConnector) NormalizeRecords(ctx context.Context,
 
 		q := insertIntoSelectQuery.String()
 
-		numParts := 29
+		numParts := 41
 		hashColName := "_peerdb_uid"
 		for i := 0; i < numParts; i++ {
 			whereClause := fmt.Sprintf("cityHash64(%s) %% %d = %d", hashColName, numParts, i)
