ac3f479cddfa365215c57d91053741cb87b1a3e7
Philip Dubé <philip@peerdb.io>
2024-09-23 15:46:02 +0000

    can only reference unique constraints, which flows lacks, need to fix some things for this

--- nexus/catalog/migrations/V39__table_schema_mapping.sql
+++ nexus/catalog/migrations/V39__table_schema_mapping.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS table_schema_mapping (
-    flow_name varchar(255) not null references flows(name),
+    flow_name varchar(255) not null,
     table_name text not null,
     table_schema bytea not null,
     primary key (flow_name, table_name)
