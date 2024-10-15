b06d7462c83318e79ec5607f648847937441f0e4
Amogh-Bharadwaj <amogh@peerdb.io>
2024-10-16 01:25:56 +0530

    add warning banner

--- ui/app/utils/notify.tsx
+++ ui/app/utils/notify.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { toast } from 'react-toastify';
 
 export const notifyErr = (msg: string, ok?: boolean) => {
@@ -12,13 +13,28 @@ export const notifyErr = (msg: string, ok?: boolean) => {
   }
 };
 
-const SortingKeyToast = () => <div></div>;
+// TODO: add a link to the document when ready
+const SortingKeyToast = () => {
+  return (
+    <div>
+      <p>
+        Using ordering keys in ClickHouse that differ from the primary key in
+        Postgres has some caveats. Please read{' '}
+        <Link style={{ color: 'teal' }} href={''} target='_blank'>
+          this document
+        </Link>{' '}
+        carefully.
+      </p>
+    </div>
+  );
+};
 
-export const notifySortingKey = (msg: string) => {
-  toast.warn(msg, {
+export const notifySortingKey = () => {
+  toast.warn(SortingKeyToast, {
     position: 'bottom-center',
     autoClose: false,
     closeOnClick: false,
     closeButton: true,
+    toastId: 'sorting_key_warning',
   });
 };
