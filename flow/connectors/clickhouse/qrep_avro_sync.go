d30017581c3e896dabdd3740cfb46616e48c3808
Kevin Biju <kevin@peerdb.io>
2024-10-18 00:26:51 +0530

    commit mistake

--- flow/connectors/clickhouse/qrep_avro_sync.go
+++ flow/connectors/clickhouse/qrep_avro_sync.go
@@ -38,7 +38,7 @@ func (s *ClickHouseAvroSyncMethod) CopyStageToDestination(ctx context.Context, a
 		return err
 	}
 
-	endpoint := "http://localhost:9001"
+	endpoint := s.connector.credsProvider.Provider.GetEndpointURL()
 	region := s.connector.credsProvider.Provider.GetRegion()
 	avroFileUrl := utils.FileURLForS3Service(endpoint, region, s3o.Bucket, avroFile.FilePath)
 	creds, err := s.connector.credsProvider.Provider.Retrieve(ctx)
@@ -124,7 +124,7 @@ func (s *ClickHouseAvroSyncMethod) SyncQRepRecords(
 		return 0, err
 	}
 
-	endpoint := "http://localhost:9001"
+	endpoint := s.connector.credsProvider.Provider.GetEndpointURL()
 	region := s.connector.credsProvider.Provider.GetRegion()
 	avroFileUrl := utils.FileURLForS3Service(endpoint, region, s3o.Bucket, avroFile.FilePath)
 	selector := make([]string, 0, len(dstTableSchema))
