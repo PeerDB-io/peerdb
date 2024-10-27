b6c1c0db3195dcedece6db1f2e1892237db67530
root <root@debian-12-cloud-image>
2024-10-27 11:00:47 +0100

    Change upload size for s3 to work with large tables.

--- flow/connectors/utils/avro/avro_writer.go
+++ flow/connectors/utils/avro/avro_writer.go
@@ -215,7 +215,12 @@ func (p *peerDBOCFWriter) WriteRecordsToS3(
 		numRows, writeOcfError = p.WriteOCF(ctx, w)
 	}()
 
-	_, err = manager.NewUploader(s3svc).Upload(ctx, &s3.PutObjectInput{
+	// Create the uploader using the AWS SDK v2 manager
+	uploader := manager.NewUploader(s3svc, func(u *manager.Uploader) {
+		u.PartSize = 500 * 1024 * 1024 // Set part size to 500 MB
+	})
+
+	_, err = uploader.Upload(ctx, &s3.PutObjectInput{
 		Bucket: aws.String(bucketName),
 		Key:    aws.String(key),
 		Body:   r,
