06f20858fadbb0e6cbe6caed494f053028f6f91c
Philip Dubé <philip@peerdb.io>
2024-12-02 19:21:01 +0000

    map typmod -1 to 0, 0 prec/scale not randomly large 16 byte integers

--- flow/datatypes/numeric.go
+++ flow/datatypes/numeric.go
@@ -90,6 +90,10 @@ func MakeNumericTypmod(precision int32, scale int32) int32 {
 // This is to reverse what make_numeric_typmod of Postgres does:
 // https://github.com/postgres/postgres/blob/21912e3c0262e2cfe64856e028799d6927862563/src/backend/utils/adt/numeric.c#L897
 func ParseNumericTypmod(typmod int32) (int16, int16) {
+	if typmod == -1 {
+		return 0, 0
+	}
+
 	offsetMod := typmod - VARHDRSZ
 	precision := int16((offsetMod >> 16) & 0x7FFF)
 	scale := int16(offsetMod & 0x7FFF)
