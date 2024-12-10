432580bc421ba30801d8e10f94eefe775a0ea892
Philip Dubé <serprex@users.noreply.github.com>
2024-12-10 15:17:27 +0000

    fix ui ci (#2346)
    https://github.com/tremorlabs/tremor/pull/1142 broke us,
    because we depended on their @headlessui/tailwindcss dependency

--- ui/tailwind.config.ts
+++ ui/tailwind.config.ts
@@ -102,5 +102,5 @@ module.exports = {
         /^(fill-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
     },
   ],
-  plugins: [require('@headlessui/tailwindcss'), require('tailwindcss-animate')],
+  plugins: [require('tailwindcss-animate')],
 } satisfies Config;
