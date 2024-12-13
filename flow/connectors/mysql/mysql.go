3e6fe477d05c23cefdc6fd9ac71d4caec434d65c
Philip Dubé <philip@peerdb.io>
2024-12-13 15:19:05 +0000

    wip

--- flow/connectors/mysql/mysql.go
+++ flow/connectors/mysql/mysql.go
@@ -1,15 +1,43 @@
-// stub to bypass validation
+// https://airbyte.com/blog/replicating-mysql-a-look-at-the-binlog-and-gtids
 
-package mysql
+package connmysql
 
-import "context"
+import (
+	"context"
 
-type MySqlConnector struct{}
+	"github.com/go-mysql-org/go-mysql/replication"
 
-func (MySqlConnector) Close() error {
+	"github.com/PeerDB-io/peer-flow/generated/protos"
+)
+
+type MySqlConnector struct {
+	config   *protos.MySqlConfig
+	syncer   *replication.BinlogSyncer
+	streamer *replication.BinlogStreamer
+}
+
+func NewMySqlConnector(ctx context.Context, config *protos.MySqlConfig) (*MySqlConnector, error) {
+	syncer := replication.NewBinlogSyncer(replication.BinlogSyncerConfig{
+		ServerID: 1729,    // TODO put in config
+		Flavor:   "mysql", // TODO put in config
+		Host:     config.Host,
+		Port:     uint16(config.Port),
+		User:     config.User,
+		Password: config.Password,
+	})
+	return &MySqlConnector{
+		config: config,
+		syncer: syncer,
+	}, nil
+}
+
+func (c *MySqlConnector) Close() error {
+	if c.syncer != nil {
+		c.syncer.Close()
+	}
 	return nil
 }
 
-func (MySqlConnector) ConnectionActive(context.Context) error {
+func (*MySqlConnector) ConnectionActive(context.Context) error {
 	return nil
 }
