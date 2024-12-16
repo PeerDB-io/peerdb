469336f4ed6314ab9ca3a641e06f29f08d103d4c
Philip Dubé <philip@peerdb.io>
2024-12-16 16:46:16 +0000

    filling out logic to take in row

--- flow/connectors/mysql/cdc.go
+++ flow/connectors/mysql/cdc.go
@@ -2,10 +2,14 @@ package connmysql
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"time"
 
 	"github.com/go-mysql-org/go-mysql/mysql"
 	"github.com/go-mysql-org/go-mysql/replication"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/shopspring/decimal"
 
 	"github.com/PeerDB-io/peer-flow/alerting"
 	"github.com/PeerDB-io/peer-flow/generated/protos"
@@ -82,8 +86,60 @@ func (c *MySqlConnector) RemoveTablesFromPublication(ctx context.Context, req *p
 	return nil
 }
 
-func qvalueFromMysql(typ byte, val any) qvalue.QValue {
-	// TODO
+func qvalueFromMysql(mytype byte, qkind qvalue.QValueKind, val any) qvalue.QValue {
+	// TODO signedness, in ev.Table, need to extend QValue system
+	// See go-mysql row_event.go for mapping
+	switch val := val.(type) {
+	case nil:
+		return qvalue.QValueNull(qkind)
+	case int8: // TODO qvalue.Int8
+		return qvalue.QValueInt16{Val: int16(val)}
+	case int16:
+		return qvalue.QValueInt16{Val: val}
+	case int32:
+		return qvalue.QValueInt32{Val: val}
+	case int64:
+		return qvalue.QValueInt64{Val: val}
+	case float32:
+		return qvalue.QValueFloat32{Val: val}
+	case float64:
+		return qvalue.QValueFloat64{Val: val}
+	case decimal.Decimal:
+		return qvalue.QValueNumeric{Val: val}
+	case int:
+		// YEAR: https://dev.mysql.com/doc/refman/8.4/en/year.html
+		return qvalue.QValueInt16{Val: int16(val)}
+	case time.Time:
+		return qvalue.QValueTimestamp{Val: val}
+	case *replication.JsonDiff:
+		// TODO support somehow??
+		return qvalue.QValueNull(qvalue.QValueKindJSON)
+	case []byte:
+		switch mytype {
+		case mysql.MYSQL_TYPE_BLOB:
+			return qvalue.QValueBytes{Val: val}
+		case mysql.MYSQL_TYPE_JSON:
+			return qvalue.QValueJSON{Val: string(val)}
+		case mysql.MYSQL_TYPE_GEOMETRY:
+			// TODO figure out mysql geo encoding
+			return qvalue.QValueGeometry{Val: string(val)}
+		}
+	case string:
+		switch mytype {
+		case mysql.MYSQL_TYPE_TIME:
+			// TODO parse
+		case mysql.MYSQL_TYPE_TIME2:
+			// TODO parse
+		case mysql.MYSQL_TYPE_DATE:
+			// TODO parse
+		case mysql.MYSQL_TYPE_VARCHAR,
+			mysql.MYSQL_TYPE_VAR_STRING,
+			mysql.MYSQL_TYPE_STRING:
+			return qvalue.QValueString{Val: val}
+		}
+	default:
+		panic(fmt.Sprintf("unexpected type %T for mysql type %d", val, mytype))
+	}
 	return nil
 }
 
@@ -112,34 +168,55 @@ func (c *MySqlConnector) PullRecords(
 		}
 		switch ev := event.Event.(type) {
 		case *replication.RowsEvent:
+			sourceTableName := string(ev.Table.Table) // TODO need ev.Table.Schema?
+			destinationTableName := req.TableNameMapping[sourceTableName].Name
+			schema := req.TableNameSchemaMapping[destinationTableName]
 			for _, row := range ev.Rows {
 				var record model.Record[model.RecordItems]
-				//TODO need tableNameMapping[source] -> destination
 				//TODO need mapping of column index to column name
 				var items model.RecordItems
 				switch event.Header.EventType {
-				case replication.WRITE_ROWS_EVENTv0, replication.WRITE_ROWS_EVENTv1, replication.WRITE_ROWS_EVENTv2:
+				case replication.WRITE_ROWS_EVENTv0, replication.UPDATE_ROWS_EVENTv0, replication.DELETE_ROWS_EVENTv0:
+					return errors.New("mysql v0 replication protocol not supported")
+				case replication.WRITE_ROWS_EVENTv1, replication.WRITE_ROWS_EVENTv2:
 					for idx, val := range row {
-						// TODO
-						items.AddColumn("ColumnName", qvalueFromMysql(ev.Table.ColumnType[idx], val))
+						fd := schema.Columns[idx]
+						items.AddColumn(fd.Name, qvalueFromMysql(ev.Table.ColumnType[idx], qvalue.QValueKind(fd.Type), val))
 					}
 					record = &model.InsertRecord[model.RecordItems]{
-						BaseRecord:      model.BaseRecord{CommitTimeNano: int64(event.Header.Timestamp) * 1e9},
-						Items:           items,
-						SourceTableName: string(ev.Table.Table),
+						BaseRecord:           model.BaseRecord{CommitTimeNano: int64(event.Header.Timestamp) * 1e9},
+						Items:                items,
+						SourceTableName:      sourceTableName,
+						DestinationTableName: destinationTableName,
 					}
-				case replication.UPDATE_ROWS_EVENTv0, replication.UPDATE_ROWS_EVENTv1, replication.UPDATE_ROWS_EVENTv2:
-					// TODO no OldItems / NewItems. How does primary key update work?
+				case replication.UPDATE_ROWS_EVENTv1, replication.UPDATE_ROWS_EVENTv2:
+					var oldItems model.RecordItems
+					for idx, val := range row {
+						fd := schema.Columns[idx>>1]
+						qv := qvalueFromMysql(ev.Table.ColumnType[idx], qvalue.QValueKind(fd.Type), val)
+						if (idx & 1) == 0 { // TODO test that it isn't other way around
+							oldItems.AddColumn(fd.Name, qv)
+						} else {
+							items.AddColumn(fd.Name, qv)
+						}
+					}
 					record = &model.UpdateRecord[model.RecordItems]{
-						BaseRecord:      model.BaseRecord{CommitTimeNano: int64(event.Header.Timestamp) * 1e9},
-						NewItems:        items,
-						SourceTableName: string(ev.Table.Table),
+						BaseRecord:           model.BaseRecord{CommitTimeNano: int64(event.Header.Timestamp) * 1e9},
+						OldItems:             oldItems,
+						NewItems:             items,
+						SourceTableName:      sourceTableName,
+						DestinationTableName: destinationTableName,
 					}
-				case replication.DELETE_ROWS_EVENTv0, replication.DELETE_ROWS_EVENTv1, replication.DELETE_ROWS_EVENTv2:
+				case replication.DELETE_ROWS_EVENTv1, replication.DELETE_ROWS_EVENTv2:
+					for idx, val := range row {
+						fd := schema.Columns[idx]
+						items.AddColumn(fd.Name, qvalueFromMysql(ev.Table.ColumnType[idx], qvalue.QValueKind(fd.Type), val))
+					}
 					record = &model.DeleteRecord[model.RecordItems]{
-						BaseRecord:      model.BaseRecord{CommitTimeNano: int64(event.Header.Timestamp) * 1e9},
-						Items:           items,
-						SourceTableName: string(ev.Table.Table),
+						BaseRecord:           model.BaseRecord{CommitTimeNano: int64(event.Header.Timestamp) * 1e9},
+						Items:                items,
+						SourceTableName:      sourceTableName,
+						DestinationTableName: destinationTableName,
 					}
 				default:
 					continue
