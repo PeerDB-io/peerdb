304f4f108761564b6c271eb6c643313633191111
Kevin Biju <52661649+heavycrystal@users.noreply.github.com>
2025-01-21 02:07:01 +0530

    call SyncFlowCleanup during SetupFlow (#2464)

--- flow/activities/flowable.go
+++ flow/activities/flowable.go
@@ -83,6 +83,13 @@ func (a *FlowableActivity) SetupMetadataTables(ctx context.Context, config *prot
 		return fmt.Errorf("failed to setup metadata tables: %w", err)
 	}
 
+	// this should have been done by DropFlowDestination
+	// but edge case due to late context cancellation
+	if err := dstConn.SyncFlowCleanup(ctx, config.FlowName); err != nil {
+		a.Alerter.LogFlowError(ctx, config.FlowName, err)
+		return fmt.Errorf("failed to clean up destination before mirror setup: %w", err)
+	}
+
 	return nil
 }
 
