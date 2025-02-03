829e5bceeb432f7ba8709cf32ebcbf228e6fd11e
Philip Dubé <serprex@users.noreply.github.com>
2025-02-03 22:55:20 +0000

    mysql: SignalAsEmpty/SignalAsNotEmpty (#2507)
    Adjust WaitAndCheckEmpty to catch this in future

--- flow/connectors/mysql/cdc.go
+++ flow/connectors/mysql/cdc.go
@@ -264,6 +264,13 @@ func (c *MySqlConnector) PullRecords(
 	defer cancelTimeout()
 
 	var recordCount uint32
+	defer func() {
+		if recordCount == 0 {
+			req.RecordStream.SignalAsEmpty()
+		}
+		c.logger.Info(fmt.Sprintf("[finished] PullRecords streamed %d records", recordCount))
+	}()
+
 	for recordCount < req.MaxBatchSize {
 		event, err := mystream.GetEvent(timeoutCtx)
 		if err != nil {
@@ -346,6 +353,9 @@ func (c *MySqlConnector) PullRecords(
 						}); err != nil {
 							return err
 						}
+						if recordCount == 1 {
+							req.RecordStream.SignalAsNotEmpty()
+						}
 					}
 				case replication.UPDATE_ROWS_EVENTv1, replication.UPDATE_ROWS_EVENTv2, replication.MARIADB_UPDATE_ROWS_COMPRESSED_EVENT_V1:
 					for idx := 0; idx < len(ev.Rows); idx += 2 {
@@ -389,6 +399,9 @@ func (c *MySqlConnector) PullRecords(
 						}); err != nil {
 							return err
 						}
+						if recordCount == 1 {
+							req.RecordStream.SignalAsNotEmpty()
+						}
 					}
 				case replication.DELETE_ROWS_EVENTv1, replication.DELETE_ROWS_EVENTv2, replication.MARIADB_DELETE_ROWS_COMPRESSED_EVENT_V1:
 					for idx, row := range ev.Rows {
@@ -419,6 +432,9 @@ func (c *MySqlConnector) PullRecords(
 							UnchangedToastColumns: unchangedToastColumns,
 						}); err != nil {
 							return err
+						}
+						if recordCount == 1 {
+							req.RecordStream.SignalAsNotEmpty()
 						}
 					}
 				default:
