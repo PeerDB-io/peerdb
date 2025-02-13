b8e0fc1c2caae4f4bbfdd233a36ca1f863c0a69c
Kunal Gupta <39487888+iamKunalGupta@users.noreply.github.com>
2025-02-14 02:57:09 +0530

    chore: lint fix

--- flow/shared/exceptions/ssh.go
+++ flow/shared/exceptions/ssh.go
@@ -1,6 +1,7 @@
 package exceptions
 
-// SSHTunnelSetupError represents errors during SSH Tunnel Setup, the SSH library we use does not provide a common error type, just does fmt.Errorf
+// SSHTunnelSetupError represents errors during SSH Tunnel Setup, the SSH library we use does not provide a common error type,
+// just does fmt.Errorf
 type SSHTunnelSetupError struct {
 	error
 }
