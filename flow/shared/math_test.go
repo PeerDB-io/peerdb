6efadeab5009128f4348b3e5fa47faadd05070cd
Philip Dubé <serprex@users.noreply.github.com>
2025-02-18 03:44:31 +0000

    mysql: pretty size (#2583)

--- flow/shared/math_test.go
+++ flow/shared/math_test.go
@@ -115,16 +115,45 @@ func TestAdjustNumPartitions(t *testing.T) {
 	}
 
 	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			got := AdjustNumPartitions(tc.totalRows, tc.desiredRowsPerPartition)
-			if got.AdjustedNumPartitions != tc.expectedPartitions {
-				t.Errorf("AdjustNumPartitions(totalRows=%d, desiredRowsPerPartition=%d).AdjustedNumPartitions = %d; want %d",
-					tc.totalRows, tc.desiredRowsPerPartition, got.AdjustedNumPartitions, tc.expectedPartitions)
-			}
-			if got.AdjustedNumRowsPerPartition != tc.expectedRowsPerPartition {
-				t.Errorf("AdjustNumPartitions(totalRows=%d, desiredRowsPerPartition=%d).AdjustedNumRowsPerPartition = %d; want %d",
-					tc.totalRows, tc.desiredRowsPerPartition, got.AdjustedNumRowsPerPartition, tc.expectedRowsPerPartition)
-			}
-		})
+		got := AdjustNumPartitions(tc.totalRows, tc.desiredRowsPerPartition)
+		if got.AdjustedNumPartitions != tc.expectedPartitions {
+			t.Errorf("AdjustNumPartitions(totalRows=%d, desiredRowsPerPartition=%d).AdjustedNumPartitions = %d; want %d",
+				tc.totalRows, tc.desiredRowsPerPartition, got.AdjustedNumPartitions, tc.expectedPartitions)
+		}
+		if got.AdjustedNumRowsPerPartition != tc.expectedRowsPerPartition {
+			t.Errorf("AdjustNumPartitions(totalRows=%d, desiredRowsPerPartition=%d).AdjustedNumRowsPerPartition = %d; want %d",
+				tc.totalRows, tc.desiredRowsPerPartition, got.AdjustedNumRowsPerPartition, tc.expectedRowsPerPartition)
+		}
+	}
+}
+
+func TestPrettySize(t *testing.T) {
+	tests := []struct {
+		text string
+		val  int64
+	}{
+		{"1024 bytes", 1024},
+		{"977 kB", 1000000},
+		{"954 MB", 1000000000},
+		{"931 GB", 1000000000000},
+		{"909 TB", 1000000000000000},
+		{"10239 bytes", 10239},
+		{"10 kB", 10240},
+		{"10239 kB", 10485247},
+		{"10 MB", 10485248},
+		{"10239 MB", 10736893951},
+		{"10 GB", 10736893952},
+		{"10239 GB", 10994579406847},
+		{"10 TB", 10994579406848},
+		{"10239 TB", 11258449312612351},
+		{"10 PB", 11258449312612352},
+		{"8192 PB", 9223372036854775807},
+	}
+
+	for _, tc := range tests {
+		got := PrettyBytes(tc.val)
+		if got != tc.text {
+			t.Errorf("PrettySize(%d) = %s; expected %s", tc.val, got, tc.text)
+		}
 	}
 }
