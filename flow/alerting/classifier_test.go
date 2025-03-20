6eb469f58ea6a091b05a59208970194622870446
Kunal Gupta <39487888+iamKunalGupta@users.noreply.github.com>
2025-03-21 04:45:09 +0530

    chore:  switch to require

--- flow/alerting/classifier_test.go
+++ flow/alerting/classifier_test.go
@@ -5,11 +5,12 @@ import (
 
 	"github.com/jackc/pgx/v5"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestPostgresDNSErrorShouldBeConnectivity(t *testing.T) {
 	config, err := pgx.ParseConfig("postgres://non-existent.domain.name.here:123/db")
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	_, err = pgx.ConnectConfig(t.Context(), config)
 	errorClass, errInfo := GetErrorClass(t.Context(), err)
 	assert.Equal(t, ErrorNotifyConnectivity, errorClass, "Unexpected error class")
