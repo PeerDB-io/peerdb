9dc1141a77320e9b725357c19e45d8d5a8af81ea
Philip Dubé <philip.dube@clickhouse.com>
2025-03-20 22:23:42 +0000

    use suite framework

--- flow/e2e/api/api_test.go
+++ flow/e2e/api/api_test.go
@@ -1,12 +1,14 @@
 package e2e_api
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 
+	"github.com/PeerDB-io/peerdb/flow/e2eshared"
 	"github.com/PeerDB-io/peerdb/flow/generated/protos"
 	"github.com/PeerDB-io/peerdb/flow/internal"
 )
@@ -19,19 +21,46 @@ func NewClient(t *testing.T) protos.FlowServiceClient {
 	return protos.NewFlowServiceClient(client)
 }
 
-func TestPostgresValidation(t *testing.T) {
-	client := NewClient(t)
-	config := internal.GetCatalogPostgresConfigFromEnv(t.Context())
+type Suite struct {
+	protos.FlowServiceClient
+	*testing.T
+}
+
+func (s Suite) Teardown(ctx context.Context) {
+}
+
+func TestApi(t *testing.T) {
+	e2eshared.RunSuite(t, func(t *testing.T) Suite {
+		t.Helper()
+		return Suite{FlowServiceClient: NewClient(t), T: t}
+	})
+}
+
+func (s Suite) TestPostgresValidation_WrongPassword() {
+	config := internal.GetCatalogPostgresConfigFromEnv(s.Context())
 	config.Password = "wrong"
-	response, err := client.ValidatePeer(t.Context(), &protos.ValidatePeerRequest{
+	response, err := s.ValidatePeer(s.Context(), &protos.ValidatePeerRequest{
 		Peer: &protos.Peer{
 			Name:   "testfail",
 			Type:   protos.DBType_POSTGRES,
 			Config: &protos.Peer_PostgresConfig{PostgresConfig: config},
 		},
 	})
-	t.Log(response, err)
-	require.NoError(t, err)
-	require.NotNil(t, response)
-	require.Equal(t, protos.ValidatePeerStatus_INVALID, response.Status)
+	require.NoError(s.T, err)
+	require.NotNil(s.T, response)
+	require.Equal(s.T, protos.ValidatePeerStatus_INVALID, response.Status)
+}
+
+func (s Suite) TestPostgresValidation_Pass() {
+	config := internal.GetCatalogPostgresConfigFromEnv(s.Context())
+	response, err := s.ValidatePeer(s.Context(), &protos.ValidatePeerRequest{
+		Peer: &protos.Peer{
+			Name:   "testfail",
+			Type:   protos.DBType_POSTGRES,
+			Config: &protos.Peer_PostgresConfig{PostgresConfig: config},
+		},
+	})
+	require.NoError(s.T, err)
+	require.NotNil(s.T, response)
+	require.Equal(s.T, protos.ValidatePeerStatus_VALID, response.Status)
 }
