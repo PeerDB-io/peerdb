e1eb8e256face8b9e260bfd4009c4cdc3ce4d112
Philip Dubé <philip.dube@clickhouse.com>
2025-03-25 16:53:43 +0000

    fix e2e regression

--- flow/model/qvalue/equals.go
+++ flow/model/qvalue/equals.go
@@ -122,14 +122,24 @@ func Equals(qv QValue, other QValue) bool {
 		return compareNumericArrays(qvValue, otherValue)
 	case QValueArrayDate:
 		return compareDateArrays(q.Val, otherValue)
-	case QValueArrayTimestamp, QValueArrayTimestampTZ:
-		return compareTimeArrays(qvValue, otherValue)
+	case QValueArrayTimestamp:
+		return compareTimeArrays(q.Val, otherValue)
+	case QValueArrayTimestampTZ:
+		return compareTimeArrays(q.Val, otherValue)
 	case QValueArrayBoolean:
-		return compareBoolArrays(q.Val, otherValue)
+		return compareArrays(q.Val, otherValue)
 	case QValueArrayUUID:
-		return compareUuidArrays(q.Val, otherValue)
+		return compareArrays(q.Val, otherValue)
 	case QValueArrayString:
-		return compareArrayString(q.Val, otherValue)
+		if qjson, ok := other.(QValueJSON); ok {
+			var val []string
+			if err := json.Unmarshal([]byte(qjson.Val), &val); err != nil {
+				return false
+			}
+			otherValue = val
+		}
+
+		return compareArrays(q.Val, otherValue)
 	default:
 		return false
 	}
@@ -148,12 +158,7 @@ func compareGoTimestamp(value1, value2 any) bool {
 		return false
 	}
 
-	// TODO: this is a hack, we should be comparing the actual time values
-	// currently this is only used for testing so that is OK.
-	t1 := et1.UnixMicro()
-	t2 := et2.UnixMicro()
-
-	return t1 == t2
+	return et1.UnixMicro() == et2.UnixMicro()
 }
 
 func compareGoTime(value1, value2 any) bool {
@@ -310,81 +315,23 @@ func compareNumericArrays(value1, value2 any) bool {
 	return true
 }
 
-func compareTimeArrays(value1, value2 any) bool {
-	array1, ok1 := value1.([]time.Time)
+func compareDateArrays(array1 []time.Time, value2 any) bool {
 	array2, ok2 := value2.([]time.Time)
-
-	if !ok1 || !ok2 || len(array1) != len(array2) {
-		return false
-	}
-
-	for i := range array1 {
-		if !array1[i].Equal(array2[i]) {
-			return false
-		}
-	}
-	return true
+	return ok2 && slices.EqualFunc(array1, array2, func(x time.Time, y time.Time) bool {
+		return x.Year() == y.Year() && x.Month() == y.Month() && x.Day() == y.Day()
+	})
 }
 
-func compareDateArrays(value1, value2 any) bool {
-	array1, ok1 := value1.([]time.Time)
+func compareTimeArrays(array1 []time.Time, value2 any) bool {
 	array2, ok2 := value2.([]time.Time)
-
-	if !ok1 || !ok2 || len(array1) != len(array2) {
-		return false
-	}
-
-	for i := range array1 {
-		if array1[i].Year() != array2[i].Year() ||
-			array1[i].Month() != array2[i].Month() ||
-			array1[i].Day() != array2[i].Day() {
-			return false
-		}
-	}
-	return true
+	return ok2 && slices.EqualFunc(array1, array2, func(x time.Time, y time.Time) bool {
+		return x.UnixMicro() == y.UnixMicro()
+	})
 }
 
-func compareBoolArrays(value1, value2 any) bool {
-	array1, ok1 := value1.([]bool)
-	array2, ok2 := value2.([]bool)
-
-	if !ok1 || !ok2 || len(array1) != len(array2) {
-		return false
-	}
-
-	for i := range array1 {
-		if array1[i] != array2[i] {
-			return false
-		}
-	}
-	return true
-}
-
-func compareUuidArrays(value1, value2 any) bool {
-	array1, ok1 := value1.([]uuid.UUID)
-	array2, ok2 := value2.([]uuid.UUID)
-
-	if !ok1 || !ok2 || len(array1) != len(array2) {
-		return false
-	}
-
-	for i := range array1 {
-		if array1[i] != array2[i] {
-			return false
-		}
-	}
-	return true
-}
-
-func compareArrayString(value1, value2 any) bool {
-	array1, ok1 := value1.([]string)
-	array2, ok2 := value2.([]string)
-
-	if !ok1 || !ok2 {
-		return false
-	}
-
-	return slices.Compare(array1, array2) == 0
+func compareArrays[T comparable](array1 []T, value2 any) bool {
+	array2, ok2 := value2.([]T)
+	return ok2 && slices.Equal(array1, array2)
 }
 
 func getUInt64(v any) (uint64, bool) {
