5a967d3a1aedee4dd497ae05ac5ecca71c90c68f
Philip Dubé <serprex@users.noreply.github.com>
2025-03-25 00:17:22 +0000

    SF qrep: remove schema changes (#2750)
    Introduced in #595 this feature is untested, caused code duplication,
    & does not handle edge cases like mixed case columns well
    
    QRep schema changes were experimental enough they were never ported to any other connector

--- flow/model/qvalue/qschema.go
+++ flow/model/qvalue/qschema.go
@@ -1,13 +1,7 @@
 package qvalue
 
 import (
-	"context"
-	"fmt"
 	"strings"
-
-	"github.com/PeerDB-io/peerdb/flow/datatypes"
-	"github.com/PeerDB-io/peerdb/flow/generated/protos"
-	"github.com/PeerDB-io/peerdb/flow/internal"
 )
 
 type QField struct {
@@ -49,57 +43,4 @@ func (q QRecordSchema) GetColumnNames() []string {
 		names = append(names, field.Name)
 	}
 	return names
-}
-
-func (q QField) getClickHouseTypeForNumericField(ctx context.Context, env map[string]string) (string, error) {
-	if q.Precision == 0 && q.Scale == 0 {
-		numericAsStringEnabled, err := internal.PeerDBEnableClickHouseNumericAsString(ctx, env)
-		if err != nil {
-			return "", err
-		}
-		if numericAsStringEnabled {
-			return "String", nil
-		}
-	} else if q.Precision > datatypes.PeerDBClickHouseMaxPrecision {
-		return "String", nil
-	}
-	return fmt.Sprintf("Decimal(%d, %d)", q.Precision, q.Scale), nil
-}
-
-// SEE ALSO: qvalue/kind.go ToDWHColumnType
-func (q QField) ToDWHColumnType(
-	ctx context.Context, env map[string]string, dwhType protos.DBType, nullableEnabled bool,
-) (string, error) {
-	var colType string
-	switch dwhType {
-	case protos.DBType_SNOWFLAKE:
-		if val, ok := QValueKindToSnowflakeTypeMap[q.Type]; ok {
-			colType = val
-		} else if q.Type == QValueKindNumeric {
-			colType = fmt.Sprintf("NUMERIC(%d,%d)", q.Precision, q.Scale)
-		} else {
-			colType = "STRING"
-		}
-		if nullableEnabled && !q.Nullable {
-			colType += " NOT NULL"
-		}
-	case protos.DBType_CLICKHOUSE:
-		if val, ok := QValueKindToClickHouseTypeMap[q.Type]; ok {
-			var err error
-			colType, err = q.getClickHouseTypeForNumericField(ctx, env)
-			if err != nil {
-				return "", err
-			}
-		} else if q.Type == QValueKindNumeric {
-			colType = val
-		} else {
-			colType = "String"
-		}
-		if nullableEnabled && q.Nullable && !q.Type.IsArray() {
-			colType = fmt.Sprintf("Nullable(%s)", colType)
-		}
-	default:
-		return "", fmt.Errorf("unknown dwh type: %v", dwhType)
-	}
-	return colType, nil
 }
