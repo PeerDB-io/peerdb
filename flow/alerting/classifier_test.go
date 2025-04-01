86742abd8267a49ebb9a207181961777ccaf4826
Kunal Gupta <39487888+iamKunalGupta@users.noreply.github.com>
2025-04-01 21:42:00 +0530

    feat(classifier): add bigger decimals (#2784)
    fixes #2765

--- flow/alerting/classifier_test.go
+++ flow/alerting/classifier_test.go
@@ -1,8 +1,12 @@
 package alerting
 
 import (
+	"fmt"
+	"strconv"
 	"testing"
 
+	chproto "github.com/ClickHouse/ch-go/proto"
+	"github.com/ClickHouse/clickhouse-go/v2"
 	"github.com/jackc/pgx/v5"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -18,4 +22,24 @@ func TestPostgresDNSErrorShouldBeConnectivity(t *testing.T) {
 		Source: ErrorSourceNet,
 		Code:   "net.DNSError",
 	}, errInfo, "Unexpected error info")
+}
+
+func TestClickHouseAvroDecimalErrorShouldBeUnsupportedDatatype(t *testing.T) {
+	// Simulate an Avro decimal error
+	errCodes := []int{int(chproto.ErrCannotParseUUID), int(chproto.ErrValueIsOutOfRangeOfDataType)}
+	for _, code := range errCodes {
+		t.Run(strconv.Itoa(code), func(t *testing.T) {
+			exception := clickhouse.Exception{
+				Code: int32(code),
+				//nolint:lll
+				Message: "Cannot parse type Decimal(76, 38), expected non-empty binary data with size equal to or less than 32, got 57: (at row 72423)....",
+			}
+			errorClass, errInfo := GetErrorClass(t.Context(), fmt.Errorf("failed to sync records: %w", &exception))
+			assert.Equal(t, ErrorUnsupportedDatatype, errorClass, "Unexpected error class")
+			assert.Equal(t, ErrorInfo{
+				Source: ErrorSourceClickHouse,
+				Code:   strconv.Itoa(code),
+			}, errInfo, "Unexpected error info")
+		})
+	}
 }
