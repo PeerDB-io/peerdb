4668e3dcc5568419d6c23d1ef66b7490b165c49a
Kunal Gupta <39487888+iamKunalGupta@users.noreply.github.com>
2025-04-08 00:57:34 +0530

    chore: more logs

--- flow/activities/maintenance_activity.go
+++ flow/activities/maintenance_activity.go
@@ -156,33 +156,36 @@ func (a *MaintenanceActivity) PauseMirrorIfRunning(ctx context.Context, mirror *
 	if err != nil {
 		logger.Error("Error getting mirror status", "error", err)
 		var notFoundErr *serviceerror.NotFound
-		if errors.As(err, &notFoundErr) && workflowNotFoundMessageRe.MatchString(notFoundErr.Message) &&
+		if errors.As(err, &notFoundErr) && workflowNotFoundMessageRe.MatchString(notFoundErr.Message) {
+			logger.Error("Received a workflow not found error, checking if the workflow is missing and if it is older than 90 days",
+				"error", err, "temporalCertAuth", internal.PeerDBTemporalEnableCertAuth())
 			// This is max temporal retention period, but this is mirror update time, not deletion time, so it is not accurate
-			mirror.MirrorUpdatedAt.AsTime().Before(time.Now().Add(-90*24*time.Hour)) &&
-			// We are in Temporal Cloud
-			internal.PeerDBTemporalEnableCertAuth() {
-			// workflow not found for ID: mirror_d1e3f532__8adb__4f79__9d00__01e44b6bcbfb-peerflow-27144d2c-06ce-4552-87e5-696b3a909702
-			logger.Warn("Workflow not found in Temporal Cloud and mirror update_at is older than 90 days, checking for existing workflows")
-			response, wErr := a.TemporalClient.ListWorkflow(ctx, &workflowservice.ListWorkflowExecutionsRequest{
-				Query: fmt.Sprintf("`MirrorName`=\"%s\"",
-					mirror.MirrorName),
-			})
-			if wErr != nil {
-				logger.Error("Error checking for ANY existing Workflows", "error", wErr)
-				return false, wErr
+			if mirror.MirrorUpdatedAt.AsTime().Before(time.Now().Add(-90*24*time.Hour)) &&
+				// We are in Temporal Cloud
+				internal.PeerDBTemporalEnableCertAuth() {
+				// workflow not found for ID: mirror_d1e3f532__8adb__4f79__9d00__01e44b6bcbfb-peerflow-27144d2c-06ce-4552-87e5-696b3a909702
+				logger.Warn("Workflow not found in Temporal Cloud and mirror update_at is older than 90 days, checking for existing workflows")
+				response, wErr := a.TemporalClient.ListWorkflow(ctx, &workflowservice.ListWorkflowExecutionsRequest{
+					Query: fmt.Sprintf("`MirrorName`=\"%s\"",
+						mirror.MirrorName),
+				})
+				if wErr != nil {
+					logger.Error("Error checking for ANY existing Workflows", "error", wErr)
+					return false, wErr
+				}
+				logger.Info("Received response for ANY existing Workflows check", "len(executions)", len(response.Executions))
+				if len(response.Executions) == 0 {
+					logger.Warn("No existing workflows found, skipping pause")
+					return false, nil
+				}
+				foundWorkflowIds := make([]string, len(response.Executions))
+				for i, exec := range response.Executions {
+					logger.Info("Found existing CDCFlow", "workflowId", exec.GetExecution().GetWorkflowId())
+					foundWorkflowIds[i] = exec.GetExecution().GetWorkflowId()
+				}
+				logger.Warn("Found some existing CDCFlow, this is unexpected and should be investigated",
+					"foundWorkflows", foundWorkflowIds)
 			}
-			logger.Info("Received response for ANY existing Workflows check", "len(executions)", len(response.Executions))
-			if len(response.Executions) == 0 {
-				logger.Warn("No existing workflows found, skipping pause")
-				return false, nil
-			}
-			foundWorkflowIds := make([]string, len(response.Executions))
-			for i, exec := range response.Executions {
-				logger.Info("Found existing CDCFlow", "workflowId", exec.GetExecution().GetWorkflowId())
-				foundWorkflowIds[i] = exec.GetExecution().GetWorkflowId()
-			}
-			logger.Warn("Found some existing CDCFlow, this is unexpected and should be investigated",
-				"foundWorkflows", foundWorkflowIds)
 		}
 		return false, err
 	}
