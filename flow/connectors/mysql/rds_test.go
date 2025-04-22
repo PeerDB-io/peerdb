d6cbef94e0c11d11694037829ebafb27408a6b81
Kunal Gupta <39487888+iamKunalGupta@users.noreply.github.com>
2025-04-23 03:52:12 +0530

    fix(test): try pushing explicity env vars

--- flow/connectors/mysql/rds_test.go
+++ flow/connectors/mysql/rds_test.go
@@ -11,9 +11,9 @@ import (
 )
 
 func TestAwsRDSIAMAuthConnectForMYSQL(t *testing.T) {
-	t.Setenv("AWS_ACCESS_KEY_ID", "")
-	t.Setenv("AWS_SECRET_ACCESS_KEY", "")
-	t.Setenv("AWS_REGION", "")
+	t.Setenv("AWS_ACCESS_KEY_ID", os.Getenv("FLOW_TESTS_RDS_IAM_AUTH_AWS_ACCESS_KEY_ID"))
+	t.Setenv("AWS_SECRET_ACCESS_KEY", os.Getenv("FLOW_TESTS_RDS_IAM_AUTH_AWS_SECRET_ACCESS_KEY"))
+	t.Setenv("AWS_SESSION_TOKEN", os.Getenv("FLOW_TESTS_RDS_IAM_AUTH_AWS_SESSION_TOKEN"))
 	host := os.Getenv("FLOW_TESTS_RDS_IAM_AUTH_HOST_MYSQL")
 	username := os.Getenv("FLOW_TESTS_RDS_IAM_AUTH_USERNAME_MYSQL")
 	mysqlConnector, err := NewMySqlConnector(t.Context(),
