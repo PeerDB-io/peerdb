526bc452083f56f462d45cf32f18120367fac47f
Philip Dubé <philip.dube@clickhouse.com>
2025-05-12 13:48:20 +0000

    wip

---
+++ flow/connectors/vitess/schema.go
@@ -0,0 +1,27 @@
+package connvitess
+
+import (
+	"context"
+	"slices"
+
+	"github.com/PeerDB-io/peerdb/flow/generated/protos"
+)
+
+func (c *VitessConnector) GetSchemas(ctx context.Context) (*protos.PeerSchemasResponse, error) {
+	rs, err := c.Execute(ctx, "SHOW SCHEMAS")
+	if err != nil {
+		return nil, err
+	}
+
+	schemas := make([]string, 0, rs.RowNumber())
+	for idx := range rs.RowNumber() {
+		val, err := rs.GetString(idx, 0)
+		if err != nil {
+			return nil, err
+		}
+		if !slices.Contains([]string{"information_schema", "performance_schema", "sys"}, val) {
+			schemas = append(schemas, val)
+		}
+	}
+	return &protos.PeerSchemasResponse{Schemas: schemas}, nil
+}
