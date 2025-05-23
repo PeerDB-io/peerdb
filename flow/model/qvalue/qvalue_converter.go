e585346b12047081ced8ae7870f6458b1b242b74
Philip Dubé <philip.dube@clickhouse.com>
2025-05-23 19:02:33 +0000

    numeric string: use generics, handle null

--- flow/model/qvalue/qvalue_converter.go
+++ flow/model/qvalue/qvalue_converter.go
@@ -1,12 +1,42 @@
 package qvalue
 
-import "github.com/shopspring/decimal"
+//nolint:iface
+type TypeConversion interface {
+	SchemaConversion(QField) QField
+	ValueConversion(QValue) QValue
+	FromKind() QValueKind
+}
 
-type TypeConversion struct {
+type TypeConversionImpl[TFrom QValue, TTo QValue] struct {
 	SchemaConversionFn func(QField) QField
-	ValueConversionFn  func(QValue) QValue
-	FromKind           QValueKind
-	ToKind             QValueKind
+	ValueConversionFn  func(TFrom) TTo
+}
+
+func (tc TypeConversionImpl[TFrom, TTo]) SchemaConversion(field QField) QField {
+	return tc.SchemaConversionFn(field)
+}
+
+func (tc TypeConversionImpl[TFrom, TTo]) ValueConversion(val QValue) QValue {
+	if _, ok := val.(QValueNull); ok {
+		var toQ TTo
+		return QValueNull(toQ.Kind())
+	}
+	return tc.ValueConversionFn(val.(TFrom))
+}
+
+func (tc TypeConversionImpl[TFrom, TTo]) FromKind() QValueKind {
+	var fromQ TFrom
+	return fromQ.Kind()
+}
+
+func NewTypeConversion[TFrom QValue, TTo QValue](
+	schemaConversionFn func(QField) QField,
+	valueConversionFn func(TFrom) TTo,
+) TypeConversionImpl[TFrom, TTo] {
+	return TypeConversionImpl[TFrom, TTo]{
+		SchemaConversionFn: schemaConversionFn,
+		ValueConversionFn:  valueConversionFn,
+	}
 }
 
 func NumericToStringSchemaConversion(val QField) QField {
@@ -21,6 +51,6 @@ func NumericToStringSchemaConversionNullable(val QField) QField {
 	return val
 }
 
-func NumericToStringValueConversion(val QValue) QValue {
-	return QValue(QValueString{Val: val.Value().(decimal.Decimal).String()})
+func NumericToStringValueConversion(val QValueNumeric) QValueString {
+	return QValueString{Val: val.Val.String()}
 }
