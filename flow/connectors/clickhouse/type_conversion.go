505b1f665f2b3bcd74780789607112f0177667d0
Joy Gao <17896160+jgao54@users.noreply.github.com>
2025-05-29 15:07:50 -1000

    organize code a bit better, no change to implementation

---
+++ flow/connectors/clickhouse/type_conversion.go
@@ -0,0 +1,74 @@
+package connclickhouse
+
+import (
+	"github.com/PeerDB-io/peerdb/flow/generated/protos"
+	"github.com/PeerDB-io/peerdb/flow/model/qvalue"
+)
+
+// add more supported type conversions as needed
+var supportedDestinationTypes = map[string][]qvalue.TypeConversion{
+	"String": {qvalue.NewTypeConversion(
+		qvalue.NumericToStringSchemaConversion,
+		qvalue.NumericToStringValueConversion,
+	)},
+}
+
+func GetColumnsTypeConversion() (*protos.ColumnsTypeConversionResponse, error) {
+	res := make([]*protos.ColumnsTypeConversion, 0)
+	for qkind, destTypes := range listSupportedTypeConversions() {
+		res = append(res, &protos.ColumnsTypeConversion{
+			Qkind:            string(qkind),
+			DestinationTypes: destTypes,
+		})
+	}
+	return &protos.ColumnsTypeConversionResponse{
+		Conversions: res,
+	}, nil
+}
+
+func listSupportedTypeConversions() map[qvalue.QValueKind][]string {
+	typeConversions := make(map[qvalue.QValueKind][]string)
+
+	for dstType, l := range supportedDestinationTypes {
+		for _, conversion := range l {
+			typeConversions[conversion.FromKind()] = append(typeConversions[conversion.FromKind()], dstType)
+		}
+	}
+	return typeConversions
+}
+
+func findTypeConversions(schema qvalue.QRecordSchema, columns []*protos.ColumnSetting) map[string]qvalue.TypeConversion {
+	typeConversions := make(map[string]qvalue.TypeConversion)
+
+	colNameToType := make(map[string]qvalue.QValueKind, len(schema.Fields))
+	for _, field := range schema.Fields {
+		colNameToType[field.Name] = field.Type
+	}
+
+	for _, col := range columns {
+		colType, exist := colNameToType[col.SourceName]
+		if !exist {
+			continue
+		}
+		conversions, exist := supportedDestinationTypes[col.DestinationType]
+		if !exist {
+			continue
+		}
+		for _, conversion := range conversions {
+			if conversion.FromKind() == colType {
+				typeConversions[col.SourceName] = conversion
+			}
+		}
+	}
+
+	return typeConversions
+}
+
+func applyTypeConversions(schema qvalue.QRecordSchema, typeConversions map[string]qvalue.TypeConversion) qvalue.QRecordSchema {
+	for i, field := range schema.Fields {
+		if conversion, exist := typeConversions[field.Name]; exist {
+			schema.Fields[i] = conversion.SchemaConversion(field)
+		}
+	}
+	return schema
+}
