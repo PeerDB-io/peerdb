6a3ef034d62a83669ae45c8bbf0546bb1058c6bb
Philip Dubé <serprex@users.noreply.github.com>
2025-06-06 14:56:17 +0000

    s3: avro codec field (#3015)

--- ui/app/peers/create/[peerType]/schema.ts
+++ ui/app/peers/create/[peerType]/schema.ts
@@ -1,5 +1,6 @@
 import { ehSchema } from '@/components/PeerForms/Eventhubs/schema';
 import {
+  AvroCodec,
   ElasticsearchAuthType,
   MySqlFlavor,
   MySqlReplicationMechanism,
@@ -526,6 +527,12 @@ export const s3Schema = z.object({
       error: () => 'Endpoint must be a string',
     })
     .optional(),
+  codec: z.enum(AvroCodec, {
+    error: (issue) =>
+      issue.input === undefined
+        ? 'Avro codec is required'
+        : 'Avro codec must be one of [Null,Deflate,Snappy,ZStandard]',
+  }),
 });
 
 export const psSchema = z.object({
