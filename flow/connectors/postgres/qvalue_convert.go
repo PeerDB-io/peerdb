PeerDB-io/peerdb

commit 146e61dacfdc3f808b8a7ec24afb2321e90449a9

move pg -> qvalue logic to shared for type conversion (#3012)
Follow-up to https://github.com/PeerDB-io/peerdb/pull/3011.

Move Postgres -> qvalue type mapping logic to shared package so we can
determined qvalue.

This gives us a way to use shared package to directly derive:
1) qvalue kind from postgres type using `PostgresOIDToQValueKind`
2) supported destination types from qvalue using
`ListSupportedTypeConversions`

Test: CI + locally tested a simple flow.

flow/connectors/postgres/qvalue_convert.go
@@ -15,8 +15,9 @@ import (
 	"github.com/lib/pq/oid"
 	"github.com/shopspring/decimal"
 
-	datatypes "github.com/PeerDB-io/peerdb/flow/datatypes"
 	"github.com/PeerDB-io/peerdb/flow/shared"
+	"github.com/PeerDB-io/peerdb/flow/shared/datatypes"
+	"github.com/PeerDB-io/peerdb/flow/shared/postgres"
 	"github.com/PeerDB-io/peerdb/flow/shared/types"
 )
 
@@ -51,106 +52,17 @@ func (c *PostgresConnector) postgresOIDToQValueKind(
 	recvOID uint32,
 	customTypeMapping map[uint32]shared.CustomDataType,
 ) types.QValueKind {
-	switch recvOID {
-	case pgtype.BoolOID:
-		return types.QValueKindBoolean
-	case pgtype.Int2OID:
-		return types.QValueKindInt16
-	case pgtype.Int4OID:
-		return types.QValueKindInt32
-	case pgtype.Int8OID:
-		return types.QValueKindInt64
-	case pgtype.Float4OID:
-		return types.QValueKindFloat32
-	case pgtype.Float8OID:
-		return types.QValueKindFloat64
-	case pgtype.QCharOID:
-		return types.QValueKindQChar
-	case pgtype.TextOID, pgtype.VarcharOID, pgtype.BPCharOID:
-		return types.QValueKindString
-	case pgtype.ByteaOID:
-		return types.QValueKindBytes
-	case pgtype.JSONOID:
-		return types.QValueKindJSON
-	case pgtype.JSONBOID:
-		return types.QValueKindJSONB
-	case pgtype.UUIDOID:
-		return types.QValueKindUUID
-	case pgtype.TimeOID:
-		return types.QValueKindTime
-	case pgtype.DateOID:
-		return types.QValueKindDate
-	case pgtype.CIDROID:
-		return types.QValueKindCIDR
-	case pgtype.MacaddrOID:
-		return types.QValueKindMacaddr
-	case pgtype.InetOID:
-		return types.QValueKindINET
-	case pgtype.TimestampOID:
-		return types.QValueKindTimestamp
-	case pgtype.TimestamptzOID:
-		return types.QValueKindTimestampTZ
-	case pgtype.NumericOID:
-		return types.QValueKindNumeric
-	case pgtype.Int2ArrayOID:
-		return types.QValueKindArrayInt16
-	case pgtype.Int4ArrayOID:
-		return types.QValueKindArrayInt32
-	case pgtype.Int8ArrayOID:
-		return types.QValueKindArrayInt64
-	case pgtype.PointOID:
-		return types.QValueKindPoint
-	case pgtype.Float4ArrayOID:
-		return types.QValueKindArrayFloat32
-	case pgtype.Float8ArrayOID:
-		return types.QValueKindArrayFloat64
-	case pgtype.BoolArrayOID:
-		return types.QValueKindArrayBoolean
-	case pgtype.DateArrayOID:
-		return types.QValueKindArrayDate
-	case pgtype.TimestampArrayOID:
-		return types.QValueKindArrayTimestamp
-	case pgtype.TimestamptzArrayOID:
-		return types.QValueKindArrayTimestampTZ
-	case pgtype.UUIDArrayOID:
-		return types.QValueKindArrayUUID
-	case pgtype.TextArrayOID, pgtype.VarcharArrayOID, pgtype.BPCharArrayOID:
-		return types.QValueKindArrayString
-	case pgtype.JSONArrayOID:
-		return types.QValueKindArrayJSON
-	case pgtype.JSONBArrayOID:
-		return types.QValueKindArrayJSONB
-	case pgtype.IntervalOID:
-		return types.QValueKindInterval
-	case pgtype.TstzrangeOID:
-		return types.QValueKindTSTZRange
-	default:
-		if typeName, ok := c.typeMap.TypeForOID(recvOID); ok {
-			colType := types.QValueKindString
-			if typeData, ok := customTypeMapping[recvOID]; ok {
-				colType = customTypeToQKind(typeData)
-			}
-			if _, warned := c.hushWarnOID[recvOID]; !warned {
-				c.logger.Warn("unsupported field type",
-					slog.Int64("oid", int64(recvOID)), slog.String("typeName", typeName.Name), slog.String("mapping", string(colType)))
-				c.hushWarnOID[recvOID] = struct{}{}
-			}
-			return colType
-		} else {
-			// workaround for some types not being defined by pgtype
-			switch oid.Oid(recvOID) {
-			case oid.T_timetz:
-				return types.QValueKindTimeTZ
-			case oid.T_point:
-				return types.QValueKindPoint
-			default:
-				if typeData, ok := customTypeMapping[recvOID]; ok {
-					return customTypeToQKind(typeData)
-				}
-				return types.QValueKindString
-			}
-		}
+	colType, err := postgres.PostgresOIDToQValueKind(recvOID, customTypeMapping, c.typeMap)
+	_, warned := c.hushWarnOID[recvOID]
+	if err != nil && !warned {
+		c.logger.Warn(
+			"unsupported field type",
+			slog.Int64("oid", int64(recvOID)),
+			slog.String("typeName", err.Error()),
+			slog.String("mapping", string(colType)))
+		c.hushWarnOID[recvOID] = struct{}{}
 	}
+	return colType
 }
 
 func qValueKindToPostgresType(colTypeStr string) string {
@@ -613,31 +525,6 @@ func numericToDecimal(numVal pgtype.Numeric) (types.QValue, error) {
 		return types.QValueNull(types.QValueKindNumeric), nil
 	default:
 		return types.QValueNumeric{Val: decimal.NewFromBigInt(numVal.Int, numVal.Exp)}, nil
-	}
-}
-
-func customTypeToQKind(typeData shared.CustomDataType) types.QValueKind {
-	if typeData.Type == 'e' {
-		if typeData.Delim != 0 {
-			return types.QValueKindArrayEnum
-		} else {
-			return types.QValueKindEnum
-		}
-	}
-
-	if typeData.Delim != 0 {
-		return types.QValueKindArrayString
-	}
-
-	switch typeData.Name {
-	case "geometry":
-		return types.QValueKindGeometry
-	case "geography":
-		return types.QValueKindGeography
-	case "hstore":
-		return types.QValueKindHStore
-	default:
-		return types.QValueKindString
 	}
 }
 

flow/connectors/bigquery/qvalue_convert.go
@@ -5,8 +5,8 @@ import (
 
 	"cloud.google.com/go/bigquery"
 
-	"github.com/PeerDB-io/peerdb/flow/datatypes"
 	"github.com/PeerDB-io/peerdb/flow/generated/protos"
+	"github.com/PeerDB-io/peerdb/flow/shared/datatypes"
 	"github.com/PeerDB-io/peerdb/flow/shared/types"
 )
 

flow/connectors/mysql/qvalue_convert.go
@@ -15,9 +15,9 @@ import (
 	"github.com/shopspring/decimal"
 	geom "github.com/twpayne/go-geos"
 
-	"github.com/PeerDB-io/peerdb/flow/datatypes"
 	"github.com/PeerDB-io/peerdb/flow/generated/protos"
 	"github.com/PeerDB-io/peerdb/flow/shared"
+	"github.com/PeerDB-io/peerdb/flow/shared/datatypes"
 	"github.com/PeerDB-io/peerdb/flow/shared/types"
 )
 

flow/connectors/bigquery/qrep.go
@@ -8,10 +8,10 @@ import (
 
 	"cloud.google.com/go/bigquery"
 
-	"github.com/PeerDB-io/peerdb/flow/datatypes"
 	"github.com/PeerDB-io/peerdb/flow/generated/protos"
 	"github.com/PeerDB-io/peerdb/flow/model"
 	"github.com/PeerDB-io/peerdb/flow/shared"
+	"github.com/PeerDB-io/peerdb/flow/shared/datatypes"
 	"github.com/PeerDB-io/peerdb/flow/shared/types"
 )
 

flow/connectors/mysql/cdc.go
@@ -22,12 +22,12 @@ import (
 	"github.com/PeerDB-io/peerdb/flow/alerting"
 	"github.com/PeerDB-io/peerdb/flow/connectors/utils"
 	"github.com/PeerDB-io/peerdb/flow/connectors/utils/monitoring"
-	"github.com/PeerDB-io/peerdb/flow/datatypes"
 	"github.com/PeerDB-io/peerdb/flow/generated/protos"
 	"github.com/PeerDB-io/peerdb/flow/internal"
 	"github.com/PeerDB-io/peerdb/flow/model"
 	"github.com/PeerDB-io/peerdb/flow/otel_metrics"
 	"github.com/PeerDB-io/peerdb/flow/shared"
+	"github.com/PeerDB-io/peerdb/flow/shared/datatypes"
 	"github.com/PeerDB-io/peerdb/flow/shared/types"
 )
 

flow/connectors/postgres/cdc.go
@@ -23,13 +23,14 @@ import (
 	connmetadata "github.com/PeerDB-io/peerdb/flow/connectors/external_metadata"
 	"github.com/PeerDB-io/peerdb/flow/connectors/utils"
 	"github.com/PeerDB-io/peerdb/flow/connectors/utils/monitoring"
-	geo "github.com/PeerDB-io/peerdb/flow/datatypes"
 	"github.com/PeerDB-io/peerdb/flow/generated/protos"
 	"github.com/PeerDB-io/peerdb/flow/internal"
 	"github.com/PeerDB-io/peerdb/flow/model"
 	"github.com/PeerDB-io/peerdb/flow/otel_metrics"
 	"github.com/PeerDB-io/peerdb/flow/shared"
+	geo "github.com/PeerDB-io/peerdb/flow/shared/datatypes"
 	"github.com/PeerDB-io/peerdb/flow/shared/exceptions"
+	"github.com/PeerDB-io/peerdb/flow/shared/postgres"
 	"github.com/PeerDB-io/peerdb/flow/shared/types"
 )
 
@@ -282,7 +283,7 @@ func (p *PostgresCDCSource) decodeColumnData(
 	} else if dataType == uint32(oid.T_timetz) { // ugly TIMETZ workaround for CDC decoding.
 		return p.parseFieldFromPostgresOID(dataType, string(data), customTypeMapping)
 	} else if typeData, ok := customTypeMapping[dataType]; ok {
-		customQKind := customTypeToQKind(typeData)
+		customQKind := postgres.CustomTypeToQKind(typeData)
 		switch customQKind {
 		case types.QValueKindGeography, types.QValueKindGeometry:
 			wkt, err := geo.GeoValidate(string(data))
@@ -911,7 +912,7 @@ func processRelationMessage[Items model.Items](
 			if qKind == types.QValueKindInvalid {
 				typeName, ok := customTypeMapping[column.DataType]
 				if ok {
-					qKind = customTypeToQKind(typeName)
+					qKind = postgres.CustomTypeToQKind(typeName)
 				}
 			}
 			currRelMap[column.Name] = string(qKind)

flow/connectors/postgres/client.go
@@ -16,10 +16,10 @@ import (
 	"github.com/lib/pq/oid"
 
 	"github.com/PeerDB-io/peerdb/flow/connectors/utils"
-	numeric "github.com/PeerDB-io/peerdb/flow/datatypes"
 	"github.com/PeerDB-io/peerdb/flow/generated/protos"
 	"github.com/PeerDB-io/peerdb/flow/model"
 	"github.com/PeerDB-io/peerdb/flow/shared"
+	numeric "github.com/PeerDB-io/peerdb/flow/shared/datatypes"
 )
 
 const (

flow/connectors/postgres/qrep_query_executor.go
@@ -10,9 +10,9 @@ import (
 	"github.com/jackc/pgx/v5/pgtype"
 	"go.temporal.io/sdk/log"
 
-	"github.com/PeerDB-io/peerdb/flow/datatypes"
 	"github.com/PeerDB-io/peerdb/flow/model"
 	"github.com/PeerDB-io/peerdb/flow/shared"
+	"github.com/PeerDB-io/peerdb/flow/shared/datatypes"
 	"github.com/PeerDB-io/peerdb/flow/shared/types"
 )
 

commit 3a7d4f8fd9b616c112fc9b99ced78bf412f0d2b0

fix small perf regression (#3017)
Undo a small perf regression I accidentally introduced in
https://github.com/PeerDB-io/peerdb/pull/3012.

I assume >99% of the time `PostgresOIDToQValueKind` does not return an
error. O(1) is cheap but not free, and given this function is in the hot
path for CDC, I'll be pedantic about it.

flow/connectors/postgres/qvalue_convert.go
@@ -53,14 +53,15 @@ func (c *PostgresConnector) postgresOIDToQValueKind(
 	customTypeMapping map[uint32]shared.CustomDataType,
 ) types.QValueKind {
 	colType, err := postgres.PostgresOIDToQValueKind(recvOID, customTypeMapping, c.typeMap)
-	_, warned := c.hushWarnOID[recvOID]
-	if err != nil && !warned {
-		c.logger.Warn(
-			"unsupported field type",
-			slog.Int64("oid", int64(recvOID)),
-			slog.String("typeName", err.Error()),
-			slog.String("mapping", string(colType)))
-		c.hushWarnOID[recvOID] = struct{}{}
+	if err != nil {
+		if _, warned := c.hushWarnOID[recvOID]; !warned {
+			c.logger.Warn(
+				"unsupported field type",
+				slog.Int64("oid", int64(recvOID)),
+				slog.String("typeName", err.Error()),
+				slog.String("mapping", string(colType)))
+			c.hushWarnOID[recvOID] = struct{}{}
+		}
 	}
 	return colType
 }