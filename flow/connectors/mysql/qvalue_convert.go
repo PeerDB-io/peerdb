34ada208b2d00889965e8e09ae4b9a6e09a0b0c5
Joy Gao <17896160+jgao54@users.noreply.github.com>
2025-06-09 07:07:22 -1000

    move mysql qvalue logic to shared (#3023)
    Follow up to https://github.com/PeerDB-io/peerdb/pull/3012 -- move MySQL
    -> qvalue type mapping logic to shared package.
    
    Test: CI + locally tested a simple flow with MySQL.

--- flow/connectors/mysql/qvalue_convert.go
+++ flow/connectors/mysql/qvalue_convert.go
@@ -101,67 +101,6 @@ func qkindFromMysql(field *mysql.Field) (types.QValueKind, error) {
 	}
 }
 
-func qkindFromMysqlColumnType(ct string) (types.QValueKind, error) {
-	ct, isUnsigned := strings.CutSuffix(ct, " unsigned")
-	ct, param, _ := strings.Cut(ct, "(")
-	switch strings.ToLower(ct) {
-	case "json":
-		return types.QValueKindJSON, nil
-	case "char", "varchar", "text", "set", "tinytext", "mediumtext", "longtext":
-		return types.QValueKindString, nil
-	case "enum":
-		return types.QValueKindEnum, nil
-	case "binary", "varbinary", "blob", "tinyblob", "mediumblob", "longblob":
-		return types.QValueKindBytes, nil
-	case "date":
-		return types.QValueKindDate, nil
-	case "time":
-		return types.QValueKindTime, nil
-	case "datetime", "timestamp":
-		return types.QValueKindTimestamp, nil
-	case "decimal", "numeric":
-		return types.QValueKindNumeric, nil
-	case "float":
-		return types.QValueKindFloat32, nil
-	case "double":
-		return types.QValueKindFloat64, nil
-	case "tinyint":
-		if strings.HasPrefix(param, "1)") {
-			return types.QValueKindBoolean, nil
-		} else if isUnsigned {
-			return types.QValueKindUInt8, nil
-		} else {
-			return types.QValueKindInt8, nil
-		}
-	case "smallint", "year":
-		if isUnsigned {
-			return types.QValueKindUInt16, nil
-		} else {
-			return types.QValueKindInt16, nil
-		}
-	case "mediumint", "int":
-		if isUnsigned {
-			return types.QValueKindUInt32, nil
-		} else {
-			return types.QValueKindInt32, nil
-		}
-	case "bit":
-		return types.QValueKindUInt64, nil
-	case "bigint":
-		if isUnsigned {
-			return types.QValueKindUInt64, nil
-		} else {
-			return types.QValueKindInt64, nil
-		}
-	case "vector":
-		return types.QValueKindArrayFloat32, nil
-	case "geometry", "point", "polygon", "linestring", "multipoint", "multipolygon", "geomcollection":
-		return types.QValueKindGeometry, nil
-	default:
-		return types.QValueKind(""), fmt.Errorf("unknown mysql type %s", ct)
-	}
-}
-
 func QRecordSchemaFromMysqlFields(tableSchema *protos.TableSchema, fields []*mysql.Field) (types.QRecordSchema, error) {
 	tableColumns := make(map[string]*protos.FieldDescription, len(tableSchema.Columns))
 	for _, col := range tableSchema.Columns {
