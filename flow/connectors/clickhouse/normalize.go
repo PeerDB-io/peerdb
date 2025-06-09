8025276bf4048c6b9874b77efe00733f1f53bce7
Amogh-Bharadwaj <amogh@peerdb.io>
2025-06-09 18:27:58 +0530

    logs in copyavrostage

--- flow/connectors/clickhouse/normalize.go
+++ flow/connectors/clickhouse/normalize.go
@@ -521,11 +521,21 @@ func (c *ClickHouseConnector) copyAvroStagesToDestination(
 	}
 
 	batchIdToLoad := max(lastSyncedBatchIdInRawTable, normBatchID)
+	c.logger.Info("[clickhouse] pushing s3 data to raw table",
+		slog.Int64("BatchID", batchIdToLoad),
+		slog.String("flowJobName", flowJobName),
+		slog.Int64("syncBatchID", syncBatchID))
+
 	for s := batchIdToLoad + 1; s <= syncBatchID; s++ {
 		if err := c.copyAvroStageToDestination(ctx, flowJobName, s, env); err != nil {
 			return fmt.Errorf("failed to copy avro stage to destination: %w", err)
 		}
+		c.logger.Info("[clickhouse] setting last batch id in raw table",
+			slog.Int64("BatchID", s),
+			slog.String("flowJobName", flowJobName))
 		if err := c.SetLastBatchIDInRawTable(ctx, flowJobName, s); err != nil {
+			c.logger.Error("[clickhouse] error while setting last batch id in raw table",
+				slog.Int64("BatchID", s), slog.Any("error", err))
 			return fmt.Errorf("failed to set last batch id in raw table: %w", err)
 		}
 	}
