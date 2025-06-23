6bc8a520bd0b9648e8302699cd201f87194c794d
Philip Dubé <philip.dube@clickhouse.com>
2025-06-23 15:32:51 +0000

    mysql: more accurate bean counting
    cdc: only count row events which aren't ignored
    qrep: meter connection rather than making terrible estimated based on decoded row values

---
+++ flow/connectors/mysql/metered_dialer.go
@@ -0,0 +1,30 @@
+package connmysql
+
+import (
+	"context"
+	"net"
+	"sync/atomic"
+
+	"github.com/go-mysql-org/go-mysql/client"
+)
+
+type MeteredConn struct {
+	net.Conn
+	BytesRead atomic.Int64
+}
+
+func (mc *MeteredConn) Read(b []byte) (int, error) {
+	read, err := mc.Conn.Read(b)
+	mc.BytesRead.Add(int64(read))
+	return read, err
+}
+
+func NewMeteredDialer(innerDialer client.Dialer) client.Dialer {
+	return func(ctx context.Context, network string, address string) (net.Conn, error) {
+		conn, err := innerDialer(ctx, network, address)
+		if err != nil {
+			return conn, err
+		}
+		return &MeteredConn{Conn: conn}, nil
+	}
+}
