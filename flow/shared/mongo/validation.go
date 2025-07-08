3748e684faf27c3adf721601b4b4054cea5a152e
Joy Gao <17896160+jgao54@users.noreply.github.com>
2025-07-08 08:36:08 -1000

    mongo: add validation for (1) storage engine (2) oplog retention (#3168)
    >You can specify the minimum number of hours to preserve an oplog entry
    where
    [mongod](https://www.mongodb.com/docs/manual/reference/program/mongod/#mongodb-binary-bin.mongod)
    only removes an oplog entry if both of the following criteria are met:
    -The oplog has reached the [maximum configured
    size.](https://www.mongodb.com/docs/manual/core/replica-set-oplog/#std-label-replica-set-oplog-sizing)
    -The oplog entry is older than the configured number of hours based on
    the host system clock.
    By default MongoDB does not set a minimum oplog retention period and
    automatically truncates the oplog starting with the oldest entries to
    maintain the configured maximum oplog size.
    
    Seems like a good idea to require a min oplog retention (instead of just
    relying on max oplog size).
    
    Also add a validation for storage engne, as 'wiredTiger' is a
    prerequisite for changestream to work.
    
    Test: tested e2e locally

--- flow/shared/mongo/validation.go
+++ flow/shared/mongo/validation.go
@@ -8,7 +8,10 @@ import (
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
-const MinSupportedVersion = "5.1.0"
+const (
+	MinSupportedVersion    = "5.1.0"
+	MinOplogRetentionHours = 24
+)
 
 type BuildInfo struct {
 	Version string `bson:"version"`
@@ -17,6 +20,19 @@ type BuildInfo struct {
 type ReplSetGetStatus struct {
 	Set     string `bson:"set"`
 	MyState int    `bson:"myState"`
+}
+
+type OplogTruncation struct {
+	OplogMinRetentionHours float64 `bson:"oplogMinRetentionHours"`
+}
+
+type StorageEngine struct {
+	Name string `bson:"name"`
+}
+
+type ServerStatus struct {
+	StorageEngine   StorageEngine   `bson:"storageEngine"`
+	OplogTruncation OplogTruncation `bson:"oplogTruncation"`
 }
 
 func GetBuildInfo(ctx context.Context, client *mongo.Client) (*BuildInfo, error) {
@@ -41,6 +57,20 @@ func GetReplSetGetStatus(ctx context.Context, client *mongo.Client) (*ReplSetGet
 	var status ReplSetGetStatus
 	if err := singleResult.Decode(&status); err != nil {
 		return nil, fmt.Errorf("failed to decode ReplSetGetStatus: %w", err)
+	}
+	return &status, nil
+}
+
+func GetServerStatus(ctx context.Context, client *mongo.Client) (*ServerStatus, error) {
+	singleResult := client.Database("admin").RunCommand(ctx, bson.D{
+		bson.E{Key: "serverStatus", Value: 1},
+	})
+	if singleResult.Err() != nil {
+		return nil, fmt.Errorf("failed to run 'serverStatus' command: %w", singleResult.Err())
+	}
+	var status ServerStatus
+	if err := singleResult.Decode(&status); err != nil {
+		return nil, fmt.Errorf("failed to decode ServerStatus: %w", err)
 	}
 	return &status, nil
 }
