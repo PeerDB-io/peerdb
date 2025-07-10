78ae6e6e1b421e9d6889395041796730ed7239c4
Philip Dubé <philip.dube@clickhouse.com>
2025-07-10 00:05:23 +0000

    nit

--- flow/e2e/clickhouse/peer_flow_ch_test.go
+++ flow/e2e/clickhouse/peer_flow_ch_test.go
@@ -2087,7 +2087,7 @@ func (s ClickHouseSuite) Test_NullEngine() {
 
 	require.NoError(s.t, s.source.Exec(s.t.Context(),
 		fmt.Sprintf(`insert into %s values (3,'cdcresync',1)`, srcFullName)))
-	e2e.EnvWaitForEqualTablesWithNames(env, s, "waiting on initial", srcTableName, "nulltarget", "id,\"key\"")
+	e2e.EnvWaitForEqualTablesWithNames(env, s, "waiting on insert after resync", srcTableName, "nulltarget", "id,\"key\"")
 
 	ch, err = connclickhouse.Connect(s.t.Context(), nil, chPeer)
 	require.NoError(s.t, err)
