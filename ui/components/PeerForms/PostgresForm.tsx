546187063a79dff219c0850ad5f278e715159971
Amogh Bharadwaj <65964360+Amogh-Bharadwaj@users.noreply.github.com>
2025-07-17 22:12:50 +0530

    Update ui/components/PeerForms/PostgresForm.tsx
    Co-authored-by: Philip Dubé <serprex@users.noreply.github.com>

--- ui/components/PeerForms/PostgresForm.tsx
+++ ui/components/PeerForms/PostgresForm.tsx
@@ -243,7 +243,7 @@ export default function PostgresForm({
                         sshParam.label === "Host's Public Key"
                           ? 'hostKey'
                           : (sshParam.label.toLowerCase() as keyof SSHConfig)
-                      ] || ''
+                      ] ?? ''
                     }
                   />
                 )}
