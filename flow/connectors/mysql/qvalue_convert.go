c6fb9ff2eb5be039027703fc278b67989b67bfc5
Ilia Demianenko <ilia.demianenko@clickhouse.com>
2025-09-08 23:49:10 -0700

    Log MySQL QKind

--- flow/connectors/mysql/qvalue_convert.go
+++ flow/connectors/mysql/qvalue_convert.go
@@ -595,9 +595,10 @@ func QValueFromMysqlRowEvent(
 	schemaName := string(ev.Schema)
 	tableName := string(ev.Table)
 	columnName := string(ev.ColumnName[idx])
+	qkindStr := string(qkind)
 
 	err := exceptions.NewMySQLIncompatibleColumnTypeError(
-		fmt.Sprintf("%s.%s", schemaName, tableName), columnName, mytype, fmt.Sprintf("%T", val))
+		fmt.Sprintf("%s.%s", schemaName, tableName), columnName, mytype, fmt.Sprintf("%T", val), qkindStr)
 	logger.Warn(err.Error())
 	return nil, err
 }
--- flow/shared/exceptions/mysql.go
+++ flow/shared/exceptions/mysql.go
@@ -1,26 +1,30 @@
 package exceptions
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type MySQLIncompatibleColumnTypeError struct {
 	TableName  string
 	ColumnName string
 	dataType   string
 	columnType byte
+	qkind      string
 }
 
 func NewMySQLIncompatibleColumnTypeError(
-	tableName string, columnName string, columnType byte, dataType string,
+	tableName string, columnName string, columnType byte, dataType string, qkind string,
 ) *MySQLIncompatibleColumnTypeError {
 	return &MySQLIncompatibleColumnTypeError{
 		TableName:  tableName,
 		ColumnName: columnName,
 		dataType:   dataType,
 		columnType: columnType,
+		qkind:      qkind,
 	}
 }
 
 func (e MySQLIncompatibleColumnTypeError) Error() string {
-	return fmt.Sprintf("Incompatible type for column %s in table %s, expect mysql type %d but data is %s",
-		e.ColumnName, e.TableName, e.columnType, e.dataType)
+	return fmt.Sprintf("Incompatible type for column %s in table %s, expect qkind %s but data is %s (mysql type %d)",
+		e.ColumnName, e.TableName, e.qkind, e.dataType, e.columnType)
 }
