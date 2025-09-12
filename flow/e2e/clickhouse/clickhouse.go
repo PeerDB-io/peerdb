2b611a0690d8795696636505673b5a6bd5f972bf
Amogh-Bharadwaj <amogh@peerdb.io>
2025-09-12 22:27:29 +0530

    remove dead function in test

--- flow/e2e/clickhouse/clickhouse.go
+++ flow/e2e/clickhouse/clickhouse.go
@@ -365,24 +365,6 @@ func (s ClickHouseSuite) queryRawTable(conn clickhouse.Conn, table string, cols 
 	)
 }
 
-func (s ClickHouseSuite) CheckTableIsEmpty(table string) (bool, error) {
-	peer := s.Peer()
-	ch, err := connclickhouse.Connect(s.t.Context(), nil, peer.GetClickhouseConfig())
-	if err != nil {
-		return false, err
-	}
-	defer ch.Close()
-
-	var count int64
-	if err := ch.QueryRow(
-		s.t.Context(),
-		"SELECT EXISTS(SELECT * FROM "+table+")",
-	).Scan(&count); err != nil {
-		return false, err
-	}
-	return count == 0, nil
-}
-
 func SetupSuite[TSource e2e.SuiteSource](
 	t *testing.T,
 	cluster bool,
