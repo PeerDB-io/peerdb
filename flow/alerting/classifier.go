dc1afb46b9f8570644fbc60d8cd0519e9f2cb7dc
Philip Dubé <serprex@users.noreply.github.com>
2025-09-17 22:01:59 +0000

    feedback
    Co-authored-by: Kunal Gupta <39487888+iamKunalGupta@users.noreply.github.com>

--- flow/alerting/classifier.go
+++ flow/alerting/classifier.go
@@ -396,11 +396,7 @@ func GetErrorClass(ctx context.Context, err error) (ErrorClass, ErrorInfo) {
 			return ErrorNotifyOOMSource, pgErrorInfo
 
 		case pgerrcode.QueryCanceled:
-			if pgErr.Message == "canceling statement due to statement timeout" {
-				return ErrorNotifyConnectivity, pgErrorInfo
-			} else {
-				return ErrorRetryRecoverable, pgErrorInfo
-			}
+			return ErrorNotifyConnectivity, pgErrorInfo
 
 		case pgerrcode.DuplicateFile, pgerrcode.DeadlockDetected, pgerrcode.SerializationFailure:
 			return ErrorRetryRecoverable, pgErrorInfo
