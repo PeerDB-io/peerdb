dfbf3590f7aa37000e0f358ef295c179c4465316
Amogh-Bharadwaj <amogh@peerdb.io>
2025-09-18 16:23:04 +0530

    classify missing pkey

--- flow/shared/exceptions/primary_key_and_replica_identity.go
+++ flow/shared/exceptions/primary_key_and_replica_identity.go
@@ -24,10 +24,22 @@ type ReplicaIdentityNothingError struct {
 	Table string
 }
 
-func NewErrReplicaIdentityNothing(table string, cause error) error {
+func NewReplicaIdentityNothingError(table string, cause error) error {
 	return &ReplicaIdentityNothingError{Table: table}
 }
 
 func (e *ReplicaIdentityNothingError) Error() string {
 	return fmt.Sprintf("table %s has replica identity 'n'/NOTHING", e.Table)
+}
+
+type MissingPrimaryKeyError struct {
+	Table string
+}
+
+func NewMissingPrimaryKeyError(table string) error {
+	return &MissingPrimaryKeyError{Table: table}
+}
+
+func (e *MissingPrimaryKeyError) Error() string {
+	return fmt.Sprintf("table %s has no primary keys and does not have REPLICA IDENTITY FULL", e.Table)
 }
