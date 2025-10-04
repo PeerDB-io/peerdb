01442ba3591f437a10cd978b5203bd304478126b
Ilia Demianenko <ilia.demianenko@clickhouse.com>
2025-10-03 22:34:36 -0700

    format all

--- flow/connectors/mongo/mongo.go
+++ flow/connectors/mongo/mongo.go
@@ -20,8 +20,8 @@ import (
 	"github.com/PeerDB-io/peerdb/flow/connectors/utils"
 	"github.com/PeerDB-io/peerdb/flow/generated/protos"
 	"github.com/PeerDB-io/peerdb/flow/internal"
-	"github.com/PeerDB-io/peerdb/flow/shared"
 	peerdb_mongo "github.com/PeerDB-io/peerdb/flow/pkg/mongo"
+	"github.com/PeerDB-io/peerdb/flow/shared"
 )
 
 const (
