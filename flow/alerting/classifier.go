2a06af8cb39fadb4912c1748fcfd4e97dc46b802
Ilia Demianenko <ilia.demianenko@clickhouse.com>
2025-11-15 20:56:41 -0800

    Classify too large string for FixedString

--- flow/alerting/classifier.go
+++ flow/alerting/classifier.go
@@ -699,7 +699,11 @@ func GetErrorClass(ctx context.Context, err error) (ErrorClass, ErrorInfo) {
 			Code:   strconv.Itoa(int(chException.Code)),
 		}
 		switch chproto.Error(chException.Code) {
-		case chproto.ErrUnknownTable, chproto.ErrNoSuchColumnInTable:
+		case chproto.ErrUnknownTable,
+			chproto.ErrNoSuchColumnInTable,
+			// Too large string for FixedString column: (at row 10195)
+			// The only one created by us is FixedString(1) for PG QChar so assuming the user did it for a string and it didn't work
+			chproto.ErrTooLargeStringSize:
 			if isClickHouseMvError(chException) {
 				return ErrorNotifyMVOrView, chErrorInfo
 			}
