e25e13a3ebe5ac15b374bd74431620b5142980d2
Joy Gao <17896160+jgao54@users.noreply.github.com>
2025-12-05 09:52:34 -1000

    Test mysql 9.5 (gtid) and 5.7 (filepos)  (#3767)
    Testing mysql 5.7 uncovered a number of scenarios that are not properly
    supported by (1) older version of mysql (2) when binlog_row_metadata is
    set to MINIMAL.
    
    Lacking `binlog_row_metadata` means no proper support for:
    - Remove column
    - Ghost schema changes
    - Column exclusion
    - Enum
    
    Other things learned:
    - mysql5 does not support utf8mb4 in JSON, so emoji doesn't show up
    properly
    - only mysql9+ support Vector data type
    
    Conclusion is that supporting `MINIMAL` opens up a can of worms in terms
    of edge cases. While we support mysql 5 due to its popularity, it is EOL
    since 2 years ago, and it's not a great idea to allow 'MINIMAL' for
    mysql 8+.
    
    
    In the process, did small refactoring of mysql setup.

--- flow/e2e/clickhouse_mysql_test.go
+++ flow/e2e/clickhouse_mysql_test.go
@@ -11,6 +11,7 @@ import (
 	connclickhouse "github.com/PeerDB-io/peerdb/flow/connectors/clickhouse"
 	"github.com/PeerDB-io/peerdb/flow/generated/protos"
 	"github.com/PeerDB-io/peerdb/flow/pkg/clickhouse"
+	mysql_validation "github.com/PeerDB-io/peerdb/flow/pkg/mysql"
 	"github.com/PeerDB-io/peerdb/flow/shared"
 	"github.com/PeerDB-io/peerdb/flow/shared/datatypes"
 	"github.com/PeerDB-io/peerdb/flow/shared/types"
@@ -220,8 +221,14 @@ func (s ClickHouseSuite) Test_MySQL_Blobs() {
 }
 
 func (s ClickHouseSuite) Test_MySQL_Enum() {
-	if _, ok := s.source.(*MySqlSource); !ok {
+	if mySource, ok := s.source.(*MySqlSource); !ok {
 		s.t.Skip("only applies to mysql")
+	} else {
+		cmp, err := mySource.CompareServerVersion(s.t.Context(), mysql_validation.MySQLMinVersionForBinlogRowMetadata)
+		require.NoError(s.t, err)
+		if cmp < 0 {
+			s.t.Skip("only applies to mysql versions with binlog_row_metadata")
+		}
 	}
 
 	srcTableName := "test_my_enum"
@@ -265,8 +272,15 @@ func (s ClickHouseSuite) Test_MySQL_Enum() {
 }
 
 func (s ClickHouseSuite) Test_MySQL_Vector() {
-	if mysource, ok := s.source.(*MySqlSource); !ok || mysource.Config.Flavor != protos.MySqlFlavor_MYSQL_MYSQL {
+	mysource, ok := s.source.(*MySqlSource)
+	if !ok || mysource.Config.Flavor != protos.MySqlFlavor_MYSQL_MYSQL {
 		s.t.Skip("only applies to mysql")
+	}
+
+	if cmp, err := mysource.CompareServerVersion(s.t.Context(), "9.0.0"); err != nil {
+		s.t.Fatal(err)
+	} else if cmp < 0 {
+		s.t.Skip("VECTOR type is only supported in MySQL 9.0+")
 	}
 
 	srcTableName := "test_vector"
@@ -669,8 +683,14 @@ func (s ClickHouseSuite) Test_MySQL_Schema_Changes() {
 }
 
 func (s ClickHouseSuite) Test_MySQL_GhOst_Schema_Changes() {
-	if _, ok := s.source.(*MySqlSource); !ok {
+	if mySource, ok := s.source.(*MySqlSource); !ok {
 		s.t.Skip("only applies to mysql")
+	} else {
+		cmp, err := mySource.CompareServerVersion(s.t.Context(), mysql_validation.MySQLMinVersionForBinlogRowMetadata)
+		require.NoError(s.t, err)
+		if cmp < 0 {
+			s.t.Skip("only applies to mysql versions with binlog_row_metadata support")
+		}
 	}
 
 	t := s.T()
