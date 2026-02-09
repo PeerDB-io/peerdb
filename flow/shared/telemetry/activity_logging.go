641e694555392dc7bfe7d5da0209bc7888809333
Ilia Demianenko <ilia.demianenko@clickhouse.com>
2026-02-09 12:25:08 -0800

    Log table adds/removals upfront

--- flow/shared/telemetry/activity_logging.go
+++ flow/shared/telemetry/activity_logging.go
@@ -51,10 +51,6 @@ func LogActivityTerminateFlow(ctx context.Context, flowName string) {
 	logActivity(ctx, ActionTerminateFlow, slog.String("flowName", flowName))
 }
 
-func LogActivityStartFlowConfigUpdate(ctx context.Context, flowName string) {
-	logActivity(ctx, ActionStartFlowConfigUpdate, slog.String("flowName", flowName))
-}
-
 func LogActivityStartMaintenance(ctx context.Context) {
 	logActivity(ctx, ActionStartMaintenance)
 }
@@ -73,6 +69,31 @@ func LogActivityCreatePeer(ctx context.Context) {
 
 func LogActivityDropPeer(ctx context.Context) {
 	logActivity(ctx, ActionDropPeer)
+}
+
+func LogActivityStartFlowConfigUpdate(ctx context.Context, flowName string, update *protos.CDCFlowConfigUpdate) {
+	var changes []string
+
+	// Only logging fields that don't need old values to compare to
+	if len(update.AdditionalTables) > 0 {
+		var addedTables []string
+		for _, t := range update.AdditionalTables {
+			addedTables = append(addedTables, t.SourceTableIdentifier)
+		}
+		changes = append(changes, fmt.Sprintf("tables added: %v", addedTables))
+	}
+
+	if len(update.RemovedTables) > 0 {
+		var removedTables []string
+		for _, t := range update.RemovedTables {
+			removedTables = append(removedTables, t.SourceTableIdentifier)
+		}
+		changes = append(changes, fmt.Sprintf("tables removed: %v", removedTables))
+	}
+
+	logActivity(ctx, ActionStartFlowConfigUpdate,
+		slog.String("flowName", flowName),
+		slog.String("activityDetails", strings.Join(changes, ", ")))
 }
 
 type OldCDCFlowValues struct {
