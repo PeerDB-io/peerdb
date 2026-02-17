2d049a2a3944cbb095a4547696faf3125b149d01
Joy Gao <17896160+jgao54@users.noreply.github.com>
2026-02-17 11:32:22 -1000

    Revert "Revert TIME64 PRs again (#3923)"
    This reverts commit 1c6cd1684cb421c65879257e9cfef796370298a4.

--- flow/connectors/clickhouse/table_function_test.go
+++ flow/connectors/clickhouse/table_function_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/PeerDB-io/peerdb/flow/generated/protos"
+	chinternal "github.com/PeerDB-io/peerdb/flow/internal/clickhouse"
 	"github.com/PeerDB-io/peerdb/flow/shared/types"
 )
 
@@ -33,22 +34,26 @@ func TestBuildInsertFromTableFunctionQuery(t *testing.T) {
 	}
 
 	tableFunctionExpr := "s3('s3://bucket/key', 'format')"
-	chSettings := NewCHSettings(&chproto.Version{Major: 25, Minor: 8})
-	chSettings.Add(SettingTypeJsonSkipDuplicatedPaths, "1")
+	chSettings := chinternal.NewCHSettings(&chproto.Version{Major: 25, Minor: 8})
+	chSettings.Add(chinternal.SettingTypeJsonSkipDuplicatedPaths, "1")
 
 	// without partitioning
 	query, err := buildInsertFromTableFunctionQuery(ctx, config, tableFunctionExpr, chSettings)
 	require.NoError(t, err)
-	require.Equal(t, fmt.Sprintf("INSERT INTO `t1`(`id`,`name`) SELECT `id`,`name` FROM s3('s3://bucket/key', 'format') SETTINGS %s=%s",
-		string(SettingTypeJsonSkipDuplicatedPaths), "1"), query)
+	require.Equal(t,
+		fmt.Sprintf("INSERT INTO `t1`(`id`,`name`) SELECT `id`,`name` FROM s3('s3://bucket/key', 'format') SETTINGS %s=%s",
+			string(chinternal.SettingTypeJsonSkipDuplicatedPaths), "1"),
+		query)
 
 	// with partitioning
 	totalPartitions := uint64(8)
 	for idx := range totalPartitions {
 		query, err := buildInsertFromTableFunctionQueryWithPartitioning(ctx, config, tableFunctionExpr, idx, totalPartitions, chSettings)
 		require.NoError(t, err)
-		require.Equal(t, query,
+		require.Equal(t,
 			"INSERT INTO `t1`(`id`,`name`) SELECT `id`,`name` FROM s3('s3://bucket/key', 'format')"+
-				fmt.Sprintf(" WHERE cityHash64(`id`) %% 8 = %d SETTINGS %s=%s", idx, string(SettingTypeJsonSkipDuplicatedPaths), "1"))
+				fmt.Sprintf(" WHERE cityHash64(`id`) %% 8 = %d SETTINGS %s=%s",
+					idx, string(chinternal.SettingTypeJsonSkipDuplicatedPaths), "1"),
+			query)
 	}
 }
