2a5c8a0d03ab1bd245284093fe032c49898d3e13
Ilia Demianenko <ilia.demianenko@clickhouse.com>
2026-02-21 02:27:53 -0800

    Fix lint: use GetStoredCredentialsWithError, nolint iface on NewUpstream

--- flow/switchboard/upstream.go
+++ flow/switchboard/upstream.go
@@ -90,7 +90,6 @@ func (e *UpstreamError) Error() string {
 }
 
 // NewUpstream creates an upstream connection based on peer configuration
-//
 //nolint:iface // single impl now, more coming in subsequent PRs
 func NewUpstream(ctx context.Context, catalogPool shared.CatalogPool, peerName string, queryTimeout time.Duration) (Upstream, error) {
 	if peerName == "" {
