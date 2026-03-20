14cd498ede3ec8dc3745bcebbac24f53bf85e26c
Ilia Demianenko <ilia.demianenko@clickhouse.com>
2026-03-19 18:39:48 -0700

    Share CH ordering keys validation

---
+++ flow/pkg/clickhouse/engine.go
@@ -0,0 +1,10 @@
+package clickhouse
+
+const (
+	EngineReplacingMergeTree           = "ReplacingMergeTree"
+	EngineMergeTree                    = "MergeTree"
+	EngineNull                         = "Null"
+	EngineReplicatedReplacingMergeTree = "ReplicatedReplacingMergeTree"
+	EngineReplicatedMergeTree          = "ReplicatedMergeTree"
+	EngineCoalescingMergeTree          = "CoalescingMergeTree"
+)
