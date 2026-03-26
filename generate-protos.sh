6cb307fd1cc2138ef5164b19cec95d0a81e513d3
Pablo Francisco Pérez Hidalgo <pablo.perez@clickhouse.com>
2026-03-26 13:11:52 +0100

    Clean-up

--- generate-protos.sh
+++ generate-protos.sh
@@ -2,12 +2,12 @@
 set -xeu
 
 # check if buf is installed
-#if ! command -v buf &> /dev/null
-#then
-#    echo "buf could not be found"
-#    echo "Please install buf: https://buf.build/docs/installation"
-#    exit
-#fi
+if ! command -v buf &> /dev/null
+then
+    echo "buf could not be found"
+    echo "Please install buf: https://buf.build/docs/installation"
+    exit
+fi
 
 buf generate protos
 
