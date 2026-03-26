e9c5d3fa533772fd12a22a7e2419790a39240406
Joy Gao <17896160+jgao54@users.noreply.github.com>
2026-03-25 15:17:08 -1000

    support ctid partitioning for inherited tables (#4084)
    - implement getInheritedTables to recursively discover inherited tables
    and their block count, similar to partitioned tables, except that in
    this case the root table's blocks are also included
    - updated SQL query logic for inherited tables: 
    - explicitly list the column names rather than using `*` for when
    childTableRanges is not nil (i.e. when it's inherited/partitioned table)
    in case child table has additional columns not present in parent. This
    is consistent with the behavior today using NTILE.
    - use FROM ONLY instead of FROM for querying inherited/partitioned
    tables to avoid double-counting for inherited tables; this does not
    change behavior for partitioned tables.
    - note that for inherited tables, CHildTableRanges include all
    partitions (including the root table), so like partitioned tables, the
    Range field in QRepPartition will be nil.
    
    
    Testing:
    - Add e2e tests for single-level and multi-level inherited tables, as
    well as scenario where child table contains extra columns.
    - Run locally e2e (identified a separate bug where CDC ignores change
    events from child inherited tables, but this bug existed before this PR)

--- flow/connectors/postgres/qrep.go
+++ flow/connectors/postgres/qrep.go
@@ -230,16 +230,19 @@ func (c *PostgresConnector) getPartitions(
 	}
 
 	var partitionFunc PartitioningFunc
+	var partitionFuncName string
 	switch {
 	case isCTIDWatermarkCol && (hasCTIDOverride || hasPartitionOverride):
 		partitionFunc = CTIDBlockPartitioningFunc
+		partitionFuncName = "CTIDBlockPartitioningFunc"
 	case hasPartitionOverride:
 		partitionFunc = MinMaxRangePartitioningFunc
+		partitionFuncName = "MinMaxRangePartitioningFunc"
 	default:
 		partitionFunc = NTileBucketPartitioningFunc
+		partitionFuncName = "NTileBucketPartitioningFunc"
 	}
-
-	c.logger.Info("using partition function", slog.String("partitionFunc", fmt.Sprintf("%T", partitionFunc)))
+	c.logger.Info("using partition function", slog.String("partitionFunc", partitionFuncName))
 	return partitionFunc(ctx, partitionParams)
 }
 
@@ -368,7 +371,7 @@ func corePullQRepRecords(
 	partitionIdLog := slog.String(string(shared.PartitionIDKey), partition.PartitionId)
 
 	selectedColumns := "*"
-	if len(config.Exclude) != 0 {
+	if len(config.Exclude) != 0 || len(partition.ChildTableRanges) > 0 {
 		tableSchema, err := internal.LoadTableSchemaFromCatalog(ctx, catalogPool, config.ParentMirrorName, config.DestinationTableIdentifier)
 		if err != nil {
 			return 0, 0, fmt.Errorf("failed to load table schema: %w", err)
@@ -507,7 +510,8 @@ func pullChildTableRanges(
 			return 0, 0, fmt.Errorf("failed to parse child table %s: %w", child.Table, err)
 		}
 
-		query := fmt.Sprintf("SELECT %s FROM %s WHERE %s BETWEEN $1 AND $2",
+		// ONLY excludes rows from child tables, ensuring we don't double-count inherited rows.
+		query := fmt.Sprintf("SELECT %s FROM ONLY %s WHERE %s BETWEEN $1 AND $2",
 			selectedColumns, parsedChild.String(), quotedWatermarkColumn)
 
 		rangeStart := pgtype.TID{
