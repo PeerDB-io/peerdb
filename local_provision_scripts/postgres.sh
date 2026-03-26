2d8a2ccc2a548cae48d8d1f53f6ad13455e71cab
Pablo Francisco Pérez Hidalgo <pablo.perez@clickhouse.com>
2026-03-26 13:26:55 +0100

    Make provision script idempotent.

--- local_provision_scripts/postgres.sh
+++ local_provision_scripts/postgres.sh
@@ -9,9 +9,11 @@ DOCKER="docker"
 CONTAINER="postgres"
 
 echo "install pgvector extension"
-$DOCKER exec "$CONTAINER" apk add --no-cache build-base git
-$DOCKER exec "$CONTAINER" git clone --branch v0.8.1 https://github.com/pgvector/pgvector.git /tmp/pgvector
-$DOCKER exec "$CONTAINER" sh -c 'cd /tmp/pgvector && make with_llvm=no && make with_llvm=no install'
+if ! $DOCKER exec "$CONTAINER" test -d /tmp/pgvector; then
+  $DOCKER exec "$CONTAINER" apk add --no-cache build-base git
+  $DOCKER exec "$CONTAINER" git clone --branch v0.8.1 https://github.com/pgvector/pgvector.git /tmp/pgvector
+  $DOCKER exec "$CONTAINER" sh -c 'cd /tmp/pgvector && make with_llvm=no && make with_llvm=no install'
+fi
 
 echo "create extensions and configure replication"
 $DOCKER exec "$CONTAINER" psql -U "$PG_USER" -d "$PG_DATABASE" \
@@ -23,6 +25,9 @@ $DOCKER exec "$CONTAINER" psql -U "$PG_USER" -d "$PG_DATABASE" \
   -c "ALTER SYSTEM SET max_connections=2048;"
 
 echo "restart postgres to apply config changes"
-$DOCKER restart "$CONTAINER"
+CURRENT_WAL=$($DOCKER exec "$CONTAINER" psql -U "$PG_USER" -d "$PG_DATABASE" -tAc "SHOW wal_level;")
+if [ "$CURRENT_WAL" != "logical" ]; then
+  $DOCKER restart "$CONTAINER"
+fi
 
 echo "PostgreSQL is ready at ${PG_HOST}:${PG_PORT}"
