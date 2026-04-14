c7f7ba75459feed12b028a8b22ae80315e0dc621
Joy Gao <17896160+jgao54@users.noreply.github.com>
2026-04-14 08:40:22 -1000

    nit

--- flow/otel_metrics/otel_manager.go
+++ flow/otel_metrics/otel_manager.go
@@ -80,7 +80,6 @@ const (
 	UnchangedToastValuesCounterName      = "unchanged_toast_values"
 	CodeNotificationCounterName          = "code_notification"
 	ServerWalEndLagGaugeName             = "wal_end_lag"
-	ClickHouseDDLExecutionGaugeName      = "clickhouse_ddl_execution_duration"
 )
 
 type Metrics struct {
