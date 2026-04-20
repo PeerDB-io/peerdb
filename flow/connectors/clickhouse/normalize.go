ed5080c4228bf561bd5b63548985a0082086549b
Joy Gao <17896160+jgao54@users.noreply.github.com>
2026-04-20 09:35:44 -1000

    bugfix to ensure resync cannot fail due to table size too large (#4196)
    Scenario this change fixes:
    
    1. User resynced table - a typical resync writes to a _resync table and
    at the end exchange it with the original table, and drop the old table.
    2. Before the resync fully complete, user triggered resync again, this
    can leave the previous *_resync table with 1TB of data
    3. during the second resync, the CREATE OR REPLACE table during setup
    fails. This under the hood does the following:
        a. create a new table with name _tmp_replace_*
        b. exchange new table with old table
        c.  drop old table
    4. if the old table exceeds `max_table_size_to_drop`, 3c fails and the
    _tmp_replace_* table is orphaned.
    5. on retry, the swapped *_resync table is empty and CREATE OR REPLACE
    succeeds
    
    The fix ensures drop would not fail because table is too large. Arguably
    cleaner if we make this a client-level setting, but safer to assess it
    on the per-query level since we are authorizing dropping 1TB+ table.
    
    Fixes: DBI-698

--- flow/connectors/clickhouse/normalize.go
+++ flow/connectors/clickhouse/normalize.go
@@ -267,9 +267,18 @@ func (c *ClickHouseConnector) generateCreateTableSQLForNormalizedTable(
 			}
 		}
 
+		chSettings := chinternal.NewCHSettings(chVersion)
 		if allowNullableKey {
-			stmtBuilder.WriteString(chinternal.NewCHSettingsString(chVersion, chinternal.SettingAllowNullableKey, "1"))
+			chSettings.Add(chinternal.SettingAllowNullableKey, "1")
 		}
+		if config.IsResync {
+			// CREATE OR REPLACE TABLE (used by resync) internally would create a NEW table with name
+			// <_tmp_replace_*>, atomic swap with the existing table, and then drop the existing table.
+			// Setting max_table_size_to_drop = 0 ensure the implicit drop can't fail, since it can be
+			// non-empty if a resync is triggered on top of another resync that is mid-snapshot.
+			chSettings.Add(chinternal.SettingMaxTableSizeToDrop, "0")
+		}
+		stmtBuilder.WriteString(chSettings.String())
 
 		if c.Config.Cluster != "" {
 			fmt.Fprintf(&stmtBuilderDistributed, " ENGINE = Distributed(%s,%s,%s",
