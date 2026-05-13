1bc13e684651244c59e6480e25b2c54953524a8c
Pablo Francisco Pérez Hidalgo <pablo.perez@clickhouse.com>
2026-05-13 19:13:12 +0200

    Fix linting issues

--- flow/e2e/clickhouse_test.go
+++ flow/e2e/clickhouse_test.go
@@ -2647,17 +2647,17 @@ func (s ClickHouseSuite) Test_NullEngine() {
 	env = ExecutePeerflow(s.t, tc, flowConnConfig)
 	SetupCDCFlowStatusQuery(s.t, env, flowConnConfig)
 
-	// Block to work around changes in ClickHouse by which EXCHANGE and CREARE OR REPLACE orphan source tables.
+	// TODO: Revert this block after future CH behaviour is clarified.
+	// Work around CH 26.5+ behavior: EXCHANGE TABLES and CREATE OR REPLACE on a source table
+	// orphan its dependent materialized views.
 	ch, err = connclickhouse.Connect(s.t.Context(), nil, chPeer)
 	require.NoError(s.t, err)
-	require.NoError(s.t, ch.Exec(s.t.Context(),
-		`drop view nullmv`))
+	require.NoError(s.t, ch.Exec(s.t.Context(), `DROP VIEW IF EXISTS nullmv`))
 	require.NoError(s.t, ch.Exec(s.t.Context(),
 		`create materialized view nullmv to nulltarget as select id, "key", _peerdb_is_deleted from test_nullengine`))
 	require.NoError(s.t, ch.Close())
 
-	require.NoError(s.t, s.source.Exec(s.t.Context(),
-		fmt.Sprintf(`TRUNCATE TABLE %s`, srcFullName)))
+	require.NoError(s.t, s.source.Exec(s.t.Context(), `TRUNCATE TABLE `+srcFullName))
 	// end of workaround block
 
 	EnvWaitForEqualTablesWithNames(env, s, "waiting on initial", srcTableName, "nulltarget", "id,\"key\"")
