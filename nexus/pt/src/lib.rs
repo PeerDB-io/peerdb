2c82c1690d936d8b47b439d909802b3c8191171f
Philip Dubé <159546+serprex@users.noreply.github.com>
2026-06-13 17:14:54 +0000

    npm update, cargo update (#4419)
    update to neoeinstein-prost 0.5 for buf,
    allowing us to upgrade to prost 0.14 & tonic 0.14
    
    configure aws-sdk-kms to no longer bring in legacy hyper 0.14
    
    reqwest staying on 0.12 until
    https://github.com/lquerel/gcp-bigquery-client/pull/134

--- nexus/pt/src/lib.rs
+++ nexus/pt/src/lib.rs
@@ -5,13 +5,13 @@ use peerdb_peers::DbType;
 
 pub mod flow_model;
 #[rustfmt::skip]
-#[path ="./gen/peerdb_flow.rs"]
+#[path ="./gen/peerdb_flow/peerdb_flow.rs"]
 pub mod peerdb_flow;
 #[rustfmt::skip]
-#[path ="./gen/peerdb_peers.rs"]
+#[path ="./gen/peerdb_peers/peerdb_peers.rs"]
 pub mod peerdb_peers;
 #[rustfmt::skip]
-#[path ="./gen/peerdb_route.rs"]
+#[path ="./gen/peerdb_route/peerdb_route.rs"]
 pub mod peerdb_route;
 
 pub use prost;
