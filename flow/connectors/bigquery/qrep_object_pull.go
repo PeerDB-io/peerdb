91b8663045bf852e378ad75d6c44a21b252d5cdf
masterashu <ashutosh.chauhan@clickhouse.com>
2026-06-23 18:44:19 +0530

    fix lint error

--- flow/connectors/bigquery/qrep_object_pull.go
+++ flow/connectors/bigquery/qrep_object_pull.go
@@ -507,6 +507,9 @@ func (c *BigQueryConnector) PullFlowCleanup(context.Context, string) error {
 	return nil
 }
 
-func (c *BigQueryConnector) SetupReplication(context.Context, shared.CatalogPool, *protos.SetupReplicationInput) (model.SetupReplicationResult, error) {
+func (c *BigQueryConnector) SetupReplication(
+	context.Context, shared.CatalogPool,
+	*protos.SetupReplicationInput,
+) (model.SetupReplicationResult, error) {
 	return model.SetupReplicationResult{}, nil
 }
