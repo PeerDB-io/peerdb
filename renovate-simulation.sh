f30d169f3f580fbf4972e8e33582a5d032399618
Pablo Francisco Pérez Hidalgo <pablo.perez@clickhouse.com>
2026-07-07 12:37:44 +0200

    Renovate simulation script

---
+++ renovate-simulation.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# This scripts runs Renovate locally to validate renovate.json5 configuration file
+# It generates the folloing files:
+# - `renovate.out` With the raw debug output of the run.
+# - `update-proposals.json` with the list of proposed updates but INCLUDING DISABLED ones
+# - `final-updated-packages.txt` with the list of packages with updates in the resulting PRs (considering disabled/enabled).
+#
+# WARNING: If the script is terminated before restoring the extension source (step 2), make sure you revert
+#          the changes to renovate.json5 manually.
+
+## (1) Check config syntax
+npx --yes --package renovate -- renovate-config-validator renovate.json5 || exit 1
+
+## (2) Temporarily change the renovate.json5 to point to the local extension source
+sed -i 's/local>PeerDB-io/github>PeerDB-io/' renovate.json5
+
+function cleanup {
+  echo "Restoring renovate.json5 to point to the GitHub extension source"
+  sed -i 's/github>PeerDB-io/local>PeerDB-io/' renovate.json5
+}
+trap cleanup EXIT
+
+## (3) Run Renovate locally
+echo "Running Renovate locally..."
+LOG_LEVEL=debug npx renovate --platform=local --require-config=optional --repository-cache=reset > renovate.out
+
+## (4) Extract update proposals
+cat renovate.out | sed '1,/packageFiles with updates/d' | sed 's/"config": {/{/' | sed '/DEBUG/,$d' | jq '.gomod[].deps[]' > update-proposals.json
+
+## (5) Extract final updated packages
+cat renovate.out | grep 'flattened updates found' | tr ',' '\n' > final-updated-packages.txt
+
+echo "List of packages with proposed updates:"
+echo "----------------------------------------"
+cat final-updated-packages.txt
