b1dd07e347d9e1752ac113179dd2527e0383085e
Amogh-Bharadwaj <amogh@peerdb.io>
2026-07-28 00:34:42 +0530

    feat: support ingesting to generated columns

--- flow/connectors/postgres/client.go
+++ flow/connectors/postgres/client.go
@@ -65,7 +65,7 @@ const (
 		FROM src_rank WHERE _peerdb_rank=1 ORDER BY _peerdb_timestamp) src
 	ON %s
 	WHEN NOT MATCHED AND src._peerdb_record_type!=2 THEN
-	INSERT (%s) VALUES (%s) %s
+	INSERT (%s) OVERRIDING SYSTEM VALUE VALUES (%s) %s
 	WHEN MATCHED AND src._peerdb_record_type=2 THEN %s`
 	mergeStatementSQL = `WITH src_rank AS (
 		SELECT _peerdb_data,_peerdb_record_type,_peerdb_unchanged_toast_columns, _peerdb_timestamp,
@@ -77,14 +77,14 @@ const (
 		FROM src_rank WHERE _peerdb_rank=1 ORDER BY _peerdb_timestamp) src
 	ON %s
 	WHEN NOT MATCHED AND src._peerdb_record_type!=2 THEN
-	INSERT (%s) VALUES (%s) %s
+	INSERT (%s) OVERRIDING SYSTEM VALUE VALUES (%s) %s
 	WHEN MATCHED AND src._peerdb_record_type=2 THEN %s`
 	fallbackUpsertStatementSQL = `WITH src_rank AS (
 		SELECT _peerdb_data,_peerdb_record_type,_peerdb_unchanged_toast_columns,
 		RANK() OVER (PARTITION BY %s ORDER BY _peerdb_timestamp DESC) AS _peerdb_rank
 		FROM %s.%s WHERE _peerdb_batch_id=$1 AND _peerdb_destination_table_name=$2
 	)
-	INSERT INTO %s (%s) SELECT %s FROM src_rank WHERE _peerdb_rank=1 AND _peerdb_record_type!=2
+	INSERT INTO %s (%s) OVERRIDING SYSTEM VALUE SELECT %s FROM src_rank WHERE _peerdb_rank=1 AND _peerdb_record_type!=2
 	ON CONFLICT (%s) DO UPDATE SET %s`
 	fallbackDeleteStatementSQL = `WITH src_rank AS (
 		SELECT _peerdb_data,_peerdb_record_type,_peerdb_unchanged_toast_columns,
